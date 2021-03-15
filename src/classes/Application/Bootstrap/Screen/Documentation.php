0a8f590552f467a51f4752a857f89c4914bc7fb0
Mistralys <s.mordziol@gmail.com>
2021-03-15 18:00:24 +0100

    Added the documentation bootstrap screen.

---
+++ src/classes/Application/Bootstrap/Screen/Documentation.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+use AppUtils\FileHelper;
+use Mistralys\MarkdownViewer\DocsManager;
+use Mistralys\MarkdownViewer\DocsViewer;
+
+class Application_Bootstrap_Screen_Documentation extends Application_Bootstrap_Screen
+{
+    public function getDispatcher()
+    {
+        return 'documentation/index.php';
+    }
+
+    protected function _boot()
+    {
+        $this->enableScriptMode();
+        $this->createEnvironment();
+
+        $manager = new DocsManager();
+        $appDocFolder = APP_ROOT.'/documentation';
+
+        if(is_dir($appDocFolder))
+        {
+            $files = FileHelper::createFileFinder($appDocFolder)
+                ->includeExtension('md')
+                ->makeRecursive()
+                ->setPathmodeAbsolute();
+
+            foreach ($files as $file) {
+                $manager->addFile(FileHelper::removeExtension($file), $file);
+            }
+        }
+
+        $manager->addFile(
+            'Framework documentation',
+            $this->app->getVendorFolder().'/1and1/application_framework/docs/Documentation.md'
+        );
+
+        // The viewer needs to know the URL to the vendor/ folder, relative
+        // to the script. This is needed to load the clientside dependencies,
+        // like jQuery and Bootstrap.
+        (new DocsViewer($manager, '../vendor'))
+            ->setTitle('Documentation')
+            ->display();
+
+        Application::exit('Documentation displayed');
+    }
+}
