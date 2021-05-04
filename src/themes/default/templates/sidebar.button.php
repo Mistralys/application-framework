f92d9a44627a2559b273ec0ec2bef762a9760051
Mistralys <s.mordziol@gmail.com>
2021-05-04 08:35:11 +0200

    Converted to a class template.

--- src/themes/default/templates/sidebar.button.php
+++ src/themes/default/templates/sidebar.button.php
@@ -1,143 +1,245 @@
 <?php
+/**
+ * File containing the template class {@see template_default_frame}.
+ *
+ * @package UserInterface
+ * @subpackage Templates
+ * @see template_default_frame
+ */
 
-	/* @var $this UI_Page_Template */
-	/* @var $button UI_Page_Sidebar_Item_Button */
-    /* @var $menu UI_Bootstrap_DropdownMenu */
+declare(strict_types=1);
 
-	$button = $this->getVar('button');
-	$confirmMessage = $this->getVar('confirmMessage');
-	$menu = null;
-	
-	if($button instanceof UI_Page_Sidebar_Item_DropdownButton) {
-	    $menu = $button->getMenu();
-	}
+use AppUtils\Interface_Classable;
+use AppUtils\Traits_Classable;
 
-	$tagname = 'button';
-	$attributes = array(
-		'id' => $button->getID(),
-		'name' => $this->getVar('name'),
-		'type' => 'button',
-		'class' => 'btn btn-block',
-	);
+/**
+ * Main template for the frame skeleton of all pages.
+ *
+ * @package UserInterface
+ * @subpackage Templates
+ * @author Sebastian Mordziol <s.mordziol@mistralys.eu>
+ */
+class template_default_sidebar_button extends UI_Page_Template_Custom implements Interface_Classable
+{
+    use Traits_Classable;
 
-	$design = $this->getVar('design');
-	if(empty($design)) {
-	    $design = 'default';
-	}
-	
-	$attributes['class'] .= ' btn-'.$design;
-	
-	$mode = $this->getVar('mode');
-	
-	if(!$menu) 
-	{
-	    $attributes['onclick'] = $this->getVar('onclick');
+    protected function generateOutput() : void
+    {
+        $this->configureAction();
+        $this->configureLocking();
+        $this->configureDisabled();
+        $this->configureMode();
+        $this->configureTagName();
 
-	    if($this->hasVar('loadingText')) {
-	        $attributes['data-loading-text'] = $this->getVar('loadingText');
-	    }
-    	
-	    if($confirmMessage instanceof UI_Page_Sidebar_Item_Button_ConfirmMessage) 
-    	{
-    	    $mode = 'clickable';
-    	    $this->setVar('javascript', $confirmMessage->getJavaScript());
-    	}
-	}
-	
-	if($button->isLocked()) {
-	    $button->disable();
-	    $mode = 'clickable';
-	    $attributes['class'] .= ' btn-locked';
-	    $attributes['title'] = $button->getLockReason();
-	    $this->setVar(
-	        'javascript', 
-	        'LockManager.DialogActionDisabled()'
-        );
-	}
-	
-	if($button->isDisabled()) 
-	{
-	    $attributes['class'] .= ' disabled';
-	    $mode = 'none';
-	}
-	
-	switch($mode)
-	{
-		case 'linked':
-			$tagname = 'a';
-			$attributes['href'] = $this->getVar('url');
-			$target = $this->getVar('urlTarget');
-			if(!empty($target)) {
-			    $attributes['target'] = $target;
-			}
-			break;
+        $this->setAttribute('class', $this->classesToString());
 
-		case 'submit':
-			$attributes['type'] = 'submit';
-			$attributes['value'] = $attributes['name'];
-			break;
+        if(isset($this->menu)) {
+            ?>
+                <span style="position:relative">
+            <?php
+        }
+        ?>
+            <<?php echo $this->tagname ?> <?php echo compileAttributes($this->attributes) ?>>
+                <?php echo $this->renderLabel() ?>
+            </<?php echo $this->tagname ?>>
+        <?php
 
-		case 'clickable':
-			$attributes['onclick'] = $this->getVar('javascript');
-			break;
-			
-		case 'dropmenu':
-		    $this->ui->addJavascriptOnload(sprintf("$('#%s').dropdown()", $button->getID()));
-		    $attributes['data-toggle'] = 'dropdown';
-		    //$attributes['onclick'] = '';
-		    break;
-	}
+        if(isset($this->menu)) {
+            ?>
+                    <?php echo $this->menu->render() ?>
+                </span>
+            <?php
+        }
+    }
 
-	$icon = '';
-	$iconObject = $this->getVar('icon');
-	if($iconObject instanceof UI_Icon) {
-		$icon = $iconObject->render().' ';
-	}
-	
-	if($button->hasTooltip()) 
-	{
-	    $tIcon = UI::icon()
-	    ->setTooltip($button->getTooltip());
-	    
-	    // use a different icon if the button is disabled
-	    if($button->isDisabled()) {
-	        $tIcon->warning();
-	    } else {
-	        $tIcon->help();
-	    }
-	    
-	    $icon = 
-	    '<span class="button-tooltip-icon">'.
-	       $tIcon.
-	    '</span>'.
+    private function renderLabel() : string
+    {
+        $label = $this->button->getLabel();
+        if ($this->design == 'developer') {
+            $label = '<b>' . t('DEV:') . '</b> ' . $label;
+        }
+
+        if (isset($this->menu) && $this->button->hasCaret()) {
+            $label .= ' <span class="caret"></span>';
+        }
+
+        return $this->renderIcon().$label;
+    }
+
+    private function renderIcon() : string
+    {
+        $icon = '';
+        $iconObject = $this->getVar('icon');
+        if ($iconObject instanceof UI_Icon) {
+            $icon = $iconObject->render() . ' ';
+        }
+
+        if (!$this->button->hasTooltip()) {
+            return $icon;
+        }
+
+        $tIcon = UI::icon()
+            ->setTooltip($this->button->getTooltip());
+
+        // use a different icon if the button is disabled
+        if ($this->button->isDisabled()) {
+            $tIcon->warning();
+        } else {
+            $tIcon->help();
+        }
+
+        return
+        '<span class="button-tooltip-icon">' .
+            $tIcon .
+        '</span>' .
         $icon;
-	}
-	
-	$label = $button->getLabel();
-	if($design=='developer') {
-		$label = '<b>'.t('DEV:').'</b> '.$label;
-	}
-	
-	if($menu && $button->hasCaret()) {
-	    $label .= ' <span class="caret"></span>';
-	}
+    }
 
-	if($tagname=='a' && isset($attributes['type'])) {
-		unset($attributes['type']);
-	}
+    /**
+     * @var UI_Page_Sidebar_Item_Button
+     */
+    private $button;
 
-	$html =
-	'<'.$tagname.' '.compileAttributes($attributes).'>'.
-	   $icon.$label.
-	'</'.$tagname.'>';
-	
-    if($menu) {
-        $html = 
-        '<span style="position:relative">'.
-	        $html.
-            $menu->render().
-        '</span>';
-	}
+    /**
+     * @var UI_Bootstrap_DropdownMenu|NULL
+     */
+    private $menu = null;
 
-	echo $html;
-	
+    /**
+     * @var array<string,mixed>
+     */
+    private $attributes = array();
+
+    /**
+     * @var string
+     */
+    private $mode = '';
+
+    /**
+     * @var string
+     */
+    private $tagname = 'button';
+
+    /**
+     * @var string
+     */
+    private $design = 'default';
+
+    /**
+     * @var string
+     */
+    private $html = '';
+
+    /**
+     * @throws Application_Exception
+     */
+    protected function preRender() : void
+    {
+        $this->button = $this->getObjectVar('button', UI_Page_Sidebar_Item_Button::class);
+        $this->mode = $this->getStringVar('mode');
+
+        if ($this->button instanceof UI_Page_Sidebar_Item_DropdownButton) {
+            $this->menu = $this->button->getMenu();
+        }
+
+        if($this->hasVarNonEmpty('design')) {
+            $this->design = $this->getStringVar('design');
+        }
+
+        $this->addClass('btn');
+        $this->addClass('btn-block');
+        $this->addClass('btn-'.$this->design);
+
+        $this->setAttribute('id', $this->button->getID());
+        $this->setAttribute('name', $this->button->getName());
+        $this->setAttribute('type', 'button');
+    }
+
+    private function configureTagName() : void
+    {
+        if ($this->tagname === 'a' && isset($this->attributes['type'])) {
+            unset($this->attributes['type']);
+        }
+    }
+
+    private function configureDisabled() : void
+    {
+        if(!$this->button->isDisabled()) {
+            return;
+        }
+
+        $this->addClass('disabled');
+        $this->mode = 'none';
+    }
+
+    private function configureAction() : void
+    {
+        if (isset($this->menu)) {
+            return;
+        }
+
+        $this->setAttribute('onclick', $this->getVar('onclick'));
+
+        if ($this->hasVar('loadingText')) {
+            $this->setAttribute('data-loading-text', $this->getStringVar('loadingText'));
+        }
+
+        $confirmMessage = $this->getVar('confirmMessage');
+        if($confirmMessage instanceof UI_Page_Sidebar_Item_Button_ConfirmMessage) {
+            $this->mode = 'clickable';
+            $this->setVar('javascript', $confirmMessage->getJavaScript());
+        }
+    }
+
+    private function configureLocking() : void
+    {
+        if (!$this->button->isLocked()) {
+            return;
+        }
+
+        $this->button->disable();
+        $this->mode = 'clickable';
+
+        $this->addClass('btn-locked');
+        $this->setAttribute('title', $this->button->getLockReason());
+        $this->setVar(
+            'javascript',
+            'LockManager.DialogActionDisabled()'
+        );
+    }
+
+    private function configureMode() : void
+    {
+        switch ($this->mode) {
+            case 'linked':
+                $this->tagname = 'a';
+                $this->setAttribute('href', $this->getStringVar('url'));
+
+                if ($this->hasVarNonEmpty('urlTarget')) {
+                    $this->setAttribute('target', $this->getStringVar('urlTarget'));
+                }
+                break;
+
+            case 'submit':
+                $this->setAttribute('type', 'submit');
+                $this->setAttribute('value', $this->attributes['name']);
+                break;
+
+            case 'clickable':
+                $this->setAttribute('onclick', $this->getVar('javascript'));
+                break;
+
+            case 'dropmenu':
+                $this->ui->addJavascriptOnload(sprintf("$('#%s').dropdown()", $this->button->getID()));
+                $this->setAttribute('data-toggle', 'dropdown');
+                //$attributes['onclick'] = '';
+                break;
+        }
+    }
+
+    private function setAttribute(string $name, string $value) : template_default_sidebar_button
+    {
+        $this->attributes[$name] = $value;
+        return $this;
+    }
+}
