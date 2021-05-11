d5b61f2a687a82b29d4b45340f63c2664006d8a9
Mistralys <s.mordziol@gmail.com>
2021-05-11 08:20:22 +0200

    Added an alert message property.

---
+++ src/classes/UI/PropertiesGrid/Property/Message.php
@@ -0,0 +1,36 @@
+<?php
+
+class UI_PropertiesGrid_Property_Message extends UI_PropertiesGrid_Property_Merged
+{
+    /**
+     * @var UI_Message
+     */
+    private $message;
+
+    protected $classes = array('prop-message');
+
+    protected function init()
+    {
+        $this->message = $this->grid->getUI()->createMessage('')
+            ->makeNotDismissable()
+            ->makeInfo();
+    }
+
+    public function getMessage() : UI_Message
+    {
+        return $this->message;
+    }
+
+    public function render()
+    {
+        $this->message->setMessage($this->label);
+        $this->label = $this->message->render();
+
+        return parent::render();
+    }
+
+    protected function filterValue($text) : UI_StringBuilder
+    {
+        return sb();
+    }
+}
