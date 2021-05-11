f56cc6cc4bab8e7935bd6403090945c63d8c2830
Mistralys <s.mordziol@gmail.com>
2021-05-11 08:19:46 +0200

    Added internal classes support.

--- src/classes/UI/PropertiesGrid/Property/Merged.php
+++ src/classes/UI/PropertiesGrid/Property/Merged.php
@@ -2,14 +2,19 @@
 
 class UI_PropertiesGrid_Property_Merged extends UI_PropertiesGrid_Property
 {
+    protected $classes = array();
+
     public function render()
     {
-        $html =
-        '<tr class="prop-merged">'.
-            '<td colspan="2">'.$this->label.'</td>'.
-        '</tr>';
-
-        return $html;
+        ob_start();
+        ?>
+            <tr class="prop-merged <?php echo implode(' ', $this->classes) ?>">
+                <td colspan="2">
+                    <?php echo $this->label ?>
+                </td>
+            </tr>
+        <?php
+        return ob_get_clean();
     }
     
     protected function filterValue($text) : UI_StringBuilder
