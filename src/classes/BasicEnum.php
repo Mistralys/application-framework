6a57c8a7481f9f08c3347a379cf8d64596af0c6f
Mistralys <s.mordziol@gmail.com>
2021-05-25 13:03:13 +0200

    Revamped internals and moved to correct location.

---
+++ src/classes/BasicEnum.php
@@ -0,0 +1,112 @@
+<?php
+
+declare(strict_types=1);
+
+use function AppUtils\parseVariable;
+
+const ENUM_ERROR_VALUE_DOES_NOT_EXIST = 87001;
+
+abstract class BasicEnum
+{
+    /**
+     * @var array<string,array<string,mixed>>
+     */
+    private static $values = array();
+
+    /**
+     * Retrieves the names of all available enum values.
+     * @return string[]
+     */
+    public static function getNames() : array
+    {
+        return array_keys(self::resolveEnumValues(get_called_class()));
+    }
+
+    /**
+     * Retrieves all constants for the specified enum class.
+     *
+     * @param string $enumClass
+     * @return array<string,mixed>
+     */
+    private static function resolveEnumValues(string $enumClass) : array
+    {
+        self::initEnumValues($enumClass);
+
+        return self::$values[$enumClass];
+    }
+
+    /**
+     * Initializes the list of available constant names,
+     * once per enum class. Stores the names both in the
+     * original case, and in lowercase for a non-strict
+     * check.
+     *
+     * @param string $enumClass
+     */
+    private static function initEnumValues(string $enumClass) : void
+    {
+        if(isset(self::$values[$enumClass])) {
+            return;
+        }
+
+        $reflectionClass = new ReflectionClass(static::class);
+        self::$values[$enumClass] = $reflectionClass->getConstants();
+
+        ksort(self::$values[$enumClass]);
+    }
+
+    /**
+     * Retrieves all available values in the enum, as
+     * constant name => value pairs.
+     *
+     * @return array<string,string|int|float|bool|NULL>
+     */
+    public static function getValues() : array
+    {
+        return self::resolveEnumValues(get_called_class());
+    }
+
+    public static function isValidName(string $name) : bool
+    {
+        $values = self::resolveEnumValues(get_called_class());
+
+        return isset($values[$name]);
+    }
+
+    public static function isValidValue($value) : bool
+    {
+        $values = array_values(self::resolveEnumValues(get_called_class()));
+        return in_array($value, $values, true);
+    }
+
+    /**
+     * Attempts to retrieve the name of an enum's constant
+     * by its value.
+     *
+     * @param scalar $value
+     * @return string
+     * @throws Application_Exception
+     *
+     * @see BasicEnum::_ERROR_VALUE_DOES_NOT_EXIST
+     * @see BasicEnum::isValidValue()
+     */
+    public static function getNameByValue($value) : string
+    {
+        $values = self::resolveEnumValues(get_called_class());
+
+        foreach($values as $name => $checkValue) {
+            if($value === $checkValue) {
+                return $name;
+            }
+        }
+
+        throw new Application_Exception(
+            'Unknown enum constant name',
+            sprintf(
+                'Tried getting name for value [%s].',
+                parseVariable($value)->enableType()->toString()
+            ),
+            ENUM_ERROR_VALUE_DOES_NOT_EXIST
+        );
+    }
+}
