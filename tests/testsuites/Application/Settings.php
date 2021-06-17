af897355a662f7000a09cd3f7506334e679fd288
Mistralys <s.mordziol@gmail.com>
2021-06-17 14:43:34 +0200

    Updated tests so they can be run in any order.

--- tests/testsuites/Application/Settings.php
+++ tests/testsuites/Application/Settings.php
@@ -5,22 +5,31 @@ final class Application_SettingsTest extends ApplicationTestCase
     private $settingName = 'expiry_test';
     private $settingValue = 'bar';
     private $settingUpdatedValue = 'bar2';
+    private $expiryColumnExists = false;
 
     protected function setUp() : void
     {
         parent::setUp();
-        Application_Driver::setSetting($this->settingName, $this->settingValue);
+
+        $this->expiryColumnExists = DBHelper::columnExists(Application_Driver_Storage_DB::TABLE_NAME, Application_Driver_Storage_DB::COL_EXPIRY);
+
+        $this->assertEquals(Application_Driver::STORAGE_TYPE_DB, Application_Driver::getStorageType());
+
+        $this->clearRecords();
     }
 
     protected function tearDown() : void
     {
         parent::tearDown();
-        Application_Driver::deleteSetting($this->settingName);
+
+        $this->clearRecords();
     }
 
     public function test_setSetting() : void
     {
         $this->logHeader('Set application setting');
+
+        Application_Driver::setSetting($this->settingName, $this->settingValue);
 
         $this->assertSame($this->settingValue, Application_Driver::getSetting($this->settingName));
     }
@@ -29,27 +38,51 @@ final class Application_SettingsTest extends ApplicationTestCase
     {
         $this->logHeader('Update application setting');
 
+        Application_Driver::setSetting($this->settingName, $this->settingValue);
         Application_Driver::setSetting($this->settingName, $this->settingUpdatedValue);
+
         $this->assertSame($this->settingUpdatedValue, Application_Driver::getSetting($this->settingName));
     }
 
     public function test_setExpirySettingFuture() : void
     {
+        $this->skipIfExpiryNotPresent();
+
         $this->logHeader('Update expiry date one day later application setting');
+
+        Application_Driver::setSetting($this->settingName, $this->settingValue);
 
         $value = new DateTime();
         $value->modify('+1 day');
         Application_Driver::setSettingExpiry($this->settingName, $value);
-        $this->assertNotEmpty(Application_Driver::getSetting($this->settingName));
+
+        $this->assertEquals($this->settingValue, Application_Driver::getSetting($this->settingName));
     }
 
     public function test_setExpirySettingPast() : void
     {
+        $this->skipIfExpiryNotPresent();
+
         $this->logHeader('Update expiry date one day before application setting');
+
+        Application_Driver::setSetting($this->settingName, $this->settingValue);
 
         $value = new DateTime();
         $value->modify('-1 day');
         Application_Driver::setSettingExpiry($this->settingName, $value);
-        $this->assertEmpty(Application_Driver::getSetting($this->settingName));
+
+        $this->assertNull(Application_Driver::getSetting($this->settingName));
+    }
+
+    private function skipIfExpiryNotPresent() : void
+    {
+        if(!$this->expiryColumnExists) {
+            $this->markTestSkipped('Expiry column is not present.');
+        }
+    }
+
+    private function clearRecords() : void
+    {
+        DBHelper::deleteRecords(Application_Driver_Storage_DB::TABLE_NAME);
     }
 }
