dac002f6e2f69b5d52f68a5b0272ecea5046e1a3
Mistralys <s.mordziol@gmail.com>
2021-06-17 14:43:18 +0200

    Added constants; Added expiry column check.
    Also updated DBHelper methods to use more recent methods.

--- src/classes/Application/Driver/Storage/DB.php
+++ src/classes/Application/Driver/Storage/DB.php
@@ -2,32 +2,45 @@
 
 class Application_Driver_Storage_DB extends Application_Driver_Storage
 {
+    const TABLE_NAME = 'app_settings';
+
+    const COL_VALUE = 'data_value';
+    const COL_KEY = 'data_key';
+    const COL_EXPIRY = 'expiry_date';
+    const COL_ROLE = 'data_role';
+
     public function get($name)
     {
-        $entry = DBHelper::fetch(
-            "SELECT
-                `data_value`,
-                `expiry_date`
-            FROM
-                app_settings
-            WHERE
-                `data_key`=:data_key",
-            array(
-                ':data_key' => $name
-            )
-        );
+        $value = DBHelper::createFetchKey(self::COL_VALUE, self::TABLE_NAME)
+            ->whereValue(self::COL_KEY, $name)
+            ->fetchString();
 
-        $expiryDate = null;
-        if (!empty($entry['expiry_date']))
-        {
-            $expiryDate = DateTime::createFromFormat('Y-m-d H:i:s', $entry['expiry_date']);
+        if(empty($value)) {
+            return null;
         }
-        if($expiryDate === null || $expiryDate>new DateTime())
-        {
-            if (is_array($entry) && isset($entry['data_value']))
-            {
-                return $entry['data_value'];
-            }
+
+        $expiryDate = $this->getExpiry($name);
+
+        if($expiryDate === null || $expiryDate >= new DateTime()) {
+            return $value;
+        }
+
+        return null;
+    }
+
+    public function getExpiry(string $name) : ?DateTime
+    {
+        // FIXME: Can be removed once this is supported everywhere.
+        if(!DBHelper::columnExists(self::TABLE_NAME, self::COL_EXPIRY)) {
+            return null;
+        }
+
+        $date = DBHelper::createFetchKey(self::COL_EXPIRY, self::TABLE_NAME)
+            ->whereValue(self::COL_KEY, $name)
+            ->fetchString();
+
+        if(!empty($date)) {
+            return DateTime::createFromFormat('Y-m-d H:i:s', $date);
         }
 
         return null;
@@ -36,43 +49,43 @@ class Application_Driver_Storage_DB extends Application_Driver_Storage
     public function set($name, $value, $role)
     {
         DBHelper::insertOrUpdate(
-            'app_settings',
+            self::TABLE_NAME,
             array(
-                'data_key' => $name,
-                'data_value' => $value,
-                'data_role' => $role
+                self::COL_KEY => $name,
+                self::COL_VALUE => $value,
+                self::COL_ROLE => $role
             ),
             array(
-                'data_key'
+                self::COL_KEY
             )
         );
     }
 
     public function setExpiry(string $name, DateTime $date) : void
     {
-        DBHelper::update(
-            "UPDATE
-				`app_settings`
-			SET
-			    `expiry_date`=:expiry_date
-            WHERE
-                `data_key`=:data_key",
+        // FIXME: Can be removed once this is supported everywhere.
+        if(!DBHelper::columnExists(self::TABLE_NAME, self::COL_EXPIRY)) {
+            return;
+        }
+
+        DBHelper::updateDynamic(
+            self::TABLE_NAME,
             array(
-                ':expiry_date' => $date,
-                ':data_key' => $name
+                self::COL_EXPIRY => $date,
+                self::COL_KEY => $name
+            ),
+            array(
+                self::COL_KEY
             )
         );
     }
 
     public function delete($name)
     {
-        DBHelper::delete(
-            "DELETE FROM
-                `app_settings`
-            WHERE
-                `data_key`=:data_key",
+        DBHelper::deleteRecords(
+            self::TABLE_NAME,
             array(
-                ':data_key' => $name
+                self::COL_KEY => $name
             )
         );
     }
