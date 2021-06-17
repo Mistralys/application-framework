8ad7ca89a9cf60bd078a5ff6e8cac526ef183da3
Emre Celebi <emre.celebi@ionos.com>
2021-06-17 09:04:29 +0200

    - PHPDoc update

--- src/classes/UI/Form/Element/HTMLTimePicker.php
+++ src/classes/UI/Form/Element/HTMLTimePicker.php
@@ -1,39 +1,18 @@
 <?php
 /**
- * Base class for <input> elements
- *
- * PHP version 5
- *
- * LICENSE
- *
- * This source file is subject to BSD 3-Clause License that is bundled
- * with this package in the file LICENSE and available at the URL
- * https://raw.githubusercontent.com/pear/HTML_QuickForm2/trunk/docs/LICENSE
- *
- * @category  HTML
- * @package   HTML_QuickForm2
- * @author    Alexey Borzov <avb@php.net>
- * @author    Bertrand Mansion <golgote@mamasam.com>
- * @copyright 2006-2020 Alexey Borzov <avb@php.net>, Bertrand Mansion <golgote@mamasam.com>
- * @license   https://opensource.org/licenses/BSD-3-Clause BSD 3-Clause License
- * @link      https://pear.php.net/package/HTML_QuickForm2
- */
-
-// pear-package-only /**
-// pear-package-only  * Base class for simple HTML_QuickForm2 elements (not Containers)
-// pear-package-only  */
-// pear-package-only require_once 'HTML/QuickForm2/Element.php';
-
-/**
- * Base class for <input> elements
+ * File containing the {@link HTML_QuickForm2_Element_HTMLTimePicker} class.
  *
  * @category HTML
  * @package  HTML_QuickForm2
- * @author   Alexey Borzov <avb@php.net>
- * @author   Bertrand Mansion <golgote@mamasam.com>
- * @license  https://opensource.org/licenses/BSD-3-Clause BSD 3-Clause License
- * @version  Release: @package_version@
- * @link     https://pear.php.net/package/HTML_QuickForm2
+ */
+
+/**
+ * Element that is used to handle generate HTML input with type time.
+ * All browsers(except IE) will open time selection menu as input
+ *
+ * @category HTML
+ * @package  HTML_QuickForm2
+ * @author   Emre Celebi <emre.celebi@ionos.com>
  */
 class HTML_QuickForm2_Element_HTMLTimePicker extends HTML_QuickForm2_Element
 {
