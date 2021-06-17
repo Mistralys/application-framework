b27a1bcc7c16b8ef82ff7cacf06ae947a28f4400
Mistralys <s.mordziol@gmail.com>
2021-06-17 14:39:59 +0200

    Added `getExpiry()`.

--- src/classes/Application/Driver/Storage.php
+++ src/classes/Application/Driver/Storage.php
@@ -12,11 +12,25 @@ abstract class Application_Driver_Storage
 
     }
 
+    /**
+     * @param string $name
+     * @return string|null
+     */
     abstract public function get($name);
 
+    /**
+     * @param string $name
+     * @param mixed $value
+     * @param string $role persistent|cache
+     */
     abstract public function set($name, $value, $role);
 
+    /**
+     * @param string $name
+     */
     abstract public function delete($name);
 
     abstract public function setExpiry(string $name, DateTime $date) : void;
+
+    abstract public function getExpiry(string $name) : ?DateTime;
 }
