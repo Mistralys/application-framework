04e02ddbb6bd99699df1a68844ffb6d0a4b06319
Mistralys <s.mordziol@gmail.com>
2021-07-26 10:32:40 +0200

    Updated to a class based template. Added optional user name display.

--- src/themes/default/templates/frame.header.user-menu.php
+++ src/themes/default/templates/frame.header.user-menu.php
@@ -1,53 +1,93 @@
 <?php
+/**
+ * File containing the template class {@see template_default_frame_header_user_menu}.
+ *
+ * @package UserInterface
+ * @subpackage Templates
+ * @see template_default_frame_header_user_menu
+ */
 
-/* @var $this UI_Page_Template */
+declare(strict_types=1);
 
-$menu = $this->ui->createButtonDropdown()
-->makeNavItem()
-->noCaret()
-->setIcon(UI::icon()->user())
-->setLabel($this->user->getName());
+/**
+ * Template for the user's drop down menu.
+ *
+ * @package UserInterface
+ * @subpackage Templates
+ * @author Sebastian Mordziol <s.mordziol@mistralys.eu>
+ */
+class template_default_frame_header_user_menu extends UI_Page_Template_Custom
+{
+    /**
+     * @var bool
+     */
+    private $showUserName = false;
 
-$menu->addLink(t('Settings'), '?page=settings')
-    ->setTitle(t('Opens your %1$s user settings.', $this->driver->getAppNameShort()))
-    ->setIcon(UI::icon()->settings());
+    protected function generateOutput() : void
+    {
+        $menu = $this->ui->createButtonDropdown()
+            ->makeNavItem()
+            ->noCaret()
+            ->setIcon(UI::icon()->user());
 
-$menu->addClickable(t('Notepad'), 'Driver.DialogNotepad()')
-    ->setTitle(t('Opens your personal notepad.'))
-    ->setIcon(UI::icon()->notepad());
+        if($this->showUserName)
+        {
+            $menu->setLabel($this->user->getName());
+        }
 
-$menu->addSeparator();
+        $menu->addLink(t('Settings'), '?page=settings')
+            ->setTitle(t('Opens your %1$s user settings.', $this->driver->getAppNameShort()))
+            ->setIcon(UI::icon()->settings());
 
-$menu->addHeader(t('Interface width'));
+        $menu->addClickable(t('Notepad'), 'Driver.DialogNotepad()')
+            ->setTitle(t('Opens your personal notepad.'))
+            ->setIcon(UI::icon()->notepad());
 
-$std = $menu->addLink(t('Standard'), $this->request->buildRefreshURL(array('set_usersetting' => 'layout_width', 'value' => 'standard')));
-$max = $menu->addLink(t('Maximized'), $this->request->buildRefreshURL(array('set_usersetting' => 'layout_width', 'value' => 'maximized')));
+        $menu->addSeparator();
 
-if($this->user->getSetting('layout_width', 'standard') == 'maximized') {
-    $std->setIcon(UI::icon()->itemInactive());
-    $max->setIcon(UI::icon()->itemActive());
-} else {
-    $std->setIcon(UI::icon()->itemActive());
-    $max->setIcon(UI::icon()->itemInactive());
+        $menu->addHeader(t('Interface width'));
+
+        $std = $menu->addLink(t('Standard'), $this->request->buildRefreshURL(array(Application_Bootstrap_Screen::REQUEST_PARAM_SET_USERSETTING => 'layout_width', 'value' => 'standard')));
+        $max = $menu->addLink(t('Maximized'), $this->request->buildRefreshURL(array(Application_Bootstrap_Screen::REQUEST_PARAM_SET_USERSETTING => 'layout_width', 'value' => 'maximized')));
+
+        if ($this->user->getSetting('layout_width', 'standard') == 'maximized')
+        {
+            $std->setIcon(UI::icon()->itemInactive());
+            $max->setIcon(UI::icon()->itemActive());
+        }
+        else
+        {
+            $std->setIcon(UI::icon()->itemActive());
+            $max->setIcon(UI::icon()->itemInactive());
+        }
+
+        $menu->addHeader(t('Interface font size'));
+
+        $std = $menu->addLink(t('Standard'), $this->request->buildRefreshURL(array(Application_Bootstrap_Screen::REQUEST_PARAM_SET_USERSETTING => 'layout_fontsize', 'value' => 'standard')));
+        $max = $menu->addLink(t('Bigger'), $this->request->buildRefreshURL(array(Application_Bootstrap_Screen::REQUEST_PARAM_SET_USERSETTING => 'layout_fontsize', 'value' => 'bigger')));
+
+        if ($this->user->getSetting('layout_fontsize', 'standard') == 'bigger')
+        {
+            $std->setIcon(UI::icon()->itemInactive());
+            $max->setIcon(UI::icon()->itemActive());
+        }
+        else
+        {
+            $std->setIcon(UI::icon()->itemActive());
+            $max->setIcon(UI::icon()->itemInactive());
+        }
+
+        $menu->addSeparator();
+
+        $menu->addLink(t('Log out'), '?logout=yes')
+            ->setTitle(t('Click here to end your session and log out.'))
+            ->setIcon(UI::icon()->logOut());
+
+        $menu->display();
+    }
+
+    protected function preRender() : void
+    {
+        $this->showUserName = Application_Driver::getBoolSetting(UI_Themes::OPTION_SHOW_USER_NAME);
+    }
 }
-
-$menu->addHeader(t('Interface font size'));
-
-$std = $menu->addLink(t('Standard'), $this->request->buildRefreshURL(array('set_usersetting' => 'layout_fontsize', 'value' => 'standard')));
-$max = $menu->addLink(t('Bigger'), $this->request->buildRefreshURL(array('set_usersetting' => 'layout_fontsize', 'value' => 'bigger')));
-
-if($this->user->getSetting('layout_fontsize', 'standard') == 'bigger') {
-    $std->setIcon(UI::icon()->itemInactive());
-    $max->setIcon(UI::icon()->itemActive());
-} else {
-    $std->setIcon(UI::icon()->itemActive());
-    $max->setIcon(UI::icon()->itemInactive());
-}
-
-$menu->addSeparator();
-
-$menu->addLink(t('Log out'), '?logout=yes')
-->setTitle(t('Click here to end your session and log out.'))
-->setIcon(UI::icon()->logOut());
-
-$menu->display();
