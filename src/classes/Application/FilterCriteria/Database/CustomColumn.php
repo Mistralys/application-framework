e7b9f6ce1103f709b218248bfd15bfa6d57ca87f
Mistralys <s.mordziol@gmail.com>
2021-07-27 18:42:49 +0200

    Added new database filters with OO custom column handling.

---
+++ src/classes/Application/FilterCriteria/Database/CustomColumn.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * File containing the class {@see Application_FilterCriteria_Database_CustomColumn}.
+ *
+ * @package Application
+ * @subpackage FilterCriteria
+ * @see Application_FilterCriteria_Database_CustomColumn
+ */
+
+declare(strict_types=1);
+
+use AppUtils\NamedClosure;
+
+/**
+ * Container for the configuration of individual custom columns
+ * to use in a database filter criteria class.
+ *
+ * @package Application
+ * @subpackage FilterCriteria
+ * @author Sebastian Mordziol <s.mordziol@mistralys.eu>
+ *
+ * @see Application_FilterCriteria_Database::registerCustomColumn()
+ */
+class Application_FilterCriteria_Database_CustomColumn
+{
+    const ERROR_SELECT_STATEMENT_NOT_A_STRING = 90401;
+
+    /**
+     * @var NamedClosure
+     */
+    private $callback;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var Application_FilterCriteria_Database
+     */
+    private $filters;
+
+    /**
+     * @var bool
+     */
+    private $enabled = false;
+
+    public function __construct(Application_FilterCriteria_Database $filters, string $name, NamedClosure $callback)
+    {
+        $this->filters = $filters;
+        $this->name = $name;
+        $this->callback = $callback;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName() : string
+    {
+        return $this->name;
+    }
+
+    public function isEnabled() : bool
+    {
+        return $this->enabled;
+    }
+
+    public function setEnabled(bool $enabled) : Application_FilterCriteria_Database_CustomColumn
+    {
+        $this->enabled = $enabled;
+        return $this;
+    }
+
+    /**
+     * @return string
+     * @throws Application_Exception
+     * @see Application_FilterCriteria_Database_CustomColumn::ERROR_SELECT_STATEMENT_NOT_A_STRING
+     */
+    public function getSelect() : string
+    {
+        $result = call_user_func($this->callback);
+
+        if(is_string($result)) {
+            return $result.' AS '.$this->name;
+        }
+
+        throw new Application_Exception(
+            'Invalid custom column select value',
+            sprintf(
+                'The callback for the custom column [%s] did not return a string, but [%s].',
+                $this->name,
+                gettype($result)
+            ),
+            self::ERROR_SELECT_STATEMENT_NOT_A_STRING
+        );
+    }
+}
---
+++ src/classes/Application/FilterCriteria/DatabaseExtended.php
@@ -0,0 +1,178 @@
+<?php
+/**
+ * File containing the class {@see Application_FilterCriteria_DatabaseExtended}.
+ *
+ * @package Application
+ * @subpackage FilterCriteria
+ * @see Application_FilterCriteria_DatabaseExtended
+ */
+
+declare(strict_types=1);
+
+use AppUtils\NamedClosure;
+
+/**
+ * Formalizes the use of custom columns in the filter criteria,
+ * by defining abstract methods to set up the custom columns to
+ * use.
+ *
+ * @package Application
+ * @subpackage FilterCriteria
+ * @author Sebastian Mordziol <s.mordziol@mistralys.eu>
+ */
+abstract class Application_FilterCriteria_DatabaseExtended extends Application_FilterCriteria_Database
+{
+    /**
+     * @var bool
+     */
+    private $customInitialized = false;
+
+    /**
+     * @var array<string,Application_FilterCriteria_Database_CustomColumn>
+     */
+    protected $customColumns = array();
+
+    protected function initQuery() : void
+    {
+        $this->initCustomColumns();
+    }
+
+    abstract protected function _initCustomColumns() : void;
+
+    protected function initCustomColumns() : void
+    {
+        if($this->customInitialized)
+        {
+            return;
+        }
+
+        $this->_initCustomColumns();
+    }
+
+    protected function collectSelects() : array
+    {
+        $this->initCustomColumns();
+
+        $selects = parent::collectSelects();
+
+        return array_merge($selects, $this->getCustomSelects());
+    }
+
+    /**
+     * Fetches all select statements for custom columns.
+     *
+     * @return string[]
+     *
+     * @throws Application_Exception
+     * @see Application_FilterCriteria_Database_CustomColumn::ERROR_SELECT_STATEMENT_NOT_A_STRING
+     */
+    protected function getCustomSelects() : array
+    {
+        $result = array();
+
+        foreach ($this->customColumns as $column)
+        {
+            if(!$column->isEnabled())
+            {
+                continue;
+            }
+
+            $result[] = $column->getSelect();
+        }
+
+        return $result;
+    }
+
+    /**
+     * Fetches the custom column's select statement from the target method.
+     *
+     * @param string $columnID
+     * @return string
+     *
+     * @throws Application_Exception
+     * @see Application_FilterCriteria_Database::ERROR_CUSTOM_COLUMN_NOT_REGISTERED
+     */
+    protected function getCustomSelect(string $columnID) : string
+    {
+        if(isset($this->customColumns[$columnID]))
+        {
+            return $this->customColumns[$columnID]->getSelect();
+        }
+
+        throw $this->createMissingColumnException($columnID);
+    }
+
+    /**
+     * Select a custom column that is not directly available in the
+     * target table. This requires the column to have been registered
+     * first, using the {@see Application_FilterCriteria_Database::registerCustomColumn()}
+     * method.
+     *
+     * To use this feature, it is recommended to use the extended
+     * filter class {@see Application_FilterCriteria_DatabaseExtended}.
+     * It formalizes the use of custom columns.
+     *
+     * @param string $columnID The name of the custom column
+     * @param bool $enable Whether to enable this column. Allows turning it on and off at will.
+     * @return $this
+     *
+     * @see Application_FilterCriteria_Database::registerCustomColumn()
+     *
+     * @throws Application_Exception
+     * @see Application_FilterCriteria_Database::ERROR_CUSTOM_COLUMN_NOT_REGISTERED
+     */
+    protected function withCustomColumn(string $columnID, bool $enable=true)
+    {
+        $this->initCustomColumns();
+
+        if(isset($this->customColumns[$columnID]))
+        {
+            $this->customColumns[$columnID]->setEnabled($enable);
+            return $this;
+        }
+
+        throw $this->createMissingColumnException($columnID);
+    }
+
+    protected function createMissingColumnException(string $columnID) : Application_Exception
+    {
+        return new Application_Exception(
+            'Custom column not registered',
+            sprintf(
+                'The custom column [%s] is not available, it has not been registered in the filters class [%s]. Registered columns are: [%s].',
+                $columnID,
+                get_class($this),
+                implode(', ', array_keys($this->customColumns))
+            ),
+            self::ERROR_CUSTOM_COLUMN_NOT_REGISTERED
+        );
+    }
+
+    /**
+     * Check if the specified custom column has been added,
+     * and is enabled. Returns false if it has been added,
+     * but is not enabled.
+     *
+     * @param string $columnID
+     * @return bool
+     */
+    public function hasCustomColumn(string $columnID) : bool
+    {
+        return isset($this->customColumns[$columnID]) && $this->customColumns[$columnID]->isEnabled();
+    }
+
+    /**
+     * Registers a new custom column that is used to add
+     * additional, dynamic select statements to the query.
+     *
+     * @param string $columnID
+     * @param NamedClosure $callback
+     * @return Application_FilterCriteria_Database_CustomColumn
+     */
+    protected function registerCustomColumn(string $columnID, NamedClosure $callback) : Application_FilterCriteria_Database_CustomColumn
+    {
+        $column = new Application_FilterCriteria_Database_CustomColumn($this, $columnID, $callback);
+        $this->customColumns[$columnID] = $column;
+        return $column;
+    }
+}
