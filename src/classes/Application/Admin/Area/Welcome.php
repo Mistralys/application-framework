2d9358b3ab7f94ba737fd8da133f1e09cbbf6197
Mistralys <s.mordziol@gmail.com>
2021-07-27 20:11:30 +0200

    Added screen help.

--- src/classes/Application/Admin/Area/Welcome.php
+++ src/classes/Application/Admin/Area/Welcome.php
@@ -69,9 +69,62 @@ class Application_Admin_Area_Welcome extends Application_Admin_Area
         }
     }
 
+    private function getCategoryLabels() : array
+    {
+        $categories = $this->recent->getCategories();
+        $items = array();
+        foreach ($categories as $category)
+        {
+            $items[] = $category->getLabel();
+        }
+
+        return $items;
+    }
+
     protected function _handleHelp()
     {
-        $this->renderer->setTitle($this->getTitle())->getTitle()->setIcon($this->getNavigationIcon());
+        $this->help->setSummary(t('Your personal activity tracker'));
+
+        $this->help->addPara(sb()
+            ->t('The tracker will add elements you visit in %1$s to your quickstart, so you can easily find them again.', $this->driver->getAppNameShort())
+            ->t('This includes the following items:')
+        );
+
+        $this->help->addPara(sb()
+            ->ul($this->getCategoryLabels())
+        );
+
+        $this->help->addPara(sb()
+            ->t('Every time you visit the same item again, it is moved up to the top of the list again.')
+            ->t('This way, you can always see what you worked on last.')
+        );
+
+        $this->help->addHeader(t('Turning off the quickstart'));
+
+        $this->help->addPara(sb()
+            ->t('The quickstart can not be turned off entirely.')
+            ->t('However, you can change the page you see when you log in.')
+            ->t(
+                'For this, go into your %1$suser settings%2$s, and select the startup tab you would prefer.',
+                '<a href="'.$this->user->getAdminSettingsURL().'">',
+                '</a>'
+            )
+            ->t('The quickstart will still be available should you need it later.')
+        );
+
+        $this->renderer->getTitle()
+            ->setIcon($this->getNavigationIcon())
+            ->addContextElement(
+                UI::button(t('Open notepad'))
+                ->setIcon(UI::icon()->notepad())
+                ->setTooltip(Application_User_Notepad::getTooltipText())
+                ->click(Application_User_Notepad::getJSOpen())
+            );
+
+        $this->renderer->setTitle(sb()
+            ->add($this->getTitle())
+            ->add(UI::label(t('BETA'))->makeWarning())
+        );
     }
 
     public function _renderContent()
