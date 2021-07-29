ce11836e4b6875b2a66b115806d05ec3951c6397
Mistralys <s.mordziol@gmail.com>
2021-07-29 08:15:13 +0200

    Added special note category class for pinned notes.

---
+++ src/classes/Application/User/Recent/NoteCategory.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+class Application_User_Recent_NoteCategory extends Application_User_Recent_Category
+{
+    const REQUEST_PARAM_UNPIN_NOTE = 'unpin-note';
+    /**
+     * @var Application_User_Notepad_Note
+     */
+    private $note;
+
+    public function __construct(Application_User_Recent $recent, Application_User_Notepad_Note $note)
+    {
+        $this->note = $note;
+
+        $alias = 'note-'.$note->getID();
+
+        parent::__construct($recent, $alias, $this->resolveTitle($note));
+    }
+
+    /**
+     * @return Application_User_Notepad_Note
+     */
+    public function getNote() : Application_User_Notepad_Note
+    {
+        return $this->note;
+    }
+
+    private function resolveTitle(Application_User_Notepad_Note $note) : string
+    {
+        $title = $note->getTitle();
+
+        if(!empty($title)) {
+            return $title;
+        }
+
+        return t('Pinned note');
+    }
+
+    public function renderContent() : string
+    {
+        return $this->note->renderContent();
+    }
+
+    public function getIcon() : ?UI_Icon
+    {
+        return UI::icon()->notepad();
+    }
+
+    public function getAdminURLUnpin(array $params=array()) : string
+    {
+        $params[self::REQUEST_PARAM_UNPIN_NOTE] = $this->note->getID();
+
+        return $this->recent->getAdminURL($params);
+    }
+}
