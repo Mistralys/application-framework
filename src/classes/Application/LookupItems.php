97743395782fa69560c47227a0b8ff04696cfe83
Mistralys <s.mordziol@gmail.com>
2021-07-29 20:33:36 +0200

    Modernized the class somewhat; Added `idExists()` and `getItemByID()`.

--- src/classes/Application/LookupItems.php
+++ src/classes/Application/LookupItems.php
@@ -1,15 +1,32 @@
 <?php
 
-require_once 'Application/LookupItems/Item.php';
+declare(strict_types=1);
 
-class Application_LookupItems
+use AppUtils\FileHelper;
+use AppUtils\FileHelper_Exception;
+
+class Application_LookupItems implements Application_Interfaces_Loggable
 {
+    use Application_Traits_Loggable;
+
     const ERROR_INVALID_LOOKUP_ITEM_CLASS = 25101;
-    
+    const ERROR_ITEM_NOT_FOUND = 25102;
+
+    /**
+     * @var string
+     */
     protected $driverID;
-    
+
+    /**
+     * @var string
+     */
     protected $folder;
-    
+
+    /**
+     * @var Application_LookupItems_Item[]
+     */
+    protected $items;
+
     public function __construct(Application_Driver $driver)
     {
         $this->driverID = $driver->getID();
@@ -21,61 +38,118 @@ class Application_LookupItems
         );
     }
     
-    protected $items;
-    
-    public function getItems()
+    /**
+     * @return Application_LookupItems_Item[]
+     * @throws Application_Exception
+     * @throws FileHelper_Exception
+     */
+    public function getItems() : array
     {
         if(isset($this->items)) {
             return $this->items;
         }
         
-        $items = array();
+        $this->items = array();
         
         if(!is_dir($this->folder)) {
             $this->log(sprintf('The driver has no lookup items folder, which is expected at [%s].', $this->folder));
-            return $items;
+            return $this->items;
         }
         
-        $names = AppUtils\FileHelper::createFileFinder($this->folder)
+        $names = FileHelper::createFileFinder($this->folder)
         ->getPHPClassNames();
         
         foreach($names as $name) 
         {
-            $this->log(sprintf('Adding item [%s].', $name));
-            
-            $className = sprintf(
-                '%s_LookupItems_%s',
-                $this->driverID,
-                $name
-            );
-            
-            Application::requireClass($className);
-            
-            $item = new $className();
-            if(!$item instanceof Application_LookupItems_Item) {
-                throw new Application_Exception(
-                    'Invalid lookup item',
-                    sprintf(
-                        'The lookup item class [%s] does not extend the [%s] base class.',
-                        $className,
-                        'Application_LookupItems_Item'
-                    ),
-                    self::ERROR_INVALID_LOOKUP_ITEM_CLASS
-                );
-            }
-            
-            $items[] = $item;
+            $this->registerItem($name);
         }
  
-        $this->items = $items;
-        
-        return $items;
+        return $this->items;
     }
-    
-    protected function log($message)
+
+    /**
+     * @param string $name
+     * @throws Application_Exception
+     */
+    private function registerItem(string $name) : void
     {
-        Application::log(
-            'Lookup items | '.$message    
+        $this->log(sprintf('Adding item [%s].', $name));
+
+        $className = sprintf(
+            '%s_LookupItems_%s',
+            $this->driverID,
+            $name
         );
+
+        $item = new $className();
+        if($item instanceof Application_LookupItems_Item)
+        {
+            $this->items[] = $item;
+            return;
+        }
+
+        throw new Application_Exception(
+            'Invalid lookup item',
+            sprintf(
+                'The lookup item class [%s] does not extend the [%s] base class.',
+                $className,
+                Application_LookupItems_Item::class
+            ),
+            self::ERROR_INVALID_LOOKUP_ITEM_CLASS
+        );
+    }
+
+    /**
+     * @param string $id
+     * @return Application_LookupItems_Item
+     *
+     * @throws Application_Exception|FileHelper_Exception
+     * @see Application_LookupItems::ERROR_ITEM_NOT_FOUND
+     */
+    public function getItemByID(string $id) : Application_LookupItems_Item
+    {
+        $items = $this->getItems();
+
+        foreach ($items as $item)
+        {
+            if($item->getID() === $id) {
+                return $item;
+            }
+        }
+
+        throw new Application_Exception(
+            'Lookup item ID not found',
+            sprintf(
+                'No lookup item with ID [%s] found.',
+                $id
+            ),
+            self::ERROR_ITEM_NOT_FOUND
+        );
+    }
+
+    /**
+     * @param string $id
+     * @return bool
+     * @throws Application_Exception
+     * @throws FileHelper_Exception
+     */
+    public function idExists(string $id) : bool
+    {
+        $items = $this->getItems();
+
+        foreach ($items as $item)
+        {
+            if($item->getID() === $id)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public function getLogIdentifier() : string
+    {
+        return 'LookupItems';
     }
 }
