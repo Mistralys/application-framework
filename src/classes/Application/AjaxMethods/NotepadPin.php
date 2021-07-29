72bd0855eed160540c9643a8394b50224f024cdc
Mistralys <s.mordziol@gmail.com>
2021-07-29 08:13:45 +0200

    AJAX method to pin notes to quickstart.

---
+++ src/classes/Application/AjaxMethods/NotepadPin.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+class Application_AjaxMethods_NotepadPin extends Application_AjaxMethod
+{
+    public function processJSON()
+    {
+        $recent = $this->user->getRecent();
+
+        $recent->pinNote($this->note);
+
+        $this->sendResponse(array('success' => 'yes'));
+    }
+
+    /**
+     * @var Application_User_Notepad
+     */
+    private $notepad;
+
+    /**
+     * @var Application_User_Notepad_Note
+     */
+    private $note;
+
+    protected function validateRequest()
+    {
+        $this->notepad = Application::getUser()->getNotepad();
+
+         $noteID = intval($this->request->registerParam('note_id')->setInteger()->get());
+
+         if($this->notepad->idExists($noteID))
+         {
+             $this->note = $this->notepad->getNoteByID($noteID);
+             return;
+         }
+
+         $this->sendErrorUnknownElement(t('Notepad note'));
+    }
+}
