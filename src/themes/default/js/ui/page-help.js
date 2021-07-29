b2f08303c521236fd4937a8deaae48f89017262d
Mistralys <s.mordziol@gmail.com>
2021-07-29 10:06:18 +0200

    Converted to ES6 class; Added `Toggle()`, `Open()`, `Close()`.

--- src/themes/default/js/ui/page-help.js
+++ src/themes/default/js/ui/page-help.js
@@ -1,10 +1,51 @@
-var UI_PageHelp =
+"use strict";
+
+class UI_PageHelp
 {
-	Start:function()
+	constructor()
 	{
-		$('#page-help .help-opener')
-		.click(function() {
-			$('#page-help .help-contents').toggle();
-		});
+		this.open = false;
+		this.elHelp = null;
 	}
-};
+
+	Start()
+	{
+		var help = this;
+		this.elHelp = $('#page-help .help-contents');
+
+		$('#page-help .help-opener')
+			.click(function() {
+				help.Toggle();
+			});
+	}
+
+	Toggle()
+	{
+		if(this.open)
+		{
+			this.Close();
+		}
+		else
+		{
+			this.Open();
+		}
+	}
+
+	Close()
+	{
+		application.allowAutoRefresh('page-help');
+		this.open = false;
+		this.elHelp.hide();
+
+		UI.ScrollJumpToElement('#page-help');
+	}
+
+	Open()
+	{
+		application.disallowAutoRefresh('page-help');
+		this.open = true;
+		this.elHelp.show();
+	}
+}
+
+window.UI_PageHelp = UI_PageHelp;
