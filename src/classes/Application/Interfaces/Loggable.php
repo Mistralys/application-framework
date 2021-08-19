2e28d9e1f0e0d62d8502c192be1b6b66fa90f928
Mistralys <s.mordziol@gmail.com>
2021-08-19 18:08:38 +0200

    Added `isLoggingEnabled()` and `getLogger()`.

--- src/classes/Application/Interfaces/Loggable.php
+++ src/classes/Application/Interfaces/Loggable.php
@@ -1,9 +1,28 @@
 <?php
+/**
+ * File containing the interface {@see Application_Interfaces_Loggable}.
+ *
+ * @package Application
+ * @subpackage Logger
+ * @see Application_Interfaces_Loggable
+ */
+
+declare(strict_types=1);
 
 /**
+ * Interface for classes that use the loggable trait.
+ *
+ * @package Application
+ * @subpackage Logger
+ * @author Sebastian Mordziol <s.mordziol@mistralys.eu>
+ *
  * @see Application_Traits_Loggable 
  */
 interface Application_Interfaces_Loggable
 {
     public function getLogIdentifier() : string;
+
+    public function isLoggingEnabled() : bool;
+
+    public function getLogger() : Application_Logger;
 }
