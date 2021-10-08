90d132f78c9a7347386adac98051c42b0f80e38c
Mistralys <s.mordziol@gmail.com>
2021-10-08 14:00:38 +0200

    Updated to use `isUnitTestingRunning()`.

--- src/classes/Application/Environments/Environment/Requirement/LocalTest.php
+++ src/classes/Application/Environments/Environment/Requirement/LocalTest.php
@@ -11,29 +11,18 @@ declare(strict_types=1);
 
 /**
  * Check defined APP_TEST_RUNNING constant in config files
- * If it is set as true, this requirement returns true
+ * If it is set as true, this requirement returns true.
  *
  * @package Application
  * @subpackage Environments
  * @author Emre Celebi <emre.celebi@ionos.com>
+ *
+ * @see Application::isUnitTestingRunning()
  */
 class Application_Environments_Environment_Requirement_LocalTest extends Application_Environments_Environment_Requirement
 {
-    /**
-     * @var bool
-     */
-    protected $isLocalTest = false;
-
-    public function __construct()
-    {
-        if (defined('APP_TESTS_RUNNING'))
-        {
-            $this->isLocalTest = APP_TESTS_RUNNING;
-        }
-    }
-
     public function isValid() : bool
     {
-        return $this->isLocalTest === true;
+        return Application::isUnitTestingRunning();
     }
 }
