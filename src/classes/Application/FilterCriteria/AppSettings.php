986bcf320d27c4c248b695a128c153a43da3c641
Mistralys <s.mordziol@gmail.com>
2021-10-15 16:02:10 +0200

    Added new methods.

--- src/classes/Application/FilterCriteria/AppSettings.php
+++ src/classes/Application/FilterCriteria/AppSettings.php
@@ -77,4 +77,14 @@ class Application_FilterCriteria_AppSettings extends Application_FilterCriteria_
             )
         );
     }
+
+    protected function _registerJoins() : void
+    {
+        // TODO: Implement _registerJoins() method.
+    }
+
+    protected function _registerStatementValues(DBHelper_StatementBuilder_ValuesContainer $container) : void
+    {
+        // TODO: Implement _registerStatementValues() method.
+    }
 }
