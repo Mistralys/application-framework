efd792c6e25e4aba438f34ce99996824d9f52796
Mistralys <s.mordziol@gmail.com>
2021-11-02 15:11:06 +0100

    Added some constants and `prependXXX()` methods.

--- src/classes/UI/Bootstrap/BigSelection.php
+++ src/classes/UI/Bootstrap/BigSelection.php
@@ -11,14 +11,19 @@ use AppUtils\Interface_Optionable;
 class UI_Bootstrap_BigSelection extends UI_Bootstrap implements Interface_Optionable
 {
     use Traits_Optionable;
-    
+
+    const OPTION_FILTERING_THRESHOLD = 'filteringThreshold';
+    const OPTION_FILTERING_ENABLED = 'filteringEnabled';
+    const OPTION_EMPTY_MESSAGE = 'emptyMessage';
+    const OPTION_HEIGHT_LIMITED = 'heightLimited';
+
     public function getDefaultOptions() : array
     {
         return array(
-            'emptyMessage' => '',
-            'heightLimited' => false,
-            'filteringEnabled' => false,
-            'filteringThreshold' => 10
+            self::OPTION_EMPTY_MESSAGE => '',
+            self::OPTION_HEIGHT_LIMITED => false,
+            self::OPTION_FILTERING_ENABLED => false,
+            self::OPTION_FILTERING_THRESHOLD => 10
         );
     }
     
@@ -47,7 +52,7 @@ class UI_Bootstrap_BigSelection extends UI_Bootstrap implements Interface_Option
     */
     public function makeHeightLimited(bool $limited=true) : UI_Bootstrap_BigSelection
     {
-        return $this->setOption('heightLimited', $limited);
+        return $this->setOption(self::OPTION_HEIGHT_LIMITED, $limited);
     }
     
    /**
@@ -58,7 +63,7 @@ class UI_Bootstrap_BigSelection extends UI_Bootstrap implements Interface_Option
     */
     public function isHeightLimited() : bool
     {
-        return $this->getBoolOption('heightLimited');
+        return $this->getBoolOption(self::OPTION_HEIGHT_LIMITED);
     }
     
    /**
@@ -69,7 +74,7 @@ class UI_Bootstrap_BigSelection extends UI_Bootstrap implements Interface_Option
     */
     public function setEmptyMessage($message) : UI_Bootstrap_BigSelection
     {
-        return $this->setOption('emptyMessage', toString($message));
+        return $this->setOption(self::OPTION_EMPTY_MESSAGE, toString($message));
     }
     
    /**
@@ -79,7 +84,7 @@ class UI_Bootstrap_BigSelection extends UI_Bootstrap implements Interface_Option
     */
     public function enableFiltering(bool $enable=true) : UI_Bootstrap_BigSelection
     {
-        return $this->setOption('filteringEnabled', $enable);
+        return $this->setOption(self::OPTION_FILTERING_ENABLED, $enable);
     }
     
    /**
@@ -92,7 +97,7 @@ class UI_Bootstrap_BigSelection extends UI_Bootstrap implements Interface_Option
     */
     public function isFilteringEnabled() : bool
     {
-        return $this->getBoolOption('filteringEnabled');
+        return $this->getBoolOption(self::OPTION_FILTERING_ENABLED);
     }
     
    /**
@@ -118,7 +123,7 @@ class UI_Bootstrap_BigSelection extends UI_Bootstrap implements Interface_Option
     
     public function getFilteringThreshold() : int
     {
-        return $this->getIntOption('filteringThreshold');
+        return $this->getIntOption(self::OPTION_FILTERING_THRESHOLD);
     }
     
    /**
@@ -130,12 +135,12 @@ class UI_Bootstrap_BigSelection extends UI_Bootstrap implements Interface_Option
     */
     public function setFilteringThreshold(int $amount) : UI_Bootstrap_BigSelection
     {
-        return $this->setOption('filteringThreshold', $amount);
+        return $this->setOption(self::OPTION_FILTERING_THRESHOLD, $amount);
     }
     
     public function getEmptyMessage() : string
     {
-        $message = $this->getStringOption('emptyMessage');
+        $message = $this->getStringOption(self::OPTION_EMPTY_MESSAGE);
         
         if(!empty($message)) {
             return $message;
@@ -154,15 +159,59 @@ class UI_Bootstrap_BigSelection extends UI_Bootstrap implements Interface_Option
         $this->addClass('size-small');
         return $this;
     }
-    
+
+    // region: Adding items
+
+    /**
+     * @param string|number|UI_Renderable_Interface $label
+     * @return UI_Bootstrap_BigSelection_Item_Regular
+     * @throws Application_Exception
+     * @throws UI_Exception
+     */
+    public function prependItem($label) : UI_Bootstrap_BigSelection_Item_Regular
+    {
+        $item = $this->createRegularItem($label);
+
+        $this->prependChild($item);
+
+        return $item;
+    }
+
+    /**
+     * @param string|number|UI_Renderable_Interface $title
+     * @return UI_Bootstrap_BigSelection_Item_Header
+     * @throws Application_Exception
+     * @throws UI_Exception
+     */
+    public function prependHeader($title) : UI_Bootstrap_BigSelection_Item_Header
+    {
+        $item = $this->createHeaderItem($title);
+
+        $this->prependChild($item);
+
+        return $item;
+    }
+
+    /**
+     * @param string|number|UI_Renderable_Interface $label
+     * @param string $url
+     * @return UI_Bootstrap_BigSelection_Item_Regular
+     * @throws Application_Exception
+     * @throws UI_Exception
+     */
+    public function prependLink($label, string $url) : UI_Bootstrap_BigSelection_Item_Regular
+    {
+        return $this->prependItem($label)->makeLinked($url);
+    }
+
    /**
     * Adds a link to the list. Shortcut for adding the item and setting the link.
     * 
     * @param string|number|UI_Renderable_Interface $label
     * @param string $url
-    * @return UI_Bootstrap_BigSelection_Item
+    * @return UI_Bootstrap_BigSelection_Item_Regular
     */
-    public function addLink($label, string $url)
+    public function addLink($label, string $url) : UI_Bootstrap_BigSelection_Item_Regular
     {
         return $this->addItem($label)->makeLinked($url);
     }
@@ -172,25 +221,64 @@ class UI_Bootstrap_BigSelection extends UI_Bootstrap implements Interface_Option
     * via the returned instance.
     * 
     * @param string|number|UI_Renderable_Interface $label
-    * @return UI_Bootstrap_BigSelection_Item
+    * @return UI_Bootstrap_BigSelection_Item_Regular
     */
-    public function addItem($label) : UI_Bootstrap_BigSelection_Item
+    public function addItem($label) : UI_Bootstrap_BigSelection_Item_Regular
     {
-        $item = new UI_Bootstrap_BigSelection_Item($this->ui);
-        $item->setLabel($label);
-        
+        $item = $this->createRegularItem($label);
+
         $this->appendChild($item);
         
         return $item;
     }
+
+    /**
+     * @param string|number|UI_Renderable_Interface $title
+     * @return UI_Bootstrap_BigSelection_Item_Header
+     * @throws Application_Exception
+     */
+    public function addHeader($title) : UI_Bootstrap_BigSelection_Item_Header
+    {
+        $item = $this->createHeaderItem($title);
+
+        $this->appendChild($item);
+
+        return $item;
+    }
+
+    // endregion
     
    /**
     * Retrieves all items that have been added.
     * 
     * @return UI_Bootstrap_BigSelection_Item[]
     */
-    public function getItems()
+    public function getItems() : array
     {
         return $this->children;
+    }
+
+    /**
+     * @param $label
+     * @return UI_Bootstrap_BigSelection_Item_Regular
+     * @throws UI_Exception
+     */
+    private function createRegularItem($label) : UI_Bootstrap_BigSelection_Item_Regular
+    {
+        $item = new UI_Bootstrap_BigSelection_Item_Regular($this->ui);
+        $item->setLabel($label);
+        return $item;
+    }
+
+    /**
+     * @param $title
+     * @return UI_Bootstrap_BigSelection_Item_Header
+     * @throws UI_Exception
+     */
+    private function createHeaderItem($title) : UI_Bootstrap_BigSelection_Item_Header
+    {
+        $item = new UI_Bootstrap_BigSelection_Item_Header($this->ui);
+        $item->setTitle($title);
+        return $item;
     }
 }
