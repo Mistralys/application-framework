d58fb6ad163f393e6e89c41b2405c4592df73dca
Mistralys <s.mordziol@gmail.com>
2021-11-03 19:11:08 +0100

    Added extended replacement test.

--- tests/testsuites/TypeHinter/ReplaceTests.php
+++ tests/testsuites/TypeHinter/ReplaceTests.php
@@ -46,11 +46,68 @@ final class TypeHinter_ReplaceTests extends ApplicationTestCase
         (new TypeHinter($this->sourceFolder))
             ->setFileSuffix('output')
             ->addMethod('_handleActions', 'bool')
+            ->addReplace($this->defActionSearch, $this->defActionReplace)
             ->process();
 
         $this->assertEquals(
             FileHelper::readContents($this->expectedFile),
             FileHelper::readContents($this->outputFile)
         );
+    }
+
+    private $defActionSearch = <<<EOT
+function getDefaultAction() : string
+    {
+        return null;
+    }
+EOT;
+
+    private $defActionReplace = <<<EOT
+function getDefaultAction() : string
+    {
+        return '';
+    }
+EOT;
+
+    private $defSubmodeSearch = <<<EOT
+function getDefaultSubmode() : string
+    {
+        return null;
+    }
+EOT;
+
+    private $defSubmodeReplace = <<<EOT
+function getDefaultSubmode() : string
+    {
+        return '';
+    }
+EOT;
+
+    public function test_replaceEverywhere() : void
+    {
+        $this->markTestSkipped();
+
+        $hinter = new TypeHinter(APP_INSTALL_FOLDER);
+
+        $hinter
+            ->addMethod('_handleActions', 'bool')
+            ->addMethod('_handleSubactions', 'void')
+            ->addMethod('_handleSubnavigation', 'void')
+            ->addMethod('_handleBreadcrumb', 'void')
+            ->addMethod('_handleSidebar', 'void')
+            ->addMethod('_handleBreadcrumb', 'void')
+            ->addMethod('_handleHelp', 'void')
+            ->addMethod('_initSteps', 'void')
+            ->addMethod('getURLName', 'string')
+            ->addMethod('getRecordMissingURL', 'string')
+            ->addMethod('getDefaultSubmode', 'string')
+            ->addMethod('getDefaultAction', 'string')
+            ->addMethod('getNavigationTitle', 'string')
+            ->addMethod('isUserAllowed', 'bool')
+            ->addReplace($this->defActionSearch, $this->defActionReplace)
+            ->addReplace($this->defSubmodeSearch, $this->defSubmodeReplace)
+            ->process();
+
+        $this->addToAssertionCount(1);
     }
 }
