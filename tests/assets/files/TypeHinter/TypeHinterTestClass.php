4c48be9175d5da89e47d5dd4bab816521b096276
Mistralys <s.mordziol@gmail.com>
2021-11-03 18:21:50 +0100

    Added type hinter tests.

---
+++ tests/assets/files/TypeHinter/TypeHinterTestClass.php
@@ -0,0 +1,23 @@
+<?php
+/*
+
+class TypeHinterTestClass
+{
+    public function _handleActions()
+    {
+
+    }
+
+    public function _handleActions(){}
+
+    public function _handleActions(?array $param=null)
+    {
+
+    }
+
+    public function _handleActions() : bool
+    {
+
+    }
+}
+*/
---
+++ tests/testsuites/TypeHinter/ReplaceTests.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+use AppUtils\FileHelper;
+
+final class TypeHinter_ReplaceTests extends ApplicationTestCase
+{
+    /**
+     * @var string
+     */
+    private $inputFile;
+
+    /**
+     * @var string
+     */
+    private $outputFile;
+
+
+    /**
+     * @var string
+     */
+    private $expectedFile;
+    /**
+     * @var string
+     */
+    private $sourceFolder;
+
+    protected function setUp() : void
+    {
+        parent::setUp();
+
+        $this->sourceFolder = TESTS_ROOT.'/assets/files/TypeHinter';
+        $this->inputFile = $this->sourceFolder.'/TypeHinterTestClass.php';
+        $this->outputFile = $this->sourceFolder.'/TypeHinterTestClass.php.output';
+        $this->expectedFile = $this->sourceFolder.'/TypeHinterTestClass.php.expected';
+    }
+
+    public function test_findFiles() : void
+    {
+        $this->assertCount(1, (new TypeHinter($this->sourceFolder))->getFilesList());
+    }
+
+    public function test_replace() : void
+    {
+        (new TypeHinter($this->sourceFolder))
+            ->setFileSuffix('output')
+            ->addMethod('_handleActions', 'bool')
+            ->process();
+
+        $this->assertEquals(
+            FileHelper::readContents($this->expectedFile),
+            FileHelper::readContents($this->outputFile)
+        );
+    }
+}
