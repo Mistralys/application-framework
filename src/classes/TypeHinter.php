7b93603646a48a6f6ca9c66764f950b7d6f4ad68
Mistralys <s.mordziol@gmail.com>
2021-11-04 14:59:19 +0100

    Added logging.

--- src/classes/TypeHinter.php
+++ src/classes/TypeHinter.php
@@ -3,16 +3,19 @@
 declare(strict_types=1);
 
 use AppUtils\FileHelper;
+use AppUtils\FileHelper_PHPClassInfo;
 
-class TypeHinter
+class TypeHinter implements Application_Interfaces_Loggable
 {
+    use Application_Traits_Loggable;
+
     /**
      * @var string
      */
     private $rootPath;
 
     /**
-     * @var array<string,array{regex:string,type:string}>
+     * @var array<int,array{name:string,regex:string,type:string,instanceOf:string}>
      */
     private $methods = array();
 
@@ -49,14 +52,16 @@ class TypeHinter
         return $this;
     }
 
-    public function addMethod(string $methodName, string $type) : TypeHinter
+    public function addMethod(string $methodName, string $type, string $classSearch='') : TypeHinter
     {
-        $this->methods[$methodName] = array(
+        $this->methods[] = array(
+            'name' => $methodName,
             'regex' => sprintf(
                 '/function[ ]*%s\(([^{]*)\)\s*{/sU',
                 preg_quote($methodName, '/')
             ),
-            'type' => $type
+            'type' => $type,
+            'instanceOf' => $classSearch
         );
         return $this;
     }
@@ -91,29 +96,65 @@ class TypeHinter
 
     private function processFile(string $file) : void
     {
+        $info = new FileHelper_PHPClassInfo($file);
+
+        if(!$info->hasClasses())
+        {
+            $this->log('Ignoring file, no classes found: [%s].', $file);
+            return;
+        }
+
+        $classes = $info->getClassNames();
+        $class = array_shift($classes);
         $content = FileHelper::readContents($file);
 
-        foreach($this->methodNames as $name)
+        $this->log('Class [%s] | Processing methods.', $class);
+
+        foreach($this->methods as $def)
         {
-            $content = $this->replaceMethod($name, $content);
+            $content = $this->replaceMethod(
+                $def['name'],
+                $def['regex'],
+                $def['type'],
+                $content,
+                $class,
+                $def['instanceOf']
+            );
         }
 
         FileHelper::saveFile($file.$this->fileSuffix, $content);
     }
 
-    private function replaceMethod(string $name, string $content) : string
+    private function replaceMethod(string $name, string $regex, string $type, string $content, string $class, string $classSearch) : string
     {
+        // If a search term is specified, verify that the class name
+        // contains the term.
+        if(!empty($classSearch) && stripos($class, $classSearch) === false)
+        {
+            $this->log('Class [%s] | Method [%s] | Search term [%s] not found.', $class, $name, $classSearch);
+            return $content;
+        }
+
         $matches = array();
-        preg_match_all($this->methods[$name]['regex'], $content, $matches);
+        preg_match_all($regex, $content, $matches);
 
         $replaces = array();
 
         foreach($matches[0] as $idx => $matchedText)
         {
-            $replaces[$matchedText] = $this->generateMethod($name, $this->methods[$name]['type'], $matches[1][$idx]);
+            $replaces[$matchedText] = $this->generateMethod($name, $type, $matches[1][$idx]);
         }
 
-        $content = str_replace(array_keys($replaces), array_values($replaces), $content);
+        $found = count($replaces);
+        if($found > 0)
+        {
+            $this->log('Class [%s] | Method [%s] | Found [%s] matches.', $class, $name, $found);
+            $content = str_replace(array_keys($replaces), array_values($replaces), $content);
+        }
+        else
+        {
+            $this->log('Class [%s] | Method [%s] | No replace matches found.', $class,$name);
+        }
 
         return str_replace(array_keys($this->replaces), array_values($this->replaces), $content);
     }
@@ -127,5 +168,10 @@ class TypeHinter
             $type,
             PHP_EOL
         );
+    }
+
+    public function getLogIdentifier() : string
+    {
+        return 'TypeHinter';
     }
 }
