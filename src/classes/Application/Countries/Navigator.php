7368e2eaaa7d3eea104488b04bd30142fc40c31d
Mistralys <s.mordziol@gmail.com>
2021-11-10 13:05:54 +0100

    Added support for saving the selected country.

--- src/classes/Application/Countries/Navigator.php
+++ src/classes/Application/Countries/Navigator.php
@@ -1,5 +1,22 @@
 <?php
+/**
+ * File containing the class {@see Application_Countries_Navigator}.
+ *
+ * @package Application
+ * @subpackage Countries
+ * @see Application_Countries_Navigator
+ */
 
+declare(strict_types=1);
+
+/**
+ * Utility class used to render a country navigation element,
+ * using a button bar to easily select a language.
+ *
+ * @package Application
+ * @subpackage Countries
+ * @author Sebastian Mordziol <s.mordziol@mistralys.eu>
+ */
 class Application_Countries_Navigator extends UI_Renderable
 {
    /**
@@ -21,11 +38,32 @@ class Application_Countries_Navigator extends UI_Renderable
     * @var Application_Countries_Country[]
     */
     protected $countries;
-    
+
+    /**
+     * @var bool
+     */
     protected $autoSelect = true;
-    
+
+    /**
+     * @var array<string,scalar>
+     */
     protected $urlParams = array();
-    
+
+    /**
+     * @var bool
+     */
+    private $autoSelected = false;
+
+    /**
+     * @var bool
+     */
+    private $savingEnabled = false;
+
+    /**
+     * @var string
+     */
+    private $storageName;
+
     public function __construct(Application_Countries $collection)
     {
         parent::__construct();
@@ -34,17 +72,20 @@ class Application_Countries_Navigator extends UI_Renderable
         $this->filters = $collection->getFilterCriteria();
     }
     
-    public function getCollection()
+    public function getCollection() : Application_Countries
     {
         return $this->collection;
     }
     
-    public function getFilterCriteria()
+    public function getFilterCriteria() : Application_Countries_FilterCriteria
     {
         return $this->filters;
     }
-    
-    public function getCountries()
+
+    /**
+     * @return Application_Countries_Country[]
+     */
+    public function getCountries() : array
     {
         if(!isset($this->countries)) {
             $this->countries = $this->filters->getItemsObjects();
@@ -53,62 +94,111 @@ class Application_Countries_Navigator extends UI_Renderable
         return $this->countries;
     }
     
-    public function setAutoSelect($enabled=true)
+    public function setAutoSelect(bool $enabled=true) : Application_Countries_Navigator
     {
         $this->autoSelect = $enabled;
         return $this;
     }
     
-    public function setActiveCountry(Application_Countries_Country $country)
+    public function setActiveCountry(Application_Countries_Country $country) : Application_Countries_Navigator
     {
         $this->activeCountry = $country;
         return $this;
     }
-    
-    public function setURLParam($name, $value)
+
+    /**
+     * @param string $name
+     * @param scalar $value
+     * @return $this
+     */
+    public function setURLParam(string $name, $value) : Application_Countries_Navigator
     {
-        $this->urlParams[$name] = $value;
+        $this->urlParams[$name] = (string)$value;
         return $this;
     }
     
-    public function setURLParamsByArea(Application_Admin_Skeleton $area)
+    public function setURLParamsByArea(Application_Admin_Skeleton $area) : Application_Countries_Navigator
     {
         return $this->setURLParams($area->getPageParams());
     }
-    
-    public function setURLParams($params)
+
+    /**
+     * @param array<string,scalar> $params
+     * @return $this
+     */
+    public function setURLParams(array $params) : Application_Countries_Navigator
     {
-        foreach($params as $name => $value) {
+        foreach($params as $name => $value)
+        {
             $this->setURLParam($name, $value);
         }
         
         return $this;
     }
-    
-    protected $autoSelected = false;
-    
-    protected function autoSelect()
+
+    private function autoSelect() : void
     {
-        if($this->autoSelected || isset($this->activeCountry)) {
+        if($this->autoSelected || isset($this->activeCountry))
+        {
             return;
         }
-        
+
+        // A country has been specified in the request, use that.
         $this->activeCountry = $this->collection->getByRequest();
-        if(isset($this->activeCountry)) {
+        if(isset($this->activeCountry))
+        {
+            $this->saveActiveCountry($this->activeCountry);
             return;
         }
-        
+
+        // Use the saved country, if any.
+        $country = $this->getSavedCountry();
+        if($country !== null)
+        {
+            $this->activeCountry = $country;
+            return;
+        }
+
+        // Use the first country in the list as final fallback.
         $countries = $this->getCountries();
         $this->activeCountry = array_shift($countries);
     }
+
+    private function saveActiveCountry(Application_Countries_Country $country) : void
+    {
+        if($this->savingEnabled === true)
+        {
+            Application_Driver::setSetting($this->storageName, (string)$country->getID());
+        }
+    }
+
+    private function getSavedCountry() : ?Application_Countries_Country
+    {
+        if($this->savingEnabled === false)
+        {
+            return null;
+        }
+
+        $id = (int)Application_Driver::getSetting($this->storageName);
+
+        if($this->collection->idExists($id))
+        {
+            return $this->collection->getByID($id);
+        }
+
+        return null;
+    }
     
-    public function getActiveCountry()
+    public function getActiveCountry() : Application_Countries_Country
     {
         $this->autoSelect();
         
         return $this->activeCountry;
     }
-    
+
+    /**
+     * @return string
+     */
     protected function _render()
     {
         $this->autoSelect();
@@ -137,7 +227,7 @@ class Application_Countries_Navigator extends UI_Renderable
             $btn = UI::button(strtoupper($country->getISO()))
             ->link($request->buildURL($params));
             
-            if($id == $activeID) {
+            if($id === $activeID) {
                 $btn->makePrimary();
             }
             
@@ -145,5 +235,20 @@ class Application_Countries_Navigator extends UI_Renderable
         }
         
         return $group->render();
+    }
+
+    /**
+     * Enables storing the active country ID, to restore it
+     * automatically if no country is specifically selected
+     * in the request parameters.
+     *
+     * @param string $storageName Used to identify the navigator: all instances using the same name share the stored country setting.
+     * @return $this
+     */
+    public function enableCountryStorage(string $storageName) : Application_Countries_Navigator
+    {
+        $this->savingEnabled = true;
+        $this->storageName = 'countries_navigator_'.$storageName;
+        return $this;
     }
 }
