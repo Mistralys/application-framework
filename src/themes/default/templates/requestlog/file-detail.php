5b045595358e3074595cf6d6ce7b7eeae06d124f
Mistralys <s.mordziol@gmail.com>
2022-02-04 09:55:41 +0100

    Added the file details property table.

--- src/themes/default/templates/requestlog/file-detail.php
+++ src/themes/default/templates/requestlog/file-detail.php
@@ -28,6 +28,8 @@ class template_default_requestlog_file_detail extends UI_Page_Template_Custom
         OutputBuffering::start();
         echo $this->renderTemplate('requestlog/header');
 
+        $this->displayProperties();
+
         echo $this->createSection()
             ->collapse()
             ->setTitle(t('Request variables'))
@@ -44,6 +46,40 @@ class template_default_requestlog_file_detail extends UI_Page_Template_Custom
             ->setContent('<pre>'.$this->info->getLog().'</pre>');
 
         echo $this->renderCleanFrame(OutputBuffering::get());
+    }
+
+    private function displayProperties() : void
+    {
+        $grid = $this->ui->createPropertiesGrid();
+        $grid->addByteSize(t('Log size'), $this->info->getLogSize());
+        $grid->add(t('Session ID'), $this->info->getSessionID());
+        $grid->add(t('Request ID'), $this->info->getRequestID());
+        $grid->add(t('User'), $this->info->getUserName());
+        $grid->add(t('Time'), $this->info->getTime()->getISODate());
+        $grid->add(t('Request duration'), number_format($this->info->getDuration(), 2));
+
+        $grid->addHeader(t('Server details'));
+
+        $grid->add(t('PHP Version'), $this->info->getPHPVersion());
+        $grid->add(t('Operating system'), $this->info->getOSFamily())
+            ->setComment($this->info->getOS());
+
+        $grid->addHeader(t('Database queries'));
+        $grid->addAmount(t('Total queries'), $this->info->getQueryCount());
+        $grid->addAmount(t('Read queries'), $this->info->getQueryReadCount());
+        $grid->addAmount(t('Write queries'), $this->info->getQueryWriteCount());
+
+        $grid->addHeader(t('Environment'));
+        $grid->addBoolean(t('Developer mode'), $this->info->isDeveloperMode());
+        $grid->addBoolean(t('UI enabled'), $this->info->isUIEnabled());
+        $grid->addBoolean(t('Database enabled'), $this->info->isDatabaseEnabled());
+        $grid->addBoolean(t('Authentication enabled'), $this->info->isAuthEnabled());
+        $grid->addBoolean(t('Session enabled'), $this->info->isNoSession());
+        $grid->addBoolean(t('Session simulated'), $this->info->isSimulatedSession());
+        $grid->addBoolean(t('Demo mode'), $this->info->isDemoMode());
+        $grid->addBoolean(t('Command line mode'), $this->info->isCLI());
+
+        $grid->display();
     }
 
     /**
