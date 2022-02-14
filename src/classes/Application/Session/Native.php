218f3864e7fe6e27e5486d5e6859e5d3fe7cf8a5
Mistralys <s.mordziol@gmail.com>
2022-02-14 13:29:52 +0100

    Added exception when session cannot be started.

--- src/classes/Application/Session/Native.php
+++ src/classes/Application/Session/Native.php
@@ -3,27 +3,102 @@
  * File containing the {@link Application_Session_Native} class.
  *
  * @package Application
- * @subpackage Core
+ * @subpackage Sessions
  * @see Application_Session_Native
  */
+
+use AppUtils\NamedClosure;
 
 /**
  * Session implementation using the native PHP session functions.
  *
  * @package Application
- * @subpackage Core
+ * @subpackage Sessions
  * @author Sebastian Mordziol <s.mordziol@mistralys.eu>
  */
 abstract class Application_Session_Native extends Application_Session_Base
 {
+    public const ERROR_CANNOT_START_SESSION = 101001;
+
+    public const SESSION_HEADERS_ALREADY_SENT = 8;
+
+    /**
+     * @var array<string,string>
+     */
+    private static $options = array();
+
     protected function start(): void
     {
-        session_start();
+        // Temporarily set an error handler to catch session
+        // initialization errors, so they can be converted
+        // to an exception.
+        set_error_handler(NamedClosure::fromClosure(
+            Closure::fromCallable(array($this, 'callback_sessionStartError')),
+            array($this, 'callback_sessionStartError')
+        ));
+
+        session_start(self::$options);
+
+        restore_error_handler();
+    }
+
+    private function callback_sessionStartError(int $code, string $msg, string $file, int $line) : bool
+    {
+        $ex = new Application_Session_Exception(
+            'Session handling initialization failed',
+            'The session_start() function failed.',
+            self::ERROR_CANNOT_START_SESSION
+        );
+
+        $ex->setErrorDetails($code, $msg, $file, $line);
+
+        throw $ex;
     }
 
     public function getID() : string
     {
         return session_id();
+    }
+
+    /**
+     * Sets a session option.
+     *
+     * NOTE: Must be used before the session is started.
+     * Has no effect afterwards.
+     *
+     * Example:
+     *
+     * <pre>
+     * Application_Session_Native::setOption('cookie_lifetime', '60');
+     * </pre>
+     *
+     * @param string $name
+     * @param string $value
+     * @return void
+     *
+     * @link https://www.php.net/manual/en/session.configuration.php
+     */
+    public static function setOption(string $name, string $value) : void
+    {
+        self::$options[$name] = $value;
+    }
+
+    /**
+     * Retrieves the value of a session option.
+     *
+     * @param string $name The name, with or without `session.` prefix, as they are stored in `php.ini`.
+     * @return string An empty string is returned if it does not exist.
+     */
+    public static function getOption(string $name) : string
+    {
+        $value = ini_get('session.'.str_replace('session.', '', $name));
+
+        if($value !== false)
+        {
+            return $value;
+        }
+
+        return '';
     }
 
     protected function handleLogout() : void
@@ -32,7 +107,7 @@ abstract class Application_Session_Native extends Application_Session_Base
         session_destroy();
     }
 
-    public function getValue($name, $default = null)
+    public function getValue(string $name, $default = null)
     {
         if (isset($_SESSION[$name])) {
             return $_SESSION[$name];
@@ -41,17 +116,17 @@ abstract class Application_Session_Native extends Application_Session_Base
         return $default;
     }
 
-    public function setValue($name, $value)
+    public function setValue(string $name, $value) : void
     {
         $_SESSION[$name] = $value;
     }
 
-    public function valueExists($name)
+    public function valueExists(string $name) : bool
     {
         return isset($_SESSION[$name]);
     }
 
-    public function unsetValue($name)
+    public function unsetValue(string $name) : void
     {
         if ($this->valueExists($name)) {
             unset($_SESSION[$name]);
