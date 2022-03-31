079b7be1079da4ebc895ea8d679406a679ca38f4
Mistralys <s.mordziol@gmail.com>
2022-03-31 10:58:51 +0200

    Added `isSubmittable()`.

--- src/classes/UI/Interfaces/ClientConfirmable.php
+++ src/classes/UI/Interfaces/ClientConfirmable.php
@@ -8,17 +8,19 @@ interface UI_Interfaces_ClientConfirmable
      * Adds a confirmation message to the element, as a dialog that is shown
      * before the action is executed.
      *
-     * @param scalar|UI_Renderable_Interface $message
-     * @param boolean $withInput Whether to have the user confirm the operation by typing a confirm string.
+     * @param string|number|UI_Renderable_Interface|NULL $message
+     * @param boolean $withInput Whether to have the user confirm the operation by typing a confirmation string.
      * @return $this
      */
-    public function makeConfirm($message, bool $withInput=false);
+    public function makeConfirm($message, bool $withInput=false) : self;
 
     public function getConfirmMessage() : UI_ClientConfirmable_Message;
 
     public function getURL() : string;
 
     public function isClickable() : bool;
+
+    public function isSubmittable() : bool;
 
     public function isLinked() : bool;
 
