386d37fde459572c0a200b18e1e67a92e31bf0e6
Mistralys <s.mordziol@gmail.com>
2022-04-08 16:37:52 +0200

    Added tests.

---
+++ tests/testsuites/Application/ClassTypeNameTests.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * @package Application
+ * @subpackage UnitTests
+ */
+
+declare(strict_types=1);
+
+namespace testsuites\Application;
+
+use ApplicationTestCase;
+
+/**
+ * @package Application
+ * @subpackage UnitTests
+ */
+final class ClassTypeNameTests extends ApplicationTestCase
+{
+    public function test_simpleName() : void
+    {
+        $this->assertSame(
+            'Simple',
+            getClassTypeName('Simple')
+        );
+    }
+
+    public function test_legacyName() : void
+    {
+        $this->assertSame(
+            'Name',
+            getClassTypeName('Legacy_Class_Name')
+        );
+    }
+
+    public function test_namespacedName() : void
+    {
+        $this->assertSame(
+            'ClassName',
+            getClassTypeName('Namespace\SubPath\ClassName')
+        );
+    }
+
+    public function test_namespacedLegacyName() : void
+    {
+        $this->assertSame(
+            'ClassName',
+            getClassTypeName('Namespace\SubPath\Legacy_Class_ClassName')
+        );
+    }
+}
