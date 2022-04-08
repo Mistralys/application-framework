c79ac9f65b059e5e801d83cb1a6ee4069bf08629
Mistralys <s.mordziol@gmail.com>
2022-04-08 16:37:07 +0200

    Added some tests.

--- tests/testsuites/Application/WhatsNewTests.php
+++ tests/testsuites/Application/WhatsNewTests.php
@@ -1,18 +1,77 @@
 <?php
+/**
+ * @package Application
+ * @subpackage UnitTests
+ */
 
 declare(strict_types=1);
 
 namespace testsuites\Application;
 
+use Application\WhatsNew;
 use ApplicationTestCase;
+use AppUtils\FileHelper;
 use TestDriver;
+use Application\WhatsNew\XMLFileWriter;
 
+/**
+ * @package Application
+ * @subpackage UnitTests
+ */
 class WhatsNewTests extends ApplicationTestCase
 {
+    // region: _Tests
+
     public function test_create() : void
     {
         $new = TestDriver::createWhatsnew();
 
-        $this->assertCount(1, $new->getVersions());
+        $versions = $new->getVersions();
+
+        $this->assertCount(1, $versions);
+
+        $version = $versions[0];
+
+        $this->assertSame('1.0.0', $version->getNumber());
+        $this->assertCount(3, $version->getLanguages());
     }
+
+    public function test_write() : void
+    {
+        $whatsNew = TestDriver::createWhatsnew();
+
+        $whatsNew->addVersion('5.0.0');
+
+        $writer = new XMLFileWriter($whatsNew);
+        $writer->write($this->outputFile);
+
+        $updated = new WhatsNew($this->outputFile);
+
+        $this->assertCount(2, $updated->getVersions());
+    }
+
+    // endregion
+
+    // region: Support methods
+
+    private string $outputFile;
+
+    protected function setUp() : void
+    {
+        parent::setUp();
+
+        $this->outputFile = __DIR__.'/../../files/whats-new-edited.xml';
+    }
+
+    protected function tearDown() : void
+    {
+        parent::tearDown();
+
+        if(file_exists($this->outputFile))
+        {
+            FileHelper::deleteFile($this->outputFile);
+        }
+    }
+
+    // endregion
 }
