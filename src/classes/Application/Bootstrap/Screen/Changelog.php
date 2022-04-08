cb2c9500e41c2e990db1a044b7eab92d8913c2fe
Mistralys <s.mordziol@gmail.com>
2022-04-08 16:33:37 +0200

    Moved the rendering to a separate class.

--- src/classes/Application/Bootstrap/Screen/Changelog.php
+++ src/classes/Application/Bootstrap/Screen/Changelog.php
@@ -9,62 +9,21 @@ class Application_Bootstrap_Screen_Changelog extends Application_Bootstrap_Scree
     */
     protected string $langID = 'dev';
     
-    public function getDispatcher()
+    public function getDispatcher() : string
     {
         return 'changelog.php';
     }
     
-    protected function _boot()
+    protected function _boot() : void
     {
         $this->enableScriptMode();
         $this->disableAuthentication();
         $this->createEnvironment();
-        
-        $versions = Application_Driver::createWhatsnew()->getVersionsByLanguage($this->langID);
-        
+
         header('Content-Type:text/plain; encoding=utf-8');
         
-        $separator = str_repeat('-', 65).PHP_EOL;
+        echo Application_Driver::createWhatsnew()->toPlainText($this->langID);
 
-        foreach($versions as $version) 
-        {
-            $lang = $version->getLanguage($this->langID);
-            
-            echo $separator;
-            echo 'V'.$version->getNumber().PHP_EOL;
-            echo $separator;
-            echo PHP_EOL;
-            
-            $categories = $lang->getCategories();
-            
-            foreach($categories as $category) 
-            {
-                $entries = $category->getItems();
-                $catLabel = $category->getLabel();
-                
-                foreach($entries as $entry) 
-                {
-                    echo '- '.$catLabel;
-                    
-                    if($entry->hasIssue()) {
-                        echo ' - '.$entry->getIssue();
-                    }
-                 
-                    $text = $entry->getPlainText();
-                    
-                    echo ': '.$text;
-                    
-                    if($entry->hasAuthor()) {
-                        echo ' ('.$entry->getAuthor().')';
-                    }
-                    
-                    echo PHP_EOL;
-                }
-            }
-            
-            echo PHP_EOL;
-        }
-        
         Application::exit('Shown the changelog.');
     }
 }
---
+++ src/classes/Application/WhatsNew/PlainTextRenderer.php
@@ -0,0 +1,119 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\WhatsNew;
+
+use Application\WhatsNew;
+use Application\WhatsNew\AppVersion\CategoryItem;
+use Application\WhatsNew\AppVersion\LanguageCategory;
+
+/**
+ * Renders a what's new versions list to a plain text
+ * version (for a specific target language).
+ *
+ * @package Application
+ * @subpackage WhatsNew
+ * @author Sebastian Mordziol <s.mordziol@mistralys.eu>
+ */
+class PlainTextRenderer
+{
+    private WhatsNew $whatsNew;
+
+    /**
+     * @var string[]
+     */
+    private array $lines = array();
+    private string $separator;
+
+    public function __construct(WhatsNew $whatsNew)
+    {
+        $this->whatsNew = $whatsNew;
+        $this->separator = str_repeat('-', 65);
+    }
+
+    public function render(string $langID) : string
+    {
+        $this->lines = array();
+
+        $versions = $this->whatsNew->getVersionsByLanguage($langID);
+
+        echo count($versions);
+
+        foreach($versions as $version)
+        {
+            $this->renderVersion($version, $langID);
+        }
+
+        return implode(PHP_EOL, $this->lines);
+    }
+
+    private function renderVersion(AppVersion $version, string $langID) : void
+    {
+        $lang = $version->getLanguage($langID);
+
+        $this->addSeparator();
+        $this->addLine('V'.$version->getNumber());
+        $this->addSeparator();
+
+        $this->addNewline();
+
+        $categories = $lang->getCategories();
+
+        foreach($categories as $category)
+        {
+            $this->renderCategory($category);
+        }
+
+        $this->addNewline();
+    }
+
+    private function renderCategory(LanguageCategory $category) : void
+    {
+        $entries = $category->getItems();
+
+        foreach($entries as $entry)
+        {
+            $this->renderEntry($entry, $category);
+        }
+    }
+
+    private function addSeparator() : void
+    {
+        $this->addLine($this->separator);
+    }
+
+    private function addNewline() : void
+    {
+        $this->addLine('');
+    }
+
+    private function addLine(string $line) : void
+    {
+        $this->lines[] = $line;
+    }
+
+    private function renderEntry(CategoryItem $entry, LanguageCategory $category) : void
+    {
+        $line = sb()
+            ->add('-')
+            ->add($category->getLabel());
+
+        if($entry->hasIssue())
+        {
+            $line
+                ->add('-')
+                ->add($entry->getIssue());
+        }
+
+        $line
+            ->add(':')
+            ->add($entry->getPlainText());
+
+        if($entry->hasAuthor()) {
+            $line->parentheses($entry->getAuthor());
+        }
+
+        $this->addLine((string)$line);
+    }
+}
