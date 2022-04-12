34b7e468cfe85f309b9e9570fc2284cc8732c9a2
Mistralys <s.mordziol@gmail.com>
2022-04-12 17:45:44 +0200

    Added version getter methods.

--- src/classes/Application/WhatsNew.php
+++ src/classes/Application/WhatsNew.php
@@ -11,6 +11,7 @@ declare(strict_types=1);
 
 namespace Application;
 
+use Application\WhatsNew\WhatsNewException;
 use Application\WhatsNew\XMLFileWriter;
 use Application\WhatsNew\PlainTextRenderer;
 use Application\WhatsNew\XMLRenderer;
@@ -24,7 +25,6 @@ use Application_Exception;
 use Application\WhatsNew\AppVersion;
 use AppUtils\FileHelper;
 use Parsedown;
-use const APP_ROOT;
 
 /**
  * Handles reading the application's `WHATSNEW.xml` file, as
@@ -39,6 +39,7 @@ class WhatsNew
 {
     public const ERROR_WHATS_NEW_FILE_NOT_FOUND = 30001;
     public const ERROR_COULD_NOT_PARSE_XML = 30002;
+    public const ERROR_VERSION_NUMBER_NOT_FOUND = 30003;
 
     protected string $file;
     private Parsedown $parseDown;
@@ -48,13 +49,8 @@ class WhatsNew
      */
     protected array $versions = array();
 
-    public function __construct(string $sourceFile='')
+    public function __construct(string $sourceFile)
     {
-        if(empty($sourceFile))
-        {
-            $sourceFile = APP_ROOT . '/WHATSNEW.xml';
-        }
-
         $this->file = $sourceFile;
         $this->parseDown = new Parsedown();
 
@@ -188,12 +184,10 @@ class WhatsNew
 
         array_unshift($this->versions, $instance);
 
-        $this->writeToDisk();
-
         return $instance;
     }
 
-    private function writeToDisk() : void
+    public function writeToDisk() : void
     {
         (new XMLFileWriter($this))->write($this->getPath());
     }
@@ -206,5 +200,57 @@ class WhatsNew
     public function toXML() : string
     {
         return (new XMLRenderer($this))->render();
+    }
+
+    public function getByRequest() : ?AppVersion
+    {
+        $number = (string)Application_Driver::getInstance()
+            ->getRequest()
+            ->registerParam(AppVersion::REQUEST_PARAM_NUMBER)
+            ->get();
+
+        if($this->numberExists($number))
+        {
+            return $this->getByNumber($number);
+        }
+
+        return null;
+    }
+
+    public function getByNumber(string $number) : AppVersion
+    {
+        $versions = $this->getVersions();
+
+        foreach($versions as $version)
+        {
+            if($version->getNumber() === $number)
+            {
+                return $version;
+            }
+        }
+
+        throw new WhatsNewException(
+            'Application version not found by number.',
+            sprintf(
+                'Tried getting version number [%s].',
+                $number
+            ),
+            self::ERROR_VERSION_NUMBER_NOT_FOUND
+        );
+    }
+
+    public function numberExists(string $number) : bool
+    {
+        $versions = $this->getVersions();
+
+        foreach($versions as $version)
+        {
+            if($version->getNumber() === $number)
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
