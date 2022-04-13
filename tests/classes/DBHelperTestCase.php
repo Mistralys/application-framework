869db46ff60f7e220ed759af6439cbe2e129d92d
Mistralys <s.mordziol@gmail.com>
2022-04-13 12:23:05 +0200

    Using the new test DB collection constants.

--- tests/classes/DBHelperTestCase.php
+++ tests/classes/DBHelperTestCase.php
@@ -1,14 +1,22 @@
 <?php
+/**
+ * @package Application
+ * @subpackage UnitTests
+ */
 
 declare(strict_types=1);
 
+/**
+ * @package Application
+ * @subpackage UnitTests
+ */
 abstract class DBHelperTestCase extends ApplicationTestCase
 {
-    public const TEST_RECORDS_PRIMARY = 'record_id';
-    public const TEST_RECORDS_COL_LABEL = 'label';
-    public const TEST_RECORDS_COL_ALIAS = 'alias';
-    public const TEST_RECORDS_DATA_TABLE = 'test_records_data';
-    public const TEST_RECORDS_TABLE = 'test_records';
+    public const TEST_RECORDS_PRIMARY = TestDriver_TestDBCollection::PRIMARY_NAME;
+    public const TEST_RECORDS_COL_LABEL = TestDriver_TestDBCollection_TestDBRecord::COL_LABEL;
+    public const TEST_RECORDS_COL_ALIAS = TestDriver_TestDBCollection_TestDBRecord::COL_ALIAS;
+    public const TEST_RECORDS_DATA_TABLE = TestDriver_TestDBCollection::TABLE_NAME_DATA;
+    public const TEST_RECORDS_TABLE = TestDriver_TestDBCollection::TABLE_NAME;
 
     protected function setUp() : void
     {
