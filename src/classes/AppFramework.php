1b3d28d4a759d7bef0d0c7324b48eb69556ef9c6
Mistralys <s.mordziol@gmail.com>
2022-05-19 10:18:10 +0200

    Added the utiity class.

---
+++ src/classes/AppFramework.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mistralys\AppFramework;
+
+use Mistralys\VersionParser\VersionParser;
+
+class AppFramework
+{
+    private const GITHUB_URL = 'https://github.com/Mistralys/application-framework.git';
+
+    private static ?AppFramework $instance = null;
+    private string $installFolder;
+    private ?VersionParser $version = null;
+
+    private function __construct()
+    {
+        $this->installFolder = __DIR__.'/../../';
+    }
+
+    public function getInstallFolder(): string
+    {
+        return $this->installFolder;
+    }
+
+    public function getGithubURL() : string
+    {
+        return self::GITHUB_URL;
+    }
+
+    public function getVersion() : VersionParser
+    {
+        if(!isset($this->version)) {
+            $this->version = VersionParser::create($this->getVersionPath());
+        }
+
+        return $this->version;
+    }
+
+    public function getVersionPath() : string
+    {
+        return __DIR__.'/../../VERSION';
+    }
+
+    public static function getInstance() : AppFramework
+    {
+        if(!isset(self::$instance))
+        {
+            self::$instance = new AppFramework();
+        }
+
+        return self::$instance;
+    }
+}
