7940239c331b41b56a8383dbcda1afd39da204f0
Mistralys <s.mordziol@gmail.com>
2022-06-09 13:13:48 +0200

    Fixed a return type.

--- src/classes/Application/Traits/Admin/RevisionableList.php
+++ src/classes/Application/Traits/Admin/RevisionableList.php
@@ -40,7 +40,7 @@ trait Application_Traits_Admin_RevisionableList
     
     abstract protected function getEntryData(Application_RevisionableCollection_DBRevisionable $revisionable);
     
-    protected function _renderContent() : array
+    protected function _renderContent() : string
     {
         $filters = $this->collection->getFilterCriteria();
         
