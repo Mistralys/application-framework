b1e29097bf57f838337bb1afdb7a58c58028a7f9
Mistralys <s.mordziol@gmail.com>
2022-07-19 10:46:38 +0200

    Added some media tests.

--- tests/classes/MediaTestCase.php
+++ tests/classes/MediaTestCase.php
@@ -4,7 +4,21 @@ declare(strict_types=1);
 
 namespace Mistralys\AppFrameworkTests\TestClasses;
 
+use Application;
+use Application_Media;
+
 abstract class MediaTestCase extends ApplicationTestCase
 {
+    protected Application_Media $media;
+    protected string $storageFolder;
 
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->media = Application::createMedia();
+        $this->storageFolder = __DIR__.'/../files/Media';
+
+        $this->startTransaction();
+    }
 }
