9e5a851bf7700bfe317dfc70561685869dc6f1e6
Mistralys <s.mordziol@gmail.com>
2022-08-10 11:24:55 +0200

    Modernized code.

--- src/classes/UI/Page/Breadcrumb.php
+++ src/classes/UI/Page/Breadcrumb.php
@@ -1,20 +1,28 @@
 <?php
 
-class UI_Page_Breadcrumb
+declare(strict_types=1);
+
+class UI_Page_Breadcrumb implements UI_Renderable_Interface
 {
-    protected $id;
+    use UI_Traits_RenderableGeneric;
+
+    protected string $id;
+    protected UI_Page $page;
 
     /**
-     * @var UI_Page
+     * @var UI_Page_Breadcrumb_Item[]
      */
-    protected $page;
+    protected array $items = array();
 
-    protected $items = array();
-
-    public function __construct(UI_Page $page, $id)
+    public function __construct(UI_Page $page, string $id)
     {
         $this->id = $id;
         $this->page = $page;
+    }
+
+    public function getUI() : UI
+    {
+        return $this->page->getUI();
     }
 
     /**
@@ -22,13 +30,12 @@ class UI_Page_Breadcrumb
      * item instance, use this to configure where it should be
      * linked.
      *
-     * @param string $label
+     * @param string|number|UI_Renderable_Interface|NULL $label
      * @return UI_Page_Breadcrumb_Item
+     * @throws UI_Exception
      */
-    public function appendItem($label)
+    public function appendItem($label) : UI_Page_Breadcrumb_Item
     {
-        require_once 'UI/Page/Breadcrumb/Item.php';
-
         $item = new UI_Page_Breadcrumb_Item($this, $label);
         $this->items[] = $item;
 
@@ -41,14 +48,17 @@ class UI_Page_Breadcrumb
      * the generated URL.
      *
      * @param Application_Admin_Area $area
-     * @param array $params
+     * @param array<string,mixed> $params
+     * @return UI_Page_Breadcrumb_Item
+     * @throws UI_Exception
      */
-    public function appendArea(Application_Admin_Area $area, $params = array())
+    public function appendArea(Application_Admin_Area $area, array $params = array()) : UI_Page_Breadcrumb_Item
     {
-        $this->appendItem($area->getNavigationTitle())->makeLinkedFromArea($area, $params);
+        return $this->appendItem($area->getNavigationTitle())
+            ->makeLinkedFromArea($area, $params);
     }
 
-    public function render()
+    public function render() : string
     {
         // only render the breadcrumb when we have more than 1 item.
         $total = count($this->items);
@@ -70,7 +80,7 @@ class UI_Page_Breadcrumb
      *
      * @return UI_Page_Breadcrumb_Item[]
      */
-    public function getItems()
+    public function getItems() : array
     {
         $this->updateItems();
 
@@ -78,37 +88,33 @@ class UI_Page_Breadcrumb
     }
 
     /**
-     * Updates all items in the current collection so they
+     * Updates all items in the current collection, so they
      * know in which position they are.
      */
-    protected function updateItems()
+    protected function updateItems() : void
     {
-        /* @var $item UI_Page_Breadcrumb_Item */
-
         $total = count($this->items);
-        for ($i = 0; $i < $total; $i++) {
-            $item = $this->items[$i];
+
+        foreach ($this->items as $i => $item)
+        {
             $item->reset();
 
-            if ($i == 0) {
+            if ($i === 0) {
                 $item->setFirst();
             }
 
-            if ($i == $total - 1) {
+            if ($i === ($total - 1)) {
                 $item->setLast();
             }
         }
     }
 
-    public function display()
+    public function display() : void
     {
         echo $this->render();
     }
 
-    /**
-     * @return UI_Page
-     */
-    public function getPage()
+    public function getPage() : UI_Page
     {
         return $this->page;
     }
@@ -119,9 +125,8 @@ class UI_Page_Breadcrumb
     *  
     * @return UI_Page_Breadcrumb_Item|NULL
     */
-    public function getLastItem()
+    public function getLastItem() : ?UI_Page_Breadcrumb_Item
     {
-        $items = $this->items;
-        return array_pop($items);
+        return array_value_get_last($this->items);
     }
 }
