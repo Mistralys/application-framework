1cf0fe64a486460c33181a01f81a45be66ee72cc
Mistralys <s.mordziol@gmail.com>
2022-12-19 09:22:56 +0100

    Testing the footer template.

---
+++ tests/application/themes/default/templates/frame.footer.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+class driver_template_default_frame_footer extends template_default_frame_footer
+{
+    protected function registerAboutItems(): void
+    {
+        $this->addItemHTML(t('This is an application used to showcase and test framework features.'));
+    }
+}
