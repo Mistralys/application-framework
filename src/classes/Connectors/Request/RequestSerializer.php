6fbd6356912efe196893908fd983fde77ab51b2e
Mistralys <s.mordziol@gmail.com>
2023-01-12 12:11:43 +0100

    Fixed #50 by adding request types.

--- src/classes/Connectors/Request/RequestSerializer.php
+++ src/classes/Connectors/Request/RequestSerializer.php
@@ -11,12 +11,16 @@ namespace Connectors\Request;
 use Application\Exception\UnexpectedInstanceException;
 use Application_Exception;
 use AppUtils\ArrayDataCollection;
+use AppUtils\ClassHelper;
 use AppUtils\ConvertHelper\JSONConverter;
 use AppUtils\ConvertHelper\JSONConverter\JSONConverterException;
 use Connectors;
+use Connectors_Connector;
 use Connectors_Exception;
 use Connectors_Request;
+use Connectors_Request_Method;
 use Connectors_Request_URL;
+use Connectors_Response;
 
 /**
  * Utility class that handles serializing and unserializing
@@ -28,6 +32,8 @@ use Connectors_Request_URL;
  */
 class RequestSerializer
 {
+    public const ERROR_UNKNOWN_REQUEST_TYPE = 124101;
+
     public const KEY_CONNECTOR_ID = 'connectorID';
     public const KEY_URL = 'url';
     public const KEY_ID = 'id';
@@ -36,6 +42,9 @@ class RequestSerializer
     public const KEY_HEADERS = 'headers';
     public const KEY_TIMEOUT = 'timeout';
     public const KEY_HTTP_METHOD = 'HTTPMethod';
+    public const REQUEST_TYPE_URL = 'URL';
+    public const KEY_REQUEST_TYPE = 'requestType';
+    public const REQUEST_TYPE_METHOD = 'Method';
 
     /**
      * @param Connectors_Request $request
@@ -47,6 +56,7 @@ class RequestSerializer
     public static function serialize(Connectors_Request $request) : string
     {
         return JSONConverter::var2json(array(
+            self::KEY_REQUEST_TYPE => self::resolveRequestType($request),
             self::KEY_CONNECTOR_ID => $request->getConnector()->getID(),
             self::KEY_URL => $request->getRequestURL(),
             self::KEY_ID => $request->getID(),
@@ -58,26 +68,84 @@ class RequestSerializer
         ));
     }
 
+    public static function resolveRequestType(Connectors_Request $request) : string
+    {
+        if($request instanceof Connectors_Request_URL)
+        {
+            return self::REQUEST_TYPE_URL;
+        }
+
+        if($request instanceof Connectors_Request_Method)
+        {
+            return self::REQUEST_TYPE_METHOD;
+        }
+
+        throw new Connectors_Exception(
+            $request->getConnector(),
+            'Unknown request type.',
+            sprintf(
+                'The request type of class [%s] is unknown.',
+                get_class($request)
+            ),
+            self::ERROR_UNKNOWN_REQUEST_TYPE
+        );
+    }
+
     /**
      * @param string $json
-     * @return Connectors_Request
+     * @return Connectors_Request|NULL
      *
      * @throws JSONConverterException
      * @throws UnexpectedInstanceException
      * @throws Application_Exception
      * @throws Connectors_Exception
      */
-    public static function unserialize(string $json) : Connectors_Request
+    public static function unserialize(string $json) : ?Connectors_Request
     {
         $data = ArrayDataCollection::create(JSONConverter::json2array($json));
+        $connector = Connectors::createConnector($data->getString(self::KEY_CONNECTOR_ID));
 
-        $request = new Connectors_Request_URL(
-            Connectors::createConnector($data->getString(self::KEY_CONNECTOR_ID)),
-            $data->getString(self::KEY_URL),
-            $data->getArray(self::KEY_POST_DATA),
-            $data->getArray(self::KEY_GET_DATA),
-            $data->getString(self::KEY_ID)
-        );
+        $type = $data->getString(self::KEY_REQUEST_TYPE);
+
+        // Old requests did not have the type stored.
+        if(empty($type))
+        {
+            return null;
+        }
+
+        if($type === self::REQUEST_TYPE_METHOD)
+        {
+            $request = new Connectors_Request_Method(
+                $connector,
+                $data->getString(self::KEY_URL),
+                $data->getString(self::KEY_HTTP_METHOD),
+                $data->getArray(self::KEY_POST_DATA),
+                $data->getArray(self::KEY_GET_DATA),
+                $data->getString(self::KEY_ID)
+            );
+        }
+        else if($type === self::REQUEST_TYPE_URL)
+        {
+            $request = new Connectors_Request_URL(
+                $connector,
+                $data->getString(self::KEY_URL),
+                $data->getArray(self::KEY_POST_DATA),
+                $data->getArray(self::KEY_GET_DATA),
+                $data->getString(self::KEY_ID)
+            );
+        }
+        else
+        {
+            throw new Connectors_Exception(
+                $connector,
+                'Unknown request type.',
+                sprintf(
+                    'The request type [%s] is unknown.',
+                    $type
+                ),
+                self::ERROR_UNKNOWN_REQUEST_TYPE
+            );
+        }
 
         $headers = $data->getArray(self::KEY_HEADERS);
 
