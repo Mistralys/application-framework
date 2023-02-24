cac8097710f9c72ab538ed8c069a784334242ea0
Mistralys <s.mordziol@gmail.com>
2023-02-24 15:57:48 +0100

    Fixed the empty message not being used when empty.

--- src/classes/UI/PropertiesGrid/Property/Merged.php
+++ src/classes/UI/PropertiesGrid/Property/Merged.php
@@ -9,7 +9,7 @@ class UI_PropertiesGrid_Property_Merged extends UI_PropertiesGrid_Property
     /**
      * @var string[]
      */
-    protected $classes = array();
+    private array $classes = array();
 
     public function render() : string
     {
@@ -18,16 +18,25 @@ class UI_PropertiesGrid_Property_Merged extends UI_PropertiesGrid_Property
         ?>
             <tr class="prop-merged <?php echo implode(' ', $this->classes) ?>">
                 <td colspan="2">
-                    <?php echo $this->label ?>
+                    <?php echo $this->resolveText() ?>
                 </td>
             </tr>
         <?php
 
         return OutputBuffering::get();
     }
+
+    public function addClass(string $class) : self
+    {
+        if(!in_array($class, $this->classes, true)) {
+            $this->classes[] = $class;
+        }
+
+        return $this;
+    }
     
     protected function filterValue($value) : UI_StringBuilder
     {
-        return sb();
+        return sb()->add($value);
     }
 }
