61dc1506e47b650f2cee6e5fc4f87d9321213152
Mistralys <s.mordziol@gmail.com>
2023-03-03 16:59:55 +0100

    Added missing collection list interface.

--- src/classes/Application/Admin/Area/Mode/Submode/Action/CollectionList.php
+++ src/classes/Application/Admin/Area/Mode/Submode/Action/CollectionList.php
@@ -1,6 +1,10 @@
 <?php
 
-abstract class Application_Admin_Area_Mode_Submode_Action_CollectionList extends Application_Admin_Area_Mode_Submode_Action
+use Application\Interfaces\Admin\CollectionListInterface;
+
+abstract class Application_Admin_Area_Mode_Submode_Action_CollectionList
+    extends Application_Admin_Area_Mode_Submode_Action
+    implements CollectionListInterface
 {
     use Application_Traits_Admin_CollectionList;
 }
