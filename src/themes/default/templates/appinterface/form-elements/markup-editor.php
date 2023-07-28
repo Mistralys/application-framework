65f6eeb5e82181cff9521abfa44dc11d49f2b22c
Mistralys <s.mordziol@gmail.com>
2023-07-28 09:53:40 +0200

    Added a second example.

--- src/themes/default/templates/appinterface/form-elements/markup-editor.php
+++ src/themes/default/templates/appinterface/form-elements/markup-editor.php
@@ -11,15 +11,34 @@ $country = Application_Countries::getInstance()->createNewCountry('de', 'German'
 $form = new Application_Formable_Generic();
 $form->createFormableForm('markup-editor-example');
 
+// ---------------------------------------------------------------
+// MARKUP EDITOR 1
+// ---------------------------------------------------------------
+
 $el = $form->addElementTextarea('markup', t('Markup Editor'));
+$el->setComment(t('With the align buttons as a dropdown menu.'));
 
 // Attach the markup editor to the element.
 $editor = $form->getFormInstance()->makeCKEditor($el, $country);
 
-// Add a custom plugin: This is not enabled by default
-// in the editor.
+// Activate the alignment plugin: This is not enabled by default
+// in the editor (but it is bundled in the build).
 $editor->addPlugin('Alignment');
-$editor->insertButtonAfter('alignment', UI_MarkupEditor_CKEditor::BUTTON_SUPERSCRIPT);
+$editor->insertButtonAfter(UI_MarkupEditor_CKEditor::BUTTON_ALIGN, UI_MarkupEditor_CKEditor::BUTTON_SUPERSCRIPT);
+
+// ---------------------------------------------------------------
+// MARKUP EDITOR 2
+// ---------------------------------------------------------------
+
+$el = $form->addElementTextarea('markup-two', t('Markup Editor'));
+$el->setComment(t('With the alignment choices as separate buttons.'));
+
+$editor = $form->getFormInstance()->makeCKEditor($el, $country);
+
+$editor->addPlugin('Alignment');
+$editor->insertButtonAfter(UI_MarkupEditor_CKEditor::BUTTON_ALIGN_RIGHT, UI_MarkupEditor_CKEditor::BUTTON_SUPERSCRIPT);
+$editor->insertButtonAfter(UI_MarkupEditor_CKEditor::BUTTON_ALIGN_CENTER, UI_MarkupEditor_CKEditor::BUTTON_SUPERSCRIPT);
+$editor->insertButtonAfter(UI_MarkupEditor_CKEditor::BUTTON_ALIGN_LEFT, UI_MarkupEditor_CKEditor::BUTTON_SUPERSCRIPT);
 
 $form->display();
 
