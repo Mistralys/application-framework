d5a1e32f5a6a8f583d7f5309ea0f481ec1dc3a0d
Mistralys <s.mordziol@gmail.com>
2023-09-25 11:41:34 +0200

    Added the environment loader.

--- tests/application/config/app-config.php
+++ tests/application/config/app-config.php
@@ -13,6 +13,20 @@
 
 declare(strict_types=1);
 
+use Application\Environments;
+use AppUtils\FileHelper\FolderInfo;
+use TestDriver\EnvironmentsConfig;
+
 if(!function_exists('boot_define')) {
     die('May not be accessed directly.');
+}
+
+try
+{
+    (new EnvironmentsConfig(FolderInfo::factory(__DIR__)))
+        ->detect();
+}
+catch (Throwable $e)
+{
+    Environments::displayException($e);
 }
