db0630816b05871d5762f650f3a77d9bf001a202
Mistralys <s.mordziol@gmail.com>
2023-11-02 22:27:46 +0100

    Added support for custom media tags.

--- src/classes/Application/MarkdownRenderer.php
+++ src/classes/Application/MarkdownRenderer.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace Application;
 
+use Application\MarkdownRenderer\BaseCustomTag;
+use Application\MarkdownRenderer\CustomTags\MediaTag;
+use AppUtils\AttributeCollection;
+use AppUtils\ConvertHelper;
 use AppUtils\Interfaces\OptionableInterface;
 use AppUtils\Traits\OptionableTrait;
 use League\CommonMark\CommonMarkConverter;
@@ -36,11 +40,77 @@ class MarkdownRenderer implements OptionableInterface
         );
     }
 
+    /**
+     * @var BaseCustomTag[]
+     */
+    private array $tags = array();
+
     public function render(string $markdown) : string
     {
+        $this->tags = array();
+
+        $markdown = $this->preParse($markdown);
+
         $parser = new CommonMarkConverter($this->getOptions());
 
-        return (string)$parser->convert($markdown);
+        $markdown = (string)$parser->convert($markdown);
+
+        return $this->postParse($markdown);
+    }
+
+    private function preParse(string $markdown) : string
+    {
+        array_push($this->tags, ...MediaTag::findTags($markdown));
+
+        foreach($this->tags as $tag)
+        {
+            $markdown = str_replace($tag->getMatchedText(), $tag->getPlaceholder(), $markdown);
+        }
+
+        return $markdown;
+    }
+
+    public static function parseParams(string $params) : AttributeCollection
+    {
+        $result = AttributeCollection::create();
+        $params = trim($params);
+        if($params === '') {
+            return $result;
+        }
+
+        $escaped = array(
+            '\"' => '__ESCAPED_DBL_QUOTE__',
+        );
+
+        $restored = array(
+            '__ESCAPED_DBL_QUOTE__' => '"',
+        );
+
+        $params = str_replace(array_keys($escaped), array_values($escaped), $params);
+        $parts = ConvertHelper::explodeTrim(' ', $params);
+
+        foreach($parts as $part)
+        {
+            preg_match('/([a-z0-9]+)\s*=\s*"([^"]+)"/', $part, $partMatches);
+
+            if(!empty($partMatches[0])) {
+                $result->attr($partMatches[1], str_replace(array_keys($restored), array_values($restored), $partMatches[2]));
+            }
+        }
+
+        return $result;
+    }
+
+    private function postParse(string $markdown) : string
+    {
+        foreach($this->tags as $tag)
+        {
+            $markdown = str_replace($tag->getPlaceholder(), $tag->render(), $markdown);
+        }
+
+        $this->tags = array();
+
+        return $markdown;
     }
 
     /**
--- tests/AppFrameworkTestClasses/MediaTestCase.php
+++ tests/AppFrameworkTestClasses/MediaTestCase.php
@@ -8,6 +8,10 @@ use AppFrameworkTestClasses\ApplicationTestCase;
 use Application;
 use Application\AppFactory;
 use Application_Media;
+use Application_Media_Document;
+use Application_Media_Document_Image;
+use AppUtils\FileHelper;
+use AppUtils\FileHelper\FileInfo;
 
 abstract class MediaTestCase extends ApplicationTestCase
 {
@@ -21,6 +25,7 @@ abstract class MediaTestCase extends ApplicationTestCase
         $this->media = AppFactory::createMedia();
         $this->uploads = AppFactory::createUploads();
         $this->storageFolder = __DIR__.'/../files/Media';
+        $this->testMedia = array();
 
         $this->startTransaction();
     }
@@ -32,5 +37,35 @@ abstract class MediaTestCase extends ApplicationTestCase
         $this->assertFileExists($file);
 
         return $file;
+    }
+
+    public function createTestImageMedia(string $name='example-image') : Application_Media_Document_Image
+    {
+        $file = $this->getExampleImagePath();
+
+        $document = $this->media->createImageFromFile($name, FileInfo::factory($file));
+        $documentPath = $document->getPath();
+
+        $this->assertFileExists($documentPath);
+
+        $this->testMedia[] = $document;
+
+        return $document;
+    }
+
+    /**
+     * @var Application_Media_Document[]
+     */
+    protected array $testMedia = array();
+
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        foreach($this->testMedia as $media)
+        {
+            FileHelper::deleteFile($media->getPath());
+        }
     }
 }
