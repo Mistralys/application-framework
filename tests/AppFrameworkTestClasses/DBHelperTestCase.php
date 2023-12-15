f0689cc8898911c34336cf5f351f119a7776974f
Mistralys <s.mordziol@gmail.com>
2023-12-15 15:42:56 +0100

    Namespaced test collection.

--- tests/AppFrameworkTestClasses/DBHelperTestCase.php
+++ tests/AppFrameworkTestClasses/DBHelperTestCase.php
@@ -9,8 +9,8 @@ declare(strict_types=1);
 namespace Mistralys\AppFrameworkTests\TestClasses;
 
 use AppFrameworkTestClasses\ApplicationTestCase;
-use TestDriver\TestDBCollection;
-use TestDriver\TestDBCollection\TestDBRecord;
+use TestDriver\TestDBRecords\TestDBCollection;
+use TestDriver\TestDBRecords\TestDBRecord;
 
 /**
  * @package Application
@@ -19,8 +19,8 @@ use TestDriver\TestDBCollection\TestDBRecord;
 abstract class DBHelperTestCase extends ApplicationTestCase
 {
     public const TEST_RECORDS_PRIMARY = TestDBCollection::PRIMARY_NAME;
-    public const TEST_RECORDS_COL_LABEL = TestDBRecord::COL_LABEL;
-    public const TEST_RECORDS_COL_ALIAS = TestDBRecord::COL_ALIAS;
+    public const TEST_RECORDS_COL_LABEL = TestDBCollection::COL_LABEL;
+    public const TEST_RECORDS_COL_ALIAS = TestDBCollection::COL_ALIAS;
     public const TEST_RECORDS_DATA_TABLE = TestDBCollection::TABLE_NAME_DATA;
     public const TEST_RECORDS_TABLE = TestDBCollection::TABLE_NAME;
 
