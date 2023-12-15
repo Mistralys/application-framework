6d4ce64a4202a00878fc5f8ea2aac0f4be7adc1d
Mistralys <s.mordziol@gmail.com>
2023-12-15 09:36:22 +0100

    Added user setting helper methods.

--- src/classes/Application/User.php
+++ src/classes/Application/User.php
@@ -1,12 +1,18 @@
 <?php
 /**
  * File containing the {@link Application_User} class
- * 
+ *
  * @package Application
  * @subpackage User
  * @see Application_User
  */
 
+use Application\Driver\DriverException;
+use Application\User\LayoutWidth;
+use Application\User\LayoutWidths;
+use Application\User\UserException;
+use AppLocalize\Localization;
+use AppLocalize\Localization_Locale;
 use AppUtils\ClassHelper;
 use AppUtils\ClassHelper\ClassNotExistsException;
 use AppUtils\ClassHelper\ClassNotImplementsException;
@@ -14,8 +20,8 @@ use AppUtils\ConvertHelper;
 
 /**
  * Base user class that handles the user that is currently
- * logged in via the active session. 
- * 
+ * logged in via the active session.
+ *
  * NOTE: This cannot be used to manage other users, only
  * the authenticated user. To manager other users, use the
  * {@link Application_Users} collection.
@@ -35,6 +41,7 @@ abstract class Application_User implements Application_User_Interface, Applicati
     public const ERROR_RECENT_ITEMS_CLASS_MISSING = 20006;
     public const ERROR_INVALID_RECENT_ITEMS_CLASS = 20007;
     public const ERROR_CANNOT_DECODE_ARRAY_VALUE = 20008;
+    public const ERROR_UNKNOWN_STARTUP_AREA = 20009;
 
     public const STORAGE_TYPE_DB = 'DB';
     public const STORAGE_TYPE_FILE = 'File';
@@ -48,15 +55,13 @@ abstract class Application_User implements Application_User_Interface, Applicati
     public const RIGHT_TRANSLATE_UI = 'TranslateUI';
     public const RIGHT_DEVELOPER = 'Developer';
 
-    public const SETTING_DEVELOPER_MODE = 'developer_mode';
-
     /**
     * Stores user right definitions.
     * @var array
     * @see Application_User::initRoles()
     */
     protected $rights = array();
-    
+
    /**
     * Stores the user setting values
     * @var array
@@ -67,12 +72,12 @@ abstract class Application_User implements Application_User_Interface, Applicati
     * @var bool
     */
     protected $settingsLoaded = false;
-    
+
    /**
     * @var Application_User_Storage
     */
     protected $storage;
-    
+
    /**
     * @var string[]
     */
@@ -121,14 +126,14 @@ abstract class Application_User implements Application_User_Interface, Applicati
 
    /**
     * Retrieves a user setting from the user's storage.
-    * 
+    *
     * @param string $name
     * @param string $default
     * @return string
     */
     public function getSetting(string $name, string $default = '') : string
     {
-        if (!$this->settingsLoaded) 
+        if (!$this->settingsLoaded)
         {
             $this->loadSettings();
         }
@@ -247,34 +252,36 @@ abstract class Application_User implements Application_User_Interface, Applicati
 
    /**
     * Sets a user setting's value.
-    * 
+    *
     * NOTE: Must be saved using `saveSettings()` for the changes to be applied.
-    * 
+    *
     * @param string $name
     * @param string $value
-    * @throws Application_Exception
+    * @return bool Whether the setting value has changed.
     */
-    public function setSetting(string $name, string $value) : void
+    public function setSetting(string $name, string $value) : bool
     {
         if (!$this->settingsLoaded)
         {
             $this->loadSettings();
         }
-        
+
         $newValue = $this->validateSetting($name, $value);
         if(isset($this->settings[$name]) && $this->settings[$name] === $newValue) {
-            return;
+            return false;
         }
-        
+
         $this->changedSettings[$name] = true;
         $this->settings[$name] = $newValue;
+
+        return true;
     }
-    
+
     public function resetSettings() : void
     {
         $this->settingsLoaded = false;
         $this->settings = array();
-        
+
         $this->storage->reset();
     }
 
@@ -284,7 +291,7 @@ abstract class Application_User implements Application_User_Interface, Applicati
 
         if (method_exists($this, $method))
         {
-            return strval($this->$method($value));
+            return (string)$this->$method($value);
         }
 
         return $value;
@@ -322,7 +329,7 @@ abstract class Application_User implements Application_User_Interface, Applicati
 
     /**
      * Retrieves an indexed array containing a list of the user's rights
-     * 
+     *
      * @return string[]
      */
     public function getRights() : array
@@ -338,11 +345,11 @@ abstract class Application_User implements Application_User_Interface, Applicati
         }
 
         $this->log('Save | Saving settings.');
-        
+
         if (!$this->settingsLoaded) {
             $this->loadSettings();
         }
-        
+
         $data = array();
         $names = array_keys($this->changedSettings);
         foreach($names as $name) {
@@ -350,7 +357,7 @@ abstract class Application_User implements Application_User_Interface, Applicati
         }
 
         $this->storage->save($data);
-        
+
         $this->changedSettings = array();
 
         $this->log('Save | Complete.');
@@ -363,7 +370,7 @@ abstract class Application_User implements Application_User_Interface, Applicati
         }
 
         $this->log('Loading settings.');
-        
+
         $data = $this->storage->load();
 
         if (!is_array($data))
@@ -381,13 +388,13 @@ abstract class Application_User implements Application_User_Interface, Applicati
 
         $this->settingsLoaded = true;
     }
-    
+
     public function removeSetting(string $name) : void
     {
         if($this->settingsLoaded && isset($this->settings[$name])) {
             unset($this->settings[$name]);
         }
-        
+
         $this->storage->removeKey($name);
     }
 
@@ -421,11 +428,11 @@ abstract class Application_User implements Application_User_Interface, Applicati
 
         return self::$roles;
     }
-    
+
     public function roleExists(string $name) : bool
     {
         self::initRoles();
-        
+
         return isset(self::$roles[$name]);
     }
 
@@ -448,23 +455,23 @@ abstract class Application_User implements Application_User_Interface, Applicati
 
         return $groups;
     }
-    
+
     /**
      * Used to cache the role definitions after initializing them
      * @var array|NULL
      * @see initRoles()
      */
     protected static ?array $roles = null;
-    
+
     protected function initRoles() : void
     {
         if (isset(self::$roles)) {
             return;
         }
-        
+
         self::$roles = $this->getRoleDefs();
-        
-        if(empty(self::$roles)) 
+
+        if(empty(self::$roles))
         {
             throw new Application_Exception(
                 'No user roles defined',
@@ -473,7 +480,7 @@ abstract class Application_User implements Application_User_Interface, Applicati
             );
         }
     }
-    
+
     abstract protected function getRoleDefs();
 
     /**
@@ -489,7 +496,7 @@ abstract class Application_User implements Application_User_Interface, Applicati
         if(!in_array($role, $this->requestedRoles)) {
             $this->requestedRoles[] = $role;
         }
-        
+
         if (!Application::isAuthenticationEnabled()) {
             return true;
         }
@@ -507,12 +514,12 @@ abstract class Application_User implements Application_User_Interface, Applicati
         // next, we go through all roles and see if this
         // role is granted along with another role that
         // the user may be allowed for.
-        foreach (self::$roles as $roleName => $roleDef) 
+        foreach (self::$roles as $roleName => $roleDef)
         {
             if($roleName == $role) {
                 continue;
             }
-            
+
             if (in_array($role, $roleDef['grants']) && $this->hasRight($roleName)) {
                 return true;
             }
@@ -520,7 +527,7 @@ abstract class Application_User implements Application_User_Interface, Applicati
 
         return false;
     }
-    
+
    /**
     * Retrieves a list of all roles requested up to this point.
     * @return array
@@ -529,10 +536,10 @@ abstract class Application_User implements Application_User_Interface, Applicati
     {
         return $this->requestedRoles;
     }
-    
+
    /**
     * Creates a user instance for the specified user ID.
-    * 
+    *
     * @param int $user_id
     * @return Application_User
     * @throws Application_Exception|DBHelper_Exception
@@ -583,7 +590,7 @@ abstract class Application_User implements Application_User_Interface, Applicati
         if(Application::isDatabaseEnabled()) {
             return self::STORAGE_TYPE_DB;
         }
-        
+
         return self::STORAGE_TYPE_FILE;
     }
 
@@ -747,6 +754,25 @@ abstract class Application_User implements Application_User_Interface, Applicati
         return in_array($rightName, $this->getRights());
     }
 
+    // region: Settings
+
+    public const SETTING_DEVELOPER_MODE = 'developer_mode';
+    public const SETTING_UI_LOCALE = 'locale';
+    public const SETTING_UI_WIDTH = 'layout_width';
+    public const SETTING_STARTUP_TAB = 'startup_tab';
+    public const SETTING_DARK_MODE = 'dark_mode';
+
+    public function isDarkModeEnabled() : bool
+    {
+        return $this->getSetting(self::SETTING_DARK_MODE) === 'true';
+    }
+
+    public function setDarkModeEnabled(bool $enabled) : self
+    {
+        $this->setSetting(self::SETTING_DARK_MODE, ConvertHelper::boolStrict2string($enabled));
+        return $this;
+    }
+
     public function isDeveloper() : bool
     {
         if(Application::isDemoMode()) {
@@ -778,6 +804,93 @@ abstract class Application_User implements Application_User_Interface, Applicati
         return $this;
     }
 
+    public function getUILocale() : Localization_Locale
+    {
+        return Localization::getAppLocaleByName($this->getUILocaleName());
+    }
+
+    public function getUILocaleName() : string
+    {
+        $locale = $this->getSetting(self::SETTING_UI_LOCALE);
+
+        if(Localization::appLocaleExists($locale)) {
+            return $locale;
+        }
+
+        return Localization::getAppLocaleName();
+    }
+
+    public function setUILocale(Localization_Locale $locale) : self
+    {
+        $this->setSetting(self::SETTING_UI_LOCALE, $locale->getName());
+        return $this;
+    }
+
+    public function setLayoutWidth(LayoutWidth $width) : self
+    {
+        $this->setSetting(self::SETTING_UI_WIDTH, $width->getID());
+        return $this;
+    }
+
+    public function getLayoutWidthID() : string
+    {
+        return LayoutWidths::getInstance()->getIDOrDefault($this->getSetting(self::SETTING_UI_WIDTH));
+    }
+
+    public function getLayoutWidth() : LayoutWidth
+    {
+        return LayoutWidths::getInstance()->getByID($this->getLayoutWidthID());
+    }
+
+    /**
+     * Sets the URL name of the admin area to open by default.
+     *
+     * @param string $name
+     * @return $this
+     * @throws DriverException
+     * @throws UserException {@see self::ERROR_UNKNOWN_STARTUP_AREA}
+     */
+    public function setStartupScreenName(string $name) : self
+    {
+        if(Application_Driver::getInstance()->areaExists($name)) {
+            $this->setSetting(self::SETTING_STARTUP_TAB, $name);
+            return $this;
+        }
+
+        throw new UserException(
+            'Unknown startup admin area.',
+            sprintf(
+                'The admin area [%s] does not exist.',
+                $name
+            ),
+            self::ERROR_UNKNOWN_STARTUP_AREA
+        );
+    }
+
+    /**
+     * Retrieves the URL name of the admin area the user
+     * wishes to open by default.
+     *
+     * @return string
+     * @throws DriverException
+     */
+    public function getStartupScreenName() : string
+    {
+        $name = $this->getSetting(self::SETTING_STARTUP_TAB);
+        $driver = Application_Driver::getInstance();
+
+        if($driver->areaExists($name)) {
+            return $name;
+        }
+
+        return $driver
+            ->getAppSet()
+            ->getDefaultArea()
+            ->getURLName();
+    }
+
+    // endregion
+
     public function canTranslateUI() : bool { return $this->can(self::RIGHT_TRANSLATE_UI); }
     public function canLogin() : bool { return $this->can(self::RIGHT_LOGIN); }
 
@@ -797,7 +910,7 @@ abstract class Application_User implements Application_User_Interface, Applicati
     public function canDeleteNews() : bool { return $this->can(self::RIGHT_DELETE_NEWS); }
 
     // endregion
-    
+
     // region: Media management
 
     public const RIGHT_CREATE_MEDIA = 'CreateMedia';
@@ -809,7 +922,7 @@ abstract class Application_User implements Application_User_Interface, Applicati
     public function canCreateMedia() : bool { return $this->can(self::RIGHT_CREATE_MEDIA); }
     public function canEditMedia() : bool { return $this->can(self::RIGHT_EDIT_MEDIA); }
     public function canDeleteMedia() : bool { return $this->can(self::RIGHT_DELETE_MEDIA); }
-    
+
     // endregion
 
     public function isSystemUser() : bool
