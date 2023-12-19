721a4f68a24b005283036d015d9870cf8db5670e
Mistralys <s.mordziol@gmail.com>
2023-12-19 16:23:51 +0100

    Added `create()` factory method.

--- src/classes/Application/Formable/Generic.php
+++ src/classes/Application/Formable/Generic.php
@@ -1,10 +1,37 @@
 <?php
+/**
+ * @package Application
+ * @subpackage Formable
+ */
 
 declare(strict_types=1);
 
+/**
+ * Generic formable class, for creating forms without having to
+ * create a dedicated class for each form.
+ *
+ * @package Application
+ * @subpackage Formable
+ */
 class Application_Formable_Generic extends Application_Formable
 {
-    public function render() : string
+    /**
+     * Creates an instance of the generic formable, and initializes
+     * the form with the given name.
+     *
+     * @param string $name
+     * @param array<string,mixed> $defaultValues Optional default values. Can be set later using {@see Application_Interfaces_Formable::setDefaultFormValues()}.
+     * @return self
+     */
+    public static function create(string $name, array $defaultValues = array()): self
+    {
+        $formable = new self();
+        $formable->createFormableForm($name, $defaultValues);
+
+        return $formable;
+    }
+
+    public function render(): string
     {
         return $this->renderFormable();
     }
