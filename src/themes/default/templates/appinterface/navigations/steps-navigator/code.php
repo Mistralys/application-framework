057139d36c7b7f23b053996d3f76cd0b96712153
Mistralys <s.mordziol@gmail.com>
2024-01-10 12:54:54 +0100

    Simplified code.

--- src/themes/default/templates/appinterface/navigations/steps-navigator/code.php
+++ src/themes/default/templates/appinterface/navigations/steps-navigator/code.php
@@ -5,38 +5,37 @@ declare(strict_types=1);
 ?><h3>Without numbering</h3><?php
 
 $nav = new UI_Page_StepsNavigator();
+
 $nav->addStep('first', 'Visited step')
     ->link('#')
-    ->setEnabled();
+    ->makeEnabled();
 
 $nav->addStep('second', 'Active step')
     ->link('#')
-    ->setEnabled();
+    ->makeActive();
 
 $nav->addStep('third', 'Inactive step')
     ->link('#');
 
-$nav->selectStep('second');
-
 echo $nav;
 
+// ---------------------------------------------------
 
 ?><h3>With numbering</h3><?php
 
 $nav = new UI_Page_StepsNavigator();
+
 $nav->makeNumbered();
 
 $nav->addStep('first', 'Visited step')
     ->link('#')
-    ->setEnabled();
+    ->makeEnabled();
 
 $nav->addStep('second', 'Active step')
     ->link('#')
-    ->setEnabled();
+    ->makeActive();
 
 $nav->addStep('third', 'Inactive step')
     ->link('#');
-
-$nav->selectStep('second');
 
 echo $nav;
