b7e4530c92c30cd463723c6bc43e05ff929541a1
Mistralys <s.mordziol@gmail.com>
2024-01-17 17:06:53 +0100

    Added interface examples.

--- src/themes/default/templates/appinterface/navigations/subnav/code.php
+++ src/themes/default/templates/appinterface/navigations/subnav/code.php
@@ -2,19 +2,37 @@
 
 declare(strict_types=1);
 
+use Application\AppFactory;
 use AppUtils\Request;
 
-$nav = new UI_Page_Navigation(UI::getInstance()->getPage(), 'subnav');
+/* @var string $activeURL The URL to this example */
+
+$nav = UI_Page_Navigation::create('custom-nav')
+    ->setTemplateID('navigation.subnav');
 
 $nav->addClickable('Clickable link', "alert('Clicked!')");
 
 $nav->addExternalLink('URL link', 'https://www.google.com');
 
+// By default, the active item is determined automatically
+// by comparing the current URL with that of the item.
+$nav->addURL('Active item', $activeURL);
+
 $nav->addDropdownMenu('Split drop down')
     ->link('#')
     ->addLink('Link 1', '#');
 
-$nav->addSearch(function() {die('Search submitted');})
+// The search simply redirects to the current page, but
+// with a success message.
+$nav->addSearch(function(UI_Page_Navigation_Item_Search $item, string $terms) use($activeURL) {
+    AppFactory::createDriver()->redirectWithSuccessMessage(
+        sprintf(
+            'The search has been submitted: "%s"',
+            htmlspecialchars($terms)
+        ),
+        $activeURL
+    );
+})
     ->addHiddenPageVars()
     ->addHiddenVar('example', Request::getInstance()->getParam('example'));
 
---
+++ src/themes/default/templates/appinterface/navigations/tabs/code.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+use Application\AppFactory;
+use AppUtils\Request;
+
+/* @var string $activeURL The URL to this example */
+
+$tabs = UI::getInstance()->createTabs('tabs-example');
+
+$tabs->appendTab('Content 1', 'switch-1')
+    ->setContent('<br><p>Content of tab <strong>#1</strong></p>');
+
+$tabs->appendTab('Content 2', 'switch-2')
+    ->setContent('<br><p>Content of tab <strong>#2</strong></p>');
+
+$tabs->appendTab('Internal URL', 'url')
+    ->makeLinked($activeURL);
+
+$tabs->appendTab('External URL', 'url-external')
+    ->makeLinked('https://mistralys.eu', true);
+
+$menu = UI::getInstance()->createDropdownMenu();
+$menu->addLink('Link 1', '#');
+$menu->addLink('Link 2', '#');
+
+$tabs->appendTab('Dropdown menu', 'dropdown')
+    ->makeDropdown($menu);
+
+// Tabs can have a select handler set executed
+// every time the user switches to the tab.
+$tabs->appendTab('JavaScript', 'js')
+    ->clientOnSelect('alert("Tab is now selected!");')
+    ->setContent('<br><p>Content of JavaScript tab content</p>');
+
+echo $tabs;
