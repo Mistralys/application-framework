30910a92955c374d962210dbf4245dbe92931c93
Mistralys <s.mordziol@gmail.com>
2024-01-17 17:04:45 +0100

    Added target.

--- src/classes/UI/Bootstrap/Tab/Renderer/Link.php
+++ src/classes/UI/Bootstrap/Tab/Renderer/Link.php
@@ -6,13 +6,18 @@ class UI_Bootstrap_Tab_Renderer_Link extends UI_Bootstrap_Tab_Renderer
 {
     protected function _render() : string
     {
-        $atts = array(
+        $attributes = array(
             'href' => $this->tab->getURL(),
             'id', $this->tab->getLinkID()
         );
+
+        $target = $this->tab->getURLTarget();
+        if(!empty($target)) {
+            $attributes['target'] = $target;
+        }
         
         return
-        '<a '.compileAttributes($atts).'>'.
+        '<a '.compileAttributes($attributes).'>'.
             $this->renderLabel().
         '</a>';
     }
