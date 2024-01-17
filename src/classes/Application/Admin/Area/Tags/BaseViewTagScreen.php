185420e1ad308e2aa5ed0b1813c22be33b36cd61
Mistralys <s.mordziol@gmail.com>
2024-01-17 19:14:11 +0100

    Added subnavigation.

--- src/classes/Application/Admin/Area/Tags/BaseViewTagScreen.php
+++ src/classes/Application/Admin/Area/Tags/BaseViewTagScreen.php
@@ -5,11 +5,16 @@ declare(strict_types=1);
 namespace Application\Area\Tags;
 
 use Application\AppFactory;
+use Application\Area\Tags\ViewTag\BaseTagTreeScreen;
+use Application\Tags\TagRecord;
 use Application_Admin_Area_Mode_CollectionRecord;
 use Application\Area\Tags\ViewTag\BaseTagSettingsScreen;
 use Application\Tags\TagCollection;
 use UI;
 
+/**
+ * @property TagRecord $record
+ */
 abstract class BaseViewTagScreen extends Application_Admin_Area_Mode_CollectionRecord
 {
     public const URL_NAME = 'view-tag';
@@ -21,7 +26,7 @@ abstract class BaseViewTagScreen extends Application_Admin_Area_Mode_CollectionR
 
     public function getDefaultSubmode(): string
     {
-        return BaseTagSettingsScreen::URL_NAME;
+        return BaseTagTreeScreen::URL_NAME;
     }
 
     public function isUserAllowed(): bool
@@ -43,6 +48,7 @@ abstract class BaseViewTagScreen extends Application_Admin_Area_Mode_CollectionR
     {
         $this->renderer
             ->getTitle()
+            ->setText($this->record->getLabel())
             ->setIcon(UI::icon()->tags());
     }
 
@@ -50,6 +56,21 @@ abstract class BaseViewTagScreen extends Application_Admin_Area_Mode_CollectionR
     {
         $this->breadcrumb->appendItem($this->record->getLabel())
             ->makeLinked($this->record->getAdminURL());
+    }
+
+    protected function _handleSubnavigation(): void
+    {
+        $this->subnav->addURL(
+            t('Tag tree'),
+            $this->record->getAdminTagTreeURL()
+        )
+            ->setIcon(UI::icon()->tree());
+
+        $this->subnav->addURL(
+            t('Settings'),
+            $this->record->getAdminSettingsURL()
+        )
+            ->setIcon(UI::icon()->settings());
     }
 
     protected function createCollection() : TagCollection
