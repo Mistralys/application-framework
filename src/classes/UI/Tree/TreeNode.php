8f8c84449e5db679ed7b776705d6ffae2182057a
Mistralys <s.mordziol@gmail.com>
2024-01-24 16:29:35 +0100

    Added the tree renderer.

---
+++ src/classes/UI/Tree/TreeNode.php
@@ -0,0 +1,268 @@
+<?php
+
+declare(strict_types=1);
+
+namespace UI\Tree;
+
+use Application_Interfaces_Iconizable;
+use Application_Traits_Iconizable;
+use AppUtils\HTMLTag;
+use AppUtils\Interfaces\StringableInterface;
+use AppUtils\OutputBuffering;
+use UI;
+use UI\Targets\BaseTarget;
+use UI\Targets\ClickTarget;
+use UI\Targets\URLTarget;
+use UI_Button;
+use UI_Exception;
+
+class TreeNode implements Application_Interfaces_Iconizable
+{
+    use Application_Traits_Iconizable;
+
+    private string $label;
+    private ?BaseTarget $target = null;
+
+    /**
+     * @var TreeNode[]
+     */
+    private array $childNodes = array();
+    private ?TreeNode $parentNode;
+    private bool $active = false;
+    private bool $selected = false;
+
+    /**
+     * @var UI_Button[]
+     */
+    private array $actions = array();
+    private string $value = '';
+    private string $id;
+    private UI $ui;
+
+    /**
+     * @param string|number|StringableInterface|NULL $label
+     * @throws UI_Exception
+     */
+    public function __construct(UI $ui, $label, ?TreeNode $parentNode=null)
+    {
+        $this->ui = $ui;
+        $this->label = toString($label);
+        $this->parentNode = $parentNode;
+        $this->id = 'node'.nextJSID();
+    }
+
+    public function getID() : string
+    {
+        return $this->id;
+    }
+
+    public function getCheckboxID() : string
+    {
+        return $this->id.'-cb';
+    }
+
+    /**
+     * @param UI $ui
+     * @param string|number|StringableInterface|NULL $label
+     * @param TreeNode|null $parentNode
+     * @return TreeNode
+     * @throws UI_Exception
+     */
+    public static function create(UI $ui, $label, ?TreeNode $parentNode=null) : TreeNode
+    {
+        return new TreeNode($ui, $label, $parentNode);
+    }
+
+    public function getLabel(): string
+    {
+        return $this->label;
+    }
+
+    public function getTarget(): ?BaseTarget
+    {
+        return $this->target;
+    }
+
+    public function isRootNode() : bool
+    {
+        return !isset($this->parentNode);
+    }
+
+    public function isActive() : bool
+    {
+        return $this->active;
+    }
+
+    public function makeActive() : self
+    {
+        return $this->setActive(true);
+    }
+
+    public function setActive(bool $active) : self
+    {
+        $this->active = $active;
+        return $this;
+    }
+
+    public function isSelected() : bool
+    {
+        return $this->selected;
+    }
+
+    public function setSelected(bool $selected) : self
+    {
+        $this->selected = $selected;
+        return $this;
+    }
+
+    /**
+     * @param string|int|float $value
+     * @return $this
+     */
+    public function setValue($value) : self
+    {
+        $this->value = (string)$value;
+        return $this;
+    }
+
+    public function getValue() : string
+    {
+        return $this->value;
+    }
+
+    /**
+     * @param string|number|StringableInterface|NULL $label
+     * @return TreeNode
+     * @throws UI_Exception
+     */
+    public function createChildNode($label) : TreeNode
+    {
+        $node = self::create($this->ui, $label, $this);
+        $this->addChildNode($node);
+        return $node;
+    }
+
+    public function addChildNode(TreeNode $node) : self
+    {
+        $this->childNodes[] = $node;
+        $node->setParentNode($this);
+        return $this;
+    }
+
+    public function setParentNode(?TreeNode $node) : self
+    {
+        $this->parentNode = $node;
+        return $this;
+    }
+
+    public function getChildNodes() : array
+    {
+        return $this->childNodes;
+    }
+
+    public function link(string $url, bool $newTab=false) : self
+    {
+        return $this->setTarget(URLTarget::create($url, $newTab));
+    }
+
+    public function click(string $statement) : self
+    {
+        return $this->setTarget(ClickTarget::create($statement));
+    }
+
+    public function setTarget(BaseTarget $target) : self
+    {
+        $this->target = $target;
+        return $this;
+    }
+
+    public function renderLabel(TreeRenderer $renderer) : string
+    {
+        $label = $this->renderIconLabel($this->getLabel());
+
+        if(isset($this->target)) {
+            return $this->target->getLinkTag()->setContent($label)->render();
+        }
+
+        if(!$this->isRootNode() && $renderer->isSelectable()) {
+            return $this->renderCheckboxLabel($label);
+        }
+
+        return $label;
+    }
+
+    public function addButton(UI_Button $button) : self
+    {
+        $this->actions[] = $button;
+        return $this;
+    }
+
+    public function renderActions() : string
+    {
+        OutputBuffering::start();
+
+        ?>
+        <div class="node-entry-actions">
+            <?php
+            echo $this->ui
+                ->createButtonGroup()
+                ->makeMini()
+                ->addButtons($this->actions);
+            ?>
+        </div>
+        <?php
+
+        return OutputBuffering::get();
+    }
+
+    /**
+     * @param string|number|StringableInterface|NULL $label
+     * @return string
+     */
+    public function renderCheckboxLabel($label) : string
+    {
+        return HTMLTag::create('label')
+            ->attr('for', $this->getCheckboxID())
+            ->setContent($label)
+            ->render();
+    }
+
+    public function renderCheckbox(TreeRenderer $renderer) : string
+    {
+        if($this->isRootNode()) {
+            return '';
+        }
+
+        return HTMLTag::create('input')
+            ->id($this->getCheckboxID())
+            ->attr('type', 'checkbox')
+            ->name($renderer->getElementName().'[]')
+            ->attr('value', $this->value)
+            ->prop('checked', $this->isSelected())
+            ->addClass('tree-checkbox')
+            ->setSelfClosing()
+            ->render();
+    }
+
+    public function getJSObjectName() : string
+    {
+        return $this->getID().'obj';
+    }
+
+    public function injectJS(string $treeObjectName) : void
+    {
+        $this->ui->addJavascriptHead(sprintf(
+            "const %s = %s.RegisterNode('%s', '%s')",
+            $this->getJSObjectName(),
+            $treeObjectName,
+            $this->getID(),
+            $this->getCheckboxID()
+        ));
+
+        $childNodes = $this->getChildNodes();
+
+        foreach ($childNodes as $childNode) {
+            $childNode->injectJS($treeObjectName);
+        }
+    }
+}
---
+++ src/classes/UI/Tree/TreeRenderer.php
@@ -0,0 +1,271 @@
+<?php
+/**
+ * @package User Interface
+ * @subpackage Tree Renderer
+ */
+
+declare(strict_types=1);
+
+namespace UI\Tree;
+
+use AppUtils\AttributeCollection;
+use AppUtils\Interfaces\OptionableInterface;
+use AppUtils\OutputBuffering;
+use AppUtils\Traits\OptionableTrait;
+use UI;
+use UI_Renderable;
+
+/**
+ * Usage:
+ *
+ * 1. Create a tree using a {@see TreeNode} instance as root node.
+ * 2. Create the renderer instance using {@see UI::createTreeRenderer()}.
+ * 3. Configure the renderer's options
+ * 4. Display the tree
+ *
+ * @package User Interface
+ * @subpackage Tree Renderer
+ */
+class TreeRenderer extends UI_Renderable
+    implements OptionableInterface
+{
+    use OptionableTrait;
+
+    public const OPTION_SHOW_ROOT = 'showRoot';
+    public const OPTION_EDITABLE = 'editable';
+    public const OPTION_SELECTABLE = 'selectable';
+    public const OPTION_ELEMENT_NAME = 'selectable_name';
+    public const DEFAULT_ELEMENT_NAME = 'tree_items';
+
+    private TreeNode $rootNode;
+    /**
+     * @var string[]
+     */
+    private array $selectedValues = array();
+    private string $id;
+
+    public function __construct(UI $ui, TreeNode $rootNode)
+    {
+        $this->rootNode = $rootNode;
+        $this->id = 'tree'.nextJSID();
+
+        parent::__construct($ui->getPage());
+    }
+
+    public function getID(): string
+    {
+        return $this->id;
+    }
+
+    public function getJSObjectName() : string
+    {
+        return $this->id.'obj';
+    }
+
+    /**
+     * Adds a node value that should be marked as selected:
+     *
+     *
+     * @param string|int|float $value
+     * @return $this
+     */
+    public function addSelectedValue($value) : self
+    {
+        if(!in_array($value, $this->selectedValues, true)) {
+            $this->selectedValues[] = (string)$value;
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param array<int,string|int|float> $values
+     * @return self
+     */
+    public function addSelectedValues(array $values) : self
+    {
+        foreach($values as $value) {
+            $this->addSelectedValue($value);
+        }
+
+        return $this;
+    }
+
+    protected function _render() : string
+    {
+        $this->ui->addStylesheet('ui/node-tree.css');
+
+        $this->injectJS();
+
+        OutputBuffering::start();
+
+        $this->updateSelectedNodes($this->rootNode);
+
+        $nodes = $this->rootNode->getChildNodes();
+
+        ?>
+        <div class="node-tree-wrapper" id="<?php echo $this->id ?>">
+            <ul class="node-tree last-with-spacing <?php if(!empty($nodes)) { echo 'with-children'; } ?>">
+                <?php
+                if($this->isRootShown()) {
+                    $this->renderTree($this->rootNode);
+                } else {
+                    foreach ($nodes as $node) {
+                        $this->renderTree($node);
+                    }
+                }
+                ?>
+            </ul>
+        </div>
+        <?php
+
+        return OutputBuffering::get();
+    }
+
+    private function injectJS() : void
+    {
+        $this->ui->addJavascript('ui/node-tree/tree-renderer.js');
+        $this->ui->addJavascript('ui/node-tree/tree-node.js');
+
+        $objName = $this->getJSObjectName();
+
+        $this->ui->addJavascriptHead(sprintf(
+            "const %s = new TreeRenderer('%s')",
+            $objName,
+            $this->getID()
+        ));
+
+        $this->rootNode->injectJS($objName);
+
+        $this->ui->addJavascriptOnload(sprintf('%s.Start()', $objName));
+    }
+
+    private function renderTree(TreeNode $node) : void
+    {
+        $childNodes = $node->getChildNodes();
+
+        $attributes = AttributeCollection::create()
+            ->id($node->getID())
+            ->attr('data-label', $node->getLabel())
+            ->addClass('node-entry');
+
+        if($node->isActive()) { $attributes->addClass('active'); }
+        if($node->isSelected()) { $attributes->addClass('selected'); }
+        if($node->isRootNode()) { $attributes->addClass('root'); }
+
+        ?>
+        <li<?php echo $attributes ?>>
+            <div class="node-label">
+                <?php
+                if($this->isSelectable()) {
+                    echo $node->renderCheckbox($this);
+                }
+                ?>
+                <?php echo $node->renderLabel($this); ?>
+                <?php
+                if($this->isEditable()) {
+                    echo $node->renderActions();
+                }
+                ?>
+            </div>
+            <?php
+
+            if(!empty($childNodes))
+            {
+                ?>
+                <ul class="node-tree last-with-spacing with-children">
+                    <?php
+                    foreach ($childNodes as $childNode) {
+                        $this->renderTree($childNode);
+                    }
+                    ?>
+                </ul>
+                <?php
+            }
+            ?>
+        </li>
+        <?php
+    }
+
+    private function updateSelectedNodes(TreeNode $node) : void
+    {
+        $value = $node->getValue();
+        $node->setSelected((!empty($value) && in_array($value, $this->selectedValues)));
+
+        $childNodes = $node->getChildNodes();
+
+        foreach($childNodes as $childNode) {
+            $this->updateSelectedNodes($childNode);
+        }
+    }
+
+    /**
+     * @param bool $showRoot
+     * @return $this
+     */
+    public function setShowRoot(bool $showRoot) : self
+    {
+        return $this->setOption(self::OPTION_SHOW_ROOT, $showRoot);
+    }
+
+    public function isRootShown() : bool
+    {
+        return $this->getBoolOption(self::OPTION_SHOW_ROOT);
+    }
+
+    public function makeEditable() : self
+    {
+        return $this->setEditable(true);
+    }
+
+    public function setEditable(bool $editable) : self
+    {
+        return $this->setOption(self::OPTION_EDITABLE, $editable);
+    }
+
+    public function isEditable() : bool
+    {
+        return $this->getBoolOption(self::OPTION_EDITABLE);
+    }
+
+    public function setSelectable(bool $selectable) : self
+    {
+        return $this->setOption(self::OPTION_SELECTABLE, $selectable);
+    }
+
+    public function isSelectable() : bool
+    {
+        return $this->getBoolOption(self::OPTION_SELECTABLE);
+    }
+
+    public function makeSelectable(?string $elementName=null) : self
+    {
+        $this->setSelectable(true);
+
+        if(!empty($elementName)) {
+            $this->setOption(self::OPTION_ELEMENT_NAME, $elementName);
+        }
+
+        return $this;
+    }
+
+    public function setElementName(string $elementName) : self
+    {
+        return $this->setOption(self::OPTION_ELEMENT_NAME, $elementName);
+    }
+
+    public function getElementName() : string
+    {
+        return $this->getStringOption(self::OPTION_ELEMENT_NAME);
+    }
+
+    public function getDefaultOptions(): array
+    {
+        return array(
+            self::OPTION_SHOW_ROOT => true,
+            self::OPTION_EDITABLE => false,
+            self::OPTION_SELECTABLE => false,
+            self::OPTION_ELEMENT_NAME => self::DEFAULT_ELEMENT_NAME
+        );
+    }
+}
