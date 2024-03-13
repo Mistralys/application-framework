b11e809d2575b9d5cf86ac1231f70b7b35df608d
Mistralys <s.mordziol@gmail.com>
2024-03-13 10:10:05 +0100

    Updated for revisions and tags.

--- docs/sql/pristine.sql
+++ docs/sql/pristine.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jan 25, 2024 at 06:38 AM
+-- Generation Time: Mar 13, 2024 at 09:07 AM
 -- Server version: 10.5.22-MariaDB
--- PHP Version: 7.4.33
+-- PHP Version: 8.3.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -114,8 +114,8 @@ CREATE TABLE `app_news` (
 --
 
 CREATE TABLE `app_news_categories` (
-     `news_category_id` int(11) UNSIGNED NOT NULL,
-     `label` varchar(160) NOT NULL
+    `news_category_id` int(11) UNSIGNED NOT NULL,
+    `label` varchar(160) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -125,8 +125,8 @@ CREATE TABLE `app_news_categories` (
 --
 
 CREATE TABLE `app_news_entry_categories` (
-     `news_id` int(11) UNSIGNED NOT NULL,
-     `news_category_id` int(11) UNSIGNED NOT NULL
+    `news_id` int(11) UNSIGNED NOT NULL,
+    `news_category_id` int(11) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -200,11 +200,11 @@ CREATE TABLE `app_ratings` (
 --
 
 CREATE TABLE `app_ratings_screens` (
-     `rating_screen_id` int(11) UNSIGNED NOT NULL,
-     `hash` varchar(32) NOT NULL,
-     `dispatcher` varchar(250) NOT NULL,
-     `path` varchar(250) NOT NULL,
-     `params` text NOT NULL
+    `rating_screen_id` int(11) UNSIGNED NOT NULL,
+    `hash` varchar(32) NOT NULL,
+    `dispatcher` varchar(250) NOT NULL,
+    `path` varchar(250) NOT NULL,
+    `params` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci COMMENT='Unique application screens rated by users';
 
 -- --------------------------------------------------------
@@ -226,9 +226,9 @@ CREATE TABLE `app_settings` (
 --
 
 CREATE TABLE `countries` (
-     `country_id` int(11) UNSIGNED NOT NULL,
-     `iso` varchar(2) NOT NULL,
-     `label` varchar(180) NOT NULL
+    `country_id` int(11) UNSIGNED NOT NULL,
+    `iso` varchar(2) NOT NULL,
+    `label` varchar(180) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci COMMENT='Cache for Editor countries';
 
 --
@@ -284,13 +284,13 @@ CREATE TABLE `custom_properties_data` (
 --
 
 CREATE TABLE `custom_properties_presets` (
-     `preset_id` int(11) UNSIGNED NOT NULL,
-     `owner_type` varchar(250) NOT NULL,
-     `editable` enum('yes','no') NOT NULL DEFAULT 'yes',
-     `name` varchar(180) NOT NULL,
-     `is_structural` enum('yes','no') NOT NULL DEFAULT 'no',
-     `label` varchar(180) NOT NULL,
-     `default_value` text NOT NULL
+    `preset_id` int(11) UNSIGNED NOT NULL,
+    `owner_type` varchar(250) NOT NULL,
+    `editable` enum('yes','no') NOT NULL DEFAULT 'yes',
+    `name` varchar(180) NOT NULL,
+    `is_structural` enum('yes','no') NOT NULL DEFAULT 'no',
+    `label` varchar(180) NOT NULL,
+    `default_value` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
@@ -338,7 +338,7 @@ INSERT INTO `known_users` (`user_id`, `foreign_id`, `firstname`, `lastname`, `em
 --
 
 CREATE TABLE `locales_application` (
-     `locale_name` varchar(5) NOT NULL
+    `locale_name` varchar(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
@@ -348,7 +348,7 @@ CREATE TABLE `locales_application` (
 --
 
 CREATE TABLE `locales_content` (
-     `locale_name` varchar(5) NOT NULL
+    `locale_name` varchar(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
@@ -358,15 +358,15 @@ CREATE TABLE `locales_content` (
 --
 
 CREATE TABLE `media` (
-     `media_id` int(11) UNSIGNED NOT NULL,
-     `user_id` int(11) UNSIGNED NOT NULL,
-     `media_date_added` datetime NOT NULL,
-     `media_type` varchar(100) NOT NULL,
-     `media_name` varchar(240) NOT NULL,
-     `media_extension` varchar(20) NOT NULL,
-     `file_size` int(11) UNSIGNED NOT NULL DEFAULT 0,
-     `keywords` varchar(500) NOT NULL DEFAULT '',
-     `description` varchar(1200) NOT NULL DEFAULT ''
+    `media_id` int(11) UNSIGNED NOT NULL,
+    `user_id` int(11) UNSIGNED NOT NULL,
+    `media_date_added` datetime NOT NULL,
+    `media_type` varchar(100) NOT NULL,
+    `media_name` varchar(240) NOT NULL,
+    `media_extension` varchar(20) NOT NULL,
+    `file_size` int(11) UNSIGNED NOT NULL DEFAULT 0,
+    `keywords` varchar(500) NOT NULL DEFAULT '',
+    `description` varchar(1200) NOT NULL DEFAULT ''
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
@@ -381,6 +381,17 @@ CREATE TABLE `media_configurations` (
     `config_key` varchar(32) NOT NULL,
     `config` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `media_tags`
+--
+
+CREATE TABLE `media_tags` (
+    `media_id` int(11) UNSIGNED NOT NULL,
+    `tag_id` int(11) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci COMMENT='Connects media documents with tags.';
 
 -- --------------------------------------------------------
 
@@ -406,17 +417,6 @@ CREATE TABLE `revisionables_changelog` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `revisionables_current_revisions`
---
-
-CREATE TABLE `revisionables_current_revisions` (
-    `revisionable_id` int(11) UNSIGNED NOT NULL,
-    `current_revision` int(11) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `revisionables_revisions`
 --
 
@@ -425,12 +425,48 @@ CREATE TABLE `revisionables_revisions` (
     `revisionable_revision` int(11) UNSIGNED NOT NULL,
     `pretty_revision` int(11) UNSIGNED NOT NULL,
     `label` varchar(160) NOT NULL,
+    `alias` varchar(160) NOT NULL,
     `state` enum('draft','finalized','inactive','deleted') NOT NULL DEFAULT 'draft',
     `date` datetime NOT NULL,
     `author` int(11) UNSIGNED NOT NULL,
     `comments` text NOT NULL,
-    `structural` VARCHAR(400) NOT NULL DEFAULT ''
+    `structural` varchar(400) NOT NULL DEFAULT ''
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tags`
+--
+
+CREATE TABLE `tags` (
+    `tag_id` int(11) UNSIGNED NOT NULL,
+    `label` varchar(160) NOT NULL,
+    `parent_tag_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tags_registry`
+--
+
+CREATE TABLE `tags_registry` (
+    `tag_id` int(11) UNSIGNED NOT NULL,
+    `registry_key` varchar(180) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tags_translations`
+--
+
+CREATE TABLE `tags_translations` (
+    `tag_id` int(11) UNSIGNED NOT NULL,
+    `locale_name` varchar(5) NOT NULL,
+    `locale_label` varchar(160) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci COMMENT='Stores tag label translations.';
 
 -- --------------------------------------------------------
 
@@ -454,6 +490,17 @@ CREATE TABLE `test_records_data` (
     `record_id` int(11) UNSIGNED NOT NULL,
     `name` varchar(250) NOT NULL,
     `value` mediumtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `test_records_tags`
+--
+
+CREATE TABLE `test_records_tags` (
+    `record_id` int(11) UNSIGNED NOT NULL,
+    `tag_id` int(11) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
@@ -713,6 +760,14 @@ ALTER TABLE `media_configurations`
     ADD KEY `type_id` (`type_id`,`config_key`);
 
 --
+-- Indexes for table `media_tags`
+--
+ALTER TABLE `media_tags`
+    ADD PRIMARY KEY (`media_id`,`tag_id`),
+    ADD KEY `media_id` (`media_id`),
+    ADD KEY `tag_id` (`tag_id`);
+
+--
 -- Indexes for table `revisionables`
 --
 ALTER TABLE `revisionables`
@@ -726,23 +781,40 @@ ALTER TABLE `revisionables_changelog`
     ADD KEY `revisionable_id` (`revisionable_id`);
 
 --
--- Indexes for table `revisionables_current_revisions`
---
-ALTER TABLE `revisionables_current_revisions`
-    ADD KEY `revisionable_id` (`revisionable_id`),
-    ADD KEY `revisionable_revision` (`current_revision`);
-
---
 -- Indexes for table `revisionables_revisions`
 --
 ALTER TABLE `revisionables_revisions`
     ADD PRIMARY KEY (`revisionable_revision`),
     ADD KEY `revisionable_id` (`revisionable_id`),
     ADD KEY `label` (`label`),
+    ADD KEY `alias` (`alias`),
     ADD KEY `state` (`state`),
     ADD KEY `date` (`date`),
     ADD KEY `author` (`author`),
     ADD KEY `pretty_revision` (`pretty_revision`);
+
+--
+-- Indexes for table `tags`
+--
+ALTER TABLE `tags`
+    ADD PRIMARY KEY (`tag_id`),
+    ADD KEY `label` (`label`),
+    ADD KEY `parent_tag_id` (`parent_tag_id`);
+
+--
+-- Indexes for table `tags_registry`
+--
+ALTER TABLE `tags_registry`
+    ADD PRIMARY KEY (`registry_key`),
+    ADD KEY `tag_id` (`tag_id`);
+
+--
+-- Indexes for table `tags_translations`
+--
+ALTER TABLE `tags_translations`
+    ADD KEY `tag_id` (`tag_id`),
+    ADD KEY `locale_name` (`locale_name`),
+    ADD KEY `locale_label` (`locale_label`);
 
 --
 -- Indexes for table `test_records`
@@ -759,6 +831,14 @@ ALTER TABLE `test_records_data`
     ADD PRIMARY KEY (`record_id`,`name`),
     ADD KEY `record_id` (`record_id`),
     ADD KEY `name` (`name`);
+
+--
+-- Indexes for table `test_records_tags`
+--
+ALTER TABLE `test_records_tags`
+    ADD PRIMARY KEY (`record_id`,`tag_id`),
+    ADD KEY `record_id` (`record_id`),
+    ADD KEY `tag_id` (`tag_id`);
 
 --
 -- Indexes for table `uploads`
@@ -896,6 +976,12 @@ ALTER TABLE `revisionables_revisions`
     MODIFY `revisionable_revision` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `tags`
+--
+ALTER TABLE `tags`
+    MODIFY `tag_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `test_records`
 --
 ALTER TABLE `test_records`
@@ -990,11 +1076,11 @@ ALTER TABLE `media`
     ADD CONSTRAINT `media_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `known_users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `revisionables_current_revisions`
+-- Constraints for table `media_tags`
 --
-ALTER TABLE `revisionables_current_revisions`
-    ADD CONSTRAINT `revisionables_current_revisions_ibfk_1` FOREIGN KEY (`revisionable_id`) REFERENCES `revisionables` (`revisionable_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-    ADD CONSTRAINT `revisionables_current_revisions_ibfk_2` FOREIGN KEY (`current_revision`) REFERENCES `revisionables_revisions` (`revisionable_revision`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `media_tags`
+    ADD CONSTRAINT `media_tags_ibfk_1` FOREIGN KEY (`media_id`) REFERENCES `media` (`media_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+    ADD CONSTRAINT `media_tags_ibfk_2` FOREIGN KEY (`tag_id`) REFERENCES `tags` (`tag_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `revisionables_revisions`
@@ -1004,10 +1090,24 @@ ALTER TABLE `revisionables_revisions`
     ADD CONSTRAINT `revisionables_revisions_ibfk_2` FOREIGN KEY (`author`) REFERENCES `known_users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
+-- Constraints for table `tags_translations`
+--
+ALTER TABLE `tags_translations`
+    ADD CONSTRAINT `tags_translations_ibfk_1` FOREIGN KEY (`tag_id`) REFERENCES `tags` (`tag_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+    ADD CONSTRAINT `tags_translations_ibfk_2` FOREIGN KEY (`locale_name`) REFERENCES `locales_application` (`locale_name`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
 -- Constraints for table `test_records_data`
 --
 ALTER TABLE `test_records_data`
     ADD CONSTRAINT `test_records_data_ibfk_1` FOREIGN KEY (`record_id`) REFERENCES `test_records` (`record_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `test_records_tags`
+--
+ALTER TABLE `test_records_tags`
+    ADD CONSTRAINT `test_records_tags_ibfk_1` FOREIGN KEY (`record_id`) REFERENCES `test_records` (`record_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+    ADD CONSTRAINT `test_records_tags_ibfk_2` FOREIGN KEY (`tag_id`) REFERENCES `tags` (`tag_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `uploads`
