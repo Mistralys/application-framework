c3a7fac38f63f0db70e28923fc40c4aee6070bb9
Mistralys <s.mordziol@gmail.com>
2024-03-19 11:13:36 +0100

    Added the locales collection.

---
+++ src/classes/Application/Locales.php
@@ -0,0 +1,97 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application;
+
+use Application\Languages\Language;
+use Application\Locales\Locale;
+use AppUtils\Collections\BaseStringPrimaryCollection;
+
+/**
+ * Locale collection class, used to fetch information on the
+ * available locales in the system (based on the available
+ * countries).
+ *
+ * NOTE: The available locales are adjusted automatically
+ * based on changes in the country collection.
+ *
+ * @package Application
+ * @subpackage Countries
+ *
+ * @method Locale getByID(string $id)
+ * @method Locale getDefault()
+ * @method Locale[] getAll()
+ */
+class Locales extends BaseStringPrimaryCollection
+{
+    public const LOCALE_EN_US = 'en_US';
+    public const LOCALE_EN_GB = 'en_GB';
+    public const LOCALE_EN_UK = 'en_UK';
+    public const LOCALE_DE_DE = 'de_DE';
+    public const LOCALE_DE_AT = 'de_AT';
+    public const LOCALE_FR_FR = 'fr_FR';
+    public const LOCALE_IT_IT = 'it_IT';
+    public const LOCALE_ES_ES = 'es_ES';
+    public const LOCALE_ES_MX = 'es_MX';
+    public const LOCALE_PL_PL = 'pl_PL';
+    public const LOCALE_RO_RO = 'ro_RO';
+
+    private static ?Locales $instance = null;
+
+    public static function getInstance() : Locales
+    {
+        if(!isset(self::$instance)) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+    public function __construct()
+    {
+        $countries = AppFactory::createCountries();
+
+        // Register events to reset the collection when the countries change
+        $countries->onIgnoredCountriesUpdated(array($this, 'reset'));
+        $countries->onAfterCreateRecord(array($this, 'reset'));
+        $countries->onAfterDeleteRecord(array($this, 'reset'));
+    }
+
+    public function reset() : void
+    {
+        unset($this->items);
+    }
+
+    public function getDefaultID(): string
+    {
+        return self::LOCALE_EN_US;
+    }
+
+    /**
+     * @param Language $language
+     * @return Locale[]
+     */
+    public function getByLanguage(Language $language) : array
+    {
+        $locales = array();
+        $iso = $language->getISO();
+
+        foreach($this->getAll() as $locale) {
+            if($locale->getLangISO() === $iso) {
+                $locales[] = $locale;
+            }
+        }
+
+        return $locales;
+    }
+
+    protected function registerItems(): void
+    {
+        $countries = AppFactory::createCountries()->getAll();
+
+        foreach($countries as $country) {
+            $this->registerItem(new Locale($country->getLocaleCode()));
+        }
+    }
+}
---
+++ src/classes/Application/Locales/Locale.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * @package Application
+ * @subpackage Countries
+ */
+
+declare(strict_types=1);
+
+namespace Application\Locales;
+
+use Application\AppFactory;
+use Application\Languages\Language;
+use Application_Countries_Country;
+use AppUtils\Interfaces\StringPrimaryRecordInterface;
+
+/**
+ * Holds information about a single locale.
+ *
+ * This is a composite of the language and the country.
+ * It is used to represent a country-specific language,
+ * aka a locale.
+ *
+ * Example: "en_US" for English in the United States.
+ *
+ * @package Application
+ * @subpackage Countries
+ */
+class Locale implements StringPrimaryRecordInterface
+{
+    private string $localeID;
+    private string $langISO;
+    private string $countryISO;
+    private ?Language $language = null;
+    private ?Application_Countries_Country $country = null;
+    private ?string $label = null;
+
+    public function __construct(string $localeID)
+    {
+        $this->localeID = $localeID;
+
+        $parts = explode('_', $localeID);
+        $this->langISO = strtolower($parts[0]);
+        $this->countryISO = strtolower($parts[1]);
+    }
+
+    public function getID(): string
+    {
+        return $this->localeID;
+    }
+
+    public function getLabel(): string
+    {
+        if (!isset($this->label)) {
+            $this->label = sprintf(
+                '%s (%s)',
+                $this->getLanguage()->getLabel(),
+                strtoupper($this->getCountry()->getISO())
+            );
+        }
+
+        return $this->label;
+    }
+
+    public function getLangISO(): string
+    {
+        return $this->langISO;
+    }
+
+    public function getCountryISO(): string
+    {
+        return $this->countryISO;
+    }
+
+    public function getLanguage(): Language
+    {
+        if (!isset($this->language)) {
+            $this->language = AppFactory::createLanguages()->getByISO($this->getLangISO());
+        }
+
+        return $this->language;
+    }
+
+    public function getCountry(): Application_Countries_Country
+    {
+        if (!isset($this->country)) {
+            $this->country = AppFactory::createCountries()->getByISO($this->getCountryISO());
+        }
+
+        return $this->country;
+    }
+}
