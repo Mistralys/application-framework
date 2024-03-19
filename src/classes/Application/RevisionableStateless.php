5e94649af389050e85fc3c1e7ee73be7c5160718
Mistralys <s.mordziol@gmail.com>
2024-03-19 14:36:59 +0100

    Updated to use the dependent trait.

--- src/classes/Application/RevisionableStateless.php
+++ src/classes/Application/RevisionableStateless.php
@@ -13,7 +13,7 @@ use Application\Revisionable\RevisionableChangelogTrait;
 use Application\Revisionable\RevisionableException;
 use Application\Revisionable\RevisionableStatelessInterface;
 use Application\Revisionable\RevisionableStorageException;
-use Application\Revisionable\RevisionDependentInterface;
+use Application\Traits\RevisionDependentTrait;
 use AppUtils\ConvertHelper;
 use AppUtils\ConvertHelper_Exception;
 use AppUtils\NamedClosure;
@@ -39,6 +39,7 @@ abstract class Application_RevisionableStateless
     use Application_Traits_Loggable;
     use Application_Traits_Simulatable;
     use RevisionableChangelogTrait;
+    use RevisionDependentTrait;
 
     public const STORAGE_PART_CUSTOM_KEYS = 'customKeys';
     public const STORAGE_PART_DATA_KEYS = 'revdata';
@@ -1307,52 +1308,6 @@ abstract class Application_RevisionableStateless
 
         $this->revisions->setKey(self::REVISION_KEY_EVENT_HANDLERS, $handlers);
         return $this;
-    }
-
-    /**
-     * @inheritDoc
-     * @return $this
-     */
-    public function requireRevisionableMatch(RevisionDependentInterface $dependent) : self
-    {
-        if(get_class($dependent->getRevisionable()) === get_class($this)) {
-            return $this;
-        }
-
-        throw new RevisionableException(
-            'Revisionable class mismatch',
-            sprintf(
-                'The revisionable class [%s] of the dependency does not match the current revisionable\'s class [%s].',
-                get_class($dependent->getRevisionable()),
-                get_class($this)
-            ),
-            RevisionableStatelessInterface::ERROR_DEPENDENT_CLASS_MISMATCH
-        );
-    }
-
-    /**
-     * @inheritDoc
-     * @return $this
-     */
-    public function requireRevisionMatch(RevisionDependentInterface $dependent) : self
-    {
-        $this->requireRevisionableMatch($dependent);
-
-        if($dependent->getRevision() === $this->getRevision()) {
-            return $this;
-        }
-
-        throw new RevisionableException(
-            'Revision mismatch',
-            sprintf(
-                'The revision [%s] of the dependency [%s] does not match the current revision [%s] in revisionable [%s].',
-                $dependent->getRevision(),
-                get_class($dependent),
-                $this->getRevision(),
-                $this->getIdentification()
-            ),
-            RevisionableStatelessInterface::ERROR_DEPENDENT_REVISION_MISMATCH
-        );
     }
 
     // endregion
