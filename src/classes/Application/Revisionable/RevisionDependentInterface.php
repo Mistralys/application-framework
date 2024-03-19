73c30db9feb928a3f29dedfbd625a70d9efd074f
Mistralys <s.mordziol@gmail.com>
2024-03-19 14:36:40 +0100

    Moved the require methods to a trait.

--- src/classes/Application/Revisionable/RevisionDependentInterface.php
+++ src/classes/Application/Revisionable/RevisionDependentInterface.php
@@ -8,6 +8,8 @@ declare(strict_types=1);
 
 namespace Application\Revisionable;
 
+use Application\Traits\RevisionDependentTrait;
+
 /**
  * Interface for objects that are dependent on a revisionable's revision.
  *
@@ -15,9 +17,33 @@ namespace Application\Revisionable;
  *
  * @package Application
  * @subpackage Revisionables
+ *
+ * @see RevisionDependentTrait
  */
 interface RevisionDependentInterface
 {
     public function getRevision() : ?int;
     public function getRevisionable() : RevisionableStatelessInterface;
+
+    /**
+     * Verifies that the revisionable connected to the revision-dependent
+     * object is of the same class.
+     *
+     * @param RevisionDependentInterface $dependent
+     * @return self
+     * @throws RevisionableException {@see self::ERROR_DEPENDENT_CLASS_MISMATCH}
+     */
+    public function requireRevisionableMatch(RevisionDependentInterface $dependent) : self;
+
+    /**
+     * Verifies that the revision-dependent object has the same
+     * revision number as the revisionable.
+     *
+     * NOTE: Also checks that the revisionable matches using {@see self::requireRevisionableMatch()}.
+     *
+     * @param RevisionDependentInterface $dependent
+     * @return $this
+     * @throws RevisionableException {@see self::ERROR_DEPENDENT_REVISION_MISMATCH} and {@see self::ERROR_DEPENDENT_CLASS_MISMATCH}
+     */
+    public function requireRevisionMatch(RevisionDependentInterface $dependent) : self;
 }
--- src/classes/Application/Revisionable/RevisionableStatelessInterface.php
+++ src/classes/Application/Revisionable/RevisionableStatelessInterface.php
@@ -181,26 +181,4 @@ interface RevisionableStatelessInterface
     public function getCustomKeyValues(): array;
 
     public function handle_revisionLoaded(int $number): void;
-
-    /**
-     * Verifies that the revisionable connected to the revision-dependent
-     * object is of the same class.
-     *
-     * @param RevisionDependentInterface $dependent
-     * @return self
-     * @throws RevisionableException {@see self::ERROR_DEPENDENT_CLASS_MISMATCH}
-     */
-    public function requireRevisionableMatch(RevisionDependentInterface $dependent) : self;
-
-    /**
-     * Verifies that the revision-dependent object has the same
-     * revision number as the revisionable.
-     *
-     * NOTE: Also checks that the revisionable matches using {@see self::requireRevisionableMatch()}.
-     *
-     * @param RevisionDependentInterface $dependent
-     * @return $this
-     * @throws RevisionableException {@see self::ERROR_DEPENDENT_REVISION_MISMATCH} and {@see self::ERROR_DEPENDENT_CLASS_MISMATCH}
-     */
-    public function requireRevisionMatch(RevisionDependentInterface $dependent) : self;
 }
