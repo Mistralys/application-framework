f64b25e407fbcb2bdb7279fe23b78a13d429c854
Mistralys <s.mordziol@gmail.com>
2024-03-30 14:18:19 +0100

    Removed `init()`, not needed anymore.

--- src/classes/UI/PropertiesGrid/Property/Amount.php
+++ src/classes/UI/PropertiesGrid/Property/Amount.php
@@ -4,11 +4,6 @@ declare(strict_types=1);
 
 class UI_PropertiesGrid_Property_Amount extends UI_PropertiesGrid_Property
 {
-    protected function init() : void
-    {
-        $this->ifEmpty(sb()->muted('('.t('Empty').')'));
-    }
-    
     protected function filterValue($value) : UI_StringBuilder
     {
         return sb()->code($value);
