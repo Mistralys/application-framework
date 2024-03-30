89d75e2e6c197f6c1eea644987ae985294e3640c
Mistralys <s.mordziol@gmail.com>
2024-03-30 14:18:46 +0100

    Added the tags property.

---
+++ src/classes/UI/PropertiesGrid/Property/TagsGridProperty.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * @package User Interface
+ * @subpackage Properties Grid
+ */
+
+declare(strict_types=1);
+
+namespace UI\PropertiesGrid\Property;
+
+use Application\Tags\Taggables\TaggableInterface;
+use UI_PropertiesGrid_Property;
+use UI_StringBuilder;
+
+/**
+ * Displays all tags of a tabbable object.
+ *
+ * NOTE: Shown only if the tagging is enabled for the object.
+ *
+ * @package User Interface
+ * @subpackage Properties Grid
+ */
+class TagsGridProperty extends UI_PropertiesGrid_Property
+{
+    protected function init(): void
+    {
+        if($this->text instanceof TaggableInterface) {
+            $this->requireTrue($this->text->isTaggingEnabled(), 'Tagging is not enabled for this object');
+        }
+    }
+
+    protected function filterValue($value): UI_StringBuilder
+    {
+        if(!$value instanceof TaggableInterface) {
+            return $this->resolveEmptyText();
+        }
+
+        $tags = $value->getTagManager()->getAll();
+        if(empty($tags)) {
+            return $this->resolveEmptyText();
+        }
+
+        $list = array();
+        foreach($tags as $tag) {
+            $list[] = $tag->getLabel();
+        }
+
+        return sb()->add(implode(', ', $list));
+    }
+}
