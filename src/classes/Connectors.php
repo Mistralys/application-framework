c49ce434644baeb83fcf7620a1fae86651ce5f77
Mistralys <s.mordziol@gmail.com>
2024-04-15 09:20:59 +0200

    `createConnector()` now accepts class names.

--- src/classes/Connectors.php
+++ src/classes/Connectors.php
@@ -1,12 +1,13 @@
 <?php
 /**
- * File containing the {@link Connectors} class.
- * 
  * @package Connectors
- * @see Connectors
  */
 
-use Application\Exception\UnexpectedInstanceException;
+declare(strict_types=1);
+
+use AppUtils\ClassHelper;
+use AppUtils\ClassHelper\BaseClassHelperException;
+use Connectors\Connector\StubConnector;
 
 /**
  * External API connectors manager: handles access to
@@ -20,50 +21,47 @@ class Connectors
 {
     public const ERROR_INVALID_CONNECTOR_TYPE = 100701;
 
-    protected static $connectors = array();
-    
-   /**
-    * Creates/gets the connector of the specified type. The type is
-    * the name of the connector file, case-sensitive. Will throw
-    * an exception if the type does not exist.
-    * 
-    * @param string $type
-    * @return Connectors_Connector
-    *
-    * @throws UnexpectedInstanceException
-    * @see Connectors::ERROR_INVALID_CONNECTOR_TYPE
-    */
-    public static function createConnector(string $type) : Connectors_Connector
+    /**
+     * @var array<string,Connectors_Connector>
+     */
+    protected static array $connectors = array();
+
+    /**
+     * Creates/gets the connector of the specified type. The type is
+     * the name of the connector file, case-sensitive. Will throw
+     * an exception if the type does not exist.
+     *
+     * @param string|class-string $typeOrClass Connector type ID or class name.
+     * @return Connectors_Connector
+     *
+     * @throws BaseClassHelperException
+     * @see Connectors::ERROR_INVALID_CONNECTOR_TYPE
+     */
+    public static function createConnector(string $typeOrClass) : Connectors_Connector
     {
-        if(isset(self::$connectors[$type]))
+        if(isset(self::$connectors[$typeOrClass]))
         {
-            return self::$connectors[$type];
+            return self::$connectors[$typeOrClass];
         }
 
-        $class = Connectors_Connector::class.'_'.$type;
-        $connector = new $class();
-
-        if($connector instanceof Connectors_Connector)
-        {
-            return $connector;
+        if(class_exists($typeOrClass)) {
+            $class = $typeOrClass;
+        } else {
+            $class = ClassHelper::requireResolvedClass('Connectors_Connector_' . $typeOrClass);
         }
 
-        throw new UnexpectedInstanceException(
+        return ClassHelper::requireObjectInstanceOf(
             Connectors_Connector::class,
-            $connector,
+            new $class(),
             self::ERROR_INVALID_CONNECTOR_TYPE
         );
     }
 
-    public static function createDummyConnector() : Connectors_Connector_Dummy
+    public static function createStubConnector() : StubConnector
     {
-        $dummy = self::createConnector('Dummy');
-
-        if($dummy instanceof Connectors_Connector_Dummy)
-        {
-            return $dummy;
-        }
-
-        throw new UnexpectedInstanceException(Connectors_Connector_Dummy::class, $dummy);
+        return ClassHelper::requireObjectInstanceOf(
+            StubConnector::class,
+            self::createConnector(StubConnector::class)
+        );
     }
 }
