572b7f1fc8d068d52648e5b79dad6dfb22c34bb1
Mistralys <s.mordziol@gmail.com>
2024-05-14 11:01:21 +0200

    Renamed and namespaced.

---
+++ src/classes/Application/Revisionable/Event/RevisionAddedEvent.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\Revisionable\Event;
+
+use Application\Revisionable\RevisionableStatelessInterface;
+use Application_EventHandler_EventableEvent;
+use Application_RevisionStorage_Event_RevisionAdded;
+
+class RevisionAddedEvent extends Application_EventHandler_EventableEvent
+{
+    public function getRevisionable(): RevisionableStatelessInterface
+    {
+        return $this->getArgumentObject(0, RevisionableStatelessInterface::class);
+    }
+
+    public function getNumber(): int
+    {
+        return $this->getOriginalEvent()->getNumber();
+    }
+
+    public function getTimestamp(): int
+    {
+        return $this->getOriginalEvent()->getTimestamp();
+    }
+
+    public function getOwnerID(): int
+    {
+        return $this->getOriginalEvent()->getOwnerID();
+    }
+
+    public function getComments(): string
+    {
+        return $this->getOriginalEvent()->getComments();
+    }
+
+    public function getOwnerName(): string
+    {
+        return $this->getOriginalEvent()->getOwnerName();
+    }
+
+    public function getOriginalEvent(): Application_RevisionStorage_Event_RevisionAdded
+    {
+        return $this->getArgumentObject(1, Application_RevisionStorage_Event_RevisionAdded::class);
+    }
+
+    public function isCancellable(): bool
+    {
+        return false;
+    }
+}
