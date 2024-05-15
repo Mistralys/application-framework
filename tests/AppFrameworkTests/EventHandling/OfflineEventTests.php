722cc1f906937563fa1f7286069807f859169d89
Mistralys <s.mordziol@gmail.com>
2024-05-15 18:06:46 +0200

    Added / updated tests.

--- tests/AppFrameworkTests/EventHandling/OfflineEventTests.php
+++ tests/AppFrameworkTests/EventHandling/OfflineEventTests.php
@@ -2,29 +2,60 @@
 
 declare(strict_types=1);
 
+namespace AppFrameworkTests\EventHandling;
+
 use AppFrameworkTestClasses\ApplicationTestCase;
+use Application\AppFactory;
+use TestDriver\OfflineEvents\TestEvent;
+use TestDriver\OfflineEvents\TestEvent\ListenerA;
+use TestDriver\OfflineEvents\TestEvent\ListenerB;
 
-final class EventHandling_OfflineEventTests extends ApplicationTestCase
+/**
+ * @see TestEvent
+ * @see ListenerA
+ * @see ListenerB
+ */
+final class OfflineEventTests extends ApplicationTestCase
 {
-    /**
-     * @see TestDriver_OfflineEvents_TestEvent_ListenerA
-     * @see TestDriver_OfflineEvents_TestEvent_ListenerB
-     */
-    public function test_removeListener() : void
+    public function test_createEventWithoutClassName(): void
     {
-        $offline = Application_EventHandler::createOfflineEvents();
-
-        $this->assertNotNull($offline->createEvent('TestEvent', array('argument')));
+        $this->assertNotNull(AppFactory::createOfflineEvents()
+            ->createEvent(
+                TestEvent::EVENT_NAME,
+                array('argument')
+            ));
     }
 
-    public function test_trigger() : void
+    public function test_createEventWithClassName(): void
     {
-        $offline = Application_EventHandler::createOfflineEvents();
+        $this->assertNotNull(AppFactory::createOfflineEvents()
+            ->createEvent(
+                TestEvent::EVENT_NAME,
+                array('argument'),
+                TestEvent::class
+            ));
+    }
 
-        $event = $offline->triggerEvent('TestEvent', array('argument'));
+    public function test_eventHasExpectedListeners(): void
+    {
+        $event = AppFactory::createOfflineEvents()->createEvent(TestEvent::EVENT_NAME, array('argument'));
 
+        $this->assertNotNull($event);
+        $this->assertNotNull($event->getEventClass());
+        $this->assertNotEmpty($event->getListenerFolders());
+        $this->assertNotEmpty($event->getListeners());
+    }
+
+    public function test_triggerEvent(): void
+    {
+        $offline = AppFactory::createOfflineEvents();
+
+        $event = $offline->triggerEvent(TestEvent::EVENT_NAME, array('argument'));
+
+        $this->assertNotNull($event);
         $this->assertNotNull($event->getTriggeredEvent());
-        $this->assertTrue(defined('OFFLINE_EVENTS_LISTENER_A_TRIGGERED'));
-        $this->assertEquals('argument', constant('OFFLINE_EVENTS_LISTENER_B_ARGUMENT'));
+        $this->assertTrue(defined(ListenerA::CONSTANT_NAME));
+        $this->assertTrue(defined(ListenerB::CONSTANT_NAME));
+        $this->assertEquals('argument', constant(ListenerB::CONSTANT_NAME));
     }
 }
