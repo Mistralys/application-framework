6b2f35776614cddb6e6d02c27ac0f45400b710f5
Mistralys <s.mordziol@gmail.com>
2024-05-21 14:00:19 +0200

    Added the `AllowableInterface` #62
    Mandatory methods are commented out for now.

---
+++ src/classes/Application/Interfaces/AllowableInterface.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\Interfaces;
+
+use Application_User;
+
+interface AllowableInterface
+{
+    /**
+     * Returns a list of all rights required to access the resource.
+     *
+     * @return string[] List of right names.
+     */
+    //public function getRequiredRights() : array;
+
+    public function isUserAllowed() : bool;
+
+    /**
+     * Returns a list of (optional) additional rights required
+     * to access specific features available in the resource.
+     *
+     * @return array<string, string> List of right name > feature description pairs
+     */
+    //public function getFeatureRights() : array;
+
+    public function getUser() : Application_User;
+}
