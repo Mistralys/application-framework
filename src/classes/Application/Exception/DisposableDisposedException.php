7df8c25404196cd11ab33353d241ca910ebce895
Mistralys <s.mordziol@gmail.com>
2024-06-21 10:15:19 +0200

    Renamed and deprecated.

--- src/classes/Application/Exception/DisposableDisposedException.php
+++ src/classes/Application/Exception/DisposableDisposedException.php
@@ -2,22 +2,30 @@
 /**
  * @package Application
  * @subpackage Disposable
- * @see Application_Exception_DisposableDisposed
  */
 
 declare(strict_types=1);
+
+namespace Application\Exception;
+
+use Application_Exception;
+use Application_Interfaces_Disposable;
 
 /**
  * @package Application
  * @subpackage Disposable
  * @author Sebastian Mordziol <s.mordziol@mistralys.eu>
  */
-class Application_Exception_DisposableDisposed extends Application_Exception
+class DisposableDisposedException extends Application_Exception
 {
     public const ERROR_DISPOSABLE_DISPOSED = 92101;
 
-    public function __construct(Application_Interfaces_Disposable $disposable, $actionLabel)
+    public function __construct(Application_Interfaces_Disposable $disposable, ?string $actionLabel)
     {
+        if (empty($actionLabel)) {
+            $actionLabel = '(not specified)';
+        }
+
         parent::__construct(
             'Object has been disposed',
             sprintf(
