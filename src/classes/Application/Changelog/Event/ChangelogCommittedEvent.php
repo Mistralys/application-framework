91707c7add748f52f381acc61f93908964ab00e7
Mistralys <s.mordziol@gmail.com>
2024-07-01 15:06:55 +0200

    Added the `ChangelogCommitted` event.

---
+++ src/classes/Application/Changelog/Event/ChangelogCommittedEvent.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\Changelog\Event;
+
+use Application_EventHandler_EventableEvent;
+
+class ChangelogCommittedEvent extends Application_EventHandler_EventableEvent
+{
+    public const EVENT_NAME = 'ChangelogCommitted';
+}
