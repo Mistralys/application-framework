0013be7a2ab30757f5db79c3d3d0c58aa2b17a2d
Mistralys <s.mordziol@gmail.com>
2024-07-08 09:49:17 +0200

    Extracted `setName()` to its own interface.
    To solve inheritance issues, so the method can be added to other unrelated interfaces and stay compatible.

--- src/classes/UI/Bootstrap/ButtonGroup/ButtonGroupItemInterface.php
+++ src/classes/UI/Bootstrap/ButtonGroup/ButtonGroupItemInterface.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * @package User Interface
+ * @subpackage Interfaces
+ */
 
 declare(strict_types=1);
 
@@ -7,18 +11,19 @@ namespace UI\Bootstrap\ButtonGroup;
 use AppUtils\Interfaces\RenderableInterface;
 use UI\Interfaces\ActivatableInterface;
 use UI\Interfaces\ButtonSizeInterface;
+use UI\Interfaces\NamedItemInterface;
 
+/**
+ * Interface for items that can be added to a button group.
+ *
+ * @package User Interface
+ * @subpackage Interfaces
+ */
 interface ButtonGroupItemInterface
     extends
     ButtonSizeInterface,
     ActivatableInterface,
-    RenderableInterface
+    RenderableInterface,
+    NamedItemInterface
 {
-    public function getName() : string;
-
-    /**
-     * @param string $name
-     * @return self
-     */
-    public function setName(string $name) : self;
 }
---
+++ src/classes/UI/Interfaces/NamedItemInterface.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @package User Interface
+ * @subpackage Interfaces
+ */
+
+declare(strict_types=1);
+
+namespace UI\Interfaces;
+
+/**
+ * Interface for elements that can be given a name.
+ *
+ * @package User Interface
+ * @subpackage Interfaces
+ */
+interface NamedItemInterface
+{
+    /**
+     * Sets the element's name, which can be used to retrieve it when used in collections.
+     * @param string $name
+     * @return $this
+     */
+    public function setName(string $name) : self;
+
+    /**
+     * @return string
+     */
+    public function getName() : string;
+}
