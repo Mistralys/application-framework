e5e9104329a84283e4f91843870270ea56395ef5
Mistralys <s.mordziol@gmail.com>
2024-07-09 08:58:30 +0200

    Authentication process updated:
    - Removed redundant steps.
    - Clarified the process through better method naming.
    - Fixed role presets not being applied.
    - Removed simulating users, too error prone and risky.
    - Storing devel mode flag in the session.

--- src/classes/Application/Session/Base.php
+++ src/classes/Application/Session/Base.php
@@ -10,10 +10,14 @@
 declare(strict_types=1);
 
 use Application\AppFactory;
+use Application\AppFactory\AppFactoryException;
+use Application\Environments;
 use Application\Session\Events\BeforeLogOutEvent;
 use Application\Session\Events\SessionStartedEvent;
 use Application\Session\Events\UserAuthenticatedEvent;
+use Application\User\Role\DeveloperRole;
 use AppUtils\ConvertHelper;
+use AppUtils\Request;
 use function AppUtils\parseURL;
 
 /**
@@ -44,6 +48,9 @@ abstract class Application_Session_Base implements Application_Session
     public const ERROR_INVALID_USER_ID = 22207;
     public const ERROR_NO_USER_AVAILABLE = 22208;
     public const ERROR_PRESET_NOT_DEFINED = 22209;
+    public const ERROR_CANNOT_AUTHENTICATE_TWICE = 22210;
+
+    public const REQUEST_PARAM_RIGHTS_PRESET = 'roleID';
 
     public const KEY_NAME_USER_ID = 'user_id';
     public const KEY_NAME_USER_RIGHTS = 'user_rights';
@@ -51,11 +58,9 @@ abstract class Application_Session_Base implements Application_Session
     public const KEY_NAME_RIGHTS_PRESET = 'simulate_rights_preset';
     public const KEY_NAME_AUTH_RETURN_URI = 'auth_return_url';
 
-    public const ADMIN_PRESET_ID = 'Admin';
-    public const QA_TESTING_PRESET_ID = 'QATesting';
-
     public const LOGOUT_REASON_USER_REQUEST = 75901;
     public const LOGOUT_REASON_LOGIN_NOT_ALLOWED = 75902;
+
 
     /**
      * @var array<int,string>
@@ -66,13 +71,8 @@ abstract class Application_Session_Base implements Application_Session
     );
 
     protected int $defaultSimulatedUser = Application::USER_ID_SYSTEM;
-
-    /**
-     * @var array<string,array<int,string>>
-     */
-    protected array $rightPresets = array();
-
     protected ?Application_User $user = null;
+    protected bool $started = false;
 
     final public function start() : self
     {
@@ -80,6 +80,7 @@ abstract class Application_Session_Base implements Application_Session
         $this->_start();
         $this->log('Session started with ID [%s].', $this->getID());
 
+        $this->started = true;
         $this->triggerStarted();
 
         return $this;
@@ -90,20 +91,15 @@ abstract class Application_Session_Base implements Application_Session
     abstract protected function handleLogout(array $clearKeys=array()) : void;
 
     /**
-     * Handles the logic to log in the user. This must
-     * call `storeUser()` when a user has been successfully
-     * logged in.
-     *
-     * It should also handle registering new users, if
-     * this is applicable.
-     */
-    abstract protected function handleLogin() : ?Application_Users_User;
-
-    /**
      * @see Application_Bootstrap_Screen::initSession()
      */
     public function __construct()
     {
+    }
+
+    public function isStarted() : bool
+    {
+        return $this->started;
     }
 
     public function logOut(int $reasonID=self::LOGOUT_REASON_USER_REQUEST): void
@@ -115,6 +111,10 @@ abstract class Application_Session_Base implements Application_Session
         $this->log('Logout requested, logging the user out.');
 
         $this->triggerBeforeLogOut($this->user, $reasonID);
+
+        // Reset the developer mode
+        $this->user->setDeveloperModeEnabled(false);
+        $this->user->saveSettings();
 
         $this->handleLogout(array(
             self::KEY_NAME_AUTH_RETURN_URI,
@@ -137,33 +137,121 @@ abstract class Application_Session_Base implements Application_Session
         Application::redirect(APP_URL.'/registration-disabled.php');
     }
 
-    private function initAuthentication() : void
+    // region: Authentication
+
+    /**
+     * @inheritDoc
+     *
+     * @throws Application_Exception
+     * @throws Application_Session_Exception {@see self::ERROR_CANNOT_AUTHENTICATE_TWICE}
+     */
+    final public function authenticate() : Application_User
     {
+        $this->log('Authenticate | Starting authentication.');
+
+        if(isset($this->user)) {
+            throw new Application_Session_Exception(
+                'Cannot authenticate user a second time.',
+                '',
+                self::ERROR_CANNOT_AUTHENTICATE_TWICE
+            );
+        }
+
+        $userID = $this->getUserID();
+
+        // Starts the authentication process. This ends either by
+        // storing the user ID in the session via `storeUser()`,
+        // or via a redirect to an error page.
+        if ($userID === 0) {
+            $this->runAuthentication();
+        }
+
+        $this->log(sprintf('Authenticate | User ID [%s] found in session.', $this->getUserID()));
+
+        $this->user = $this->loadUserByID($userID);
+
+        $this->handleUserLoaded();
+
+        return $this->user;
+    }
+
+    /**
+     * Runs the authentication process in the following steps:
+     *
+     * 1. {@see self::sendAuthenticationCallbacks()}
+     * 2. {@see self::finalizeAuthentication()}
+     *
+     * @return void
+     * @throws AppFactoryException
+     * @throws Application_Session_Exception
+     */
+    private function runAuthentication() : void
+    {
+        $this->log('Authenticate | No user ID found in the session, running authentication.');
+
         $returnURI = $_SERVER['REQUEST_URI'] ?? '(none available)';
-
-        $this->log('No user ID found in the session, initiating login sequence.');
-        $this->log('Return URI is [%s].', $returnURI);
-
         $this->setValue(self::KEY_NAME_AUTH_RETURN_URI, $returnURI);
+        $this->log('Authenticate | Return URI is [%s].', $returnURI);
 
         if(Application::isAuthenticationEnabled()) {
-            $user = $this->handleLogin();
+            $user = $this->sendAuthenticationCallbacks();
         } else {
-            $user = AppFactory::createUsers()->getByID(Application::USER_ID_SYSTEM);
+            $this->log('Authenticate | Auth is disabled - using the system user.');
+            $user = AppFactory::createUsers()->getSystemUser();
         }
 
         if($user !== null)
         {
-            $this->storeUser($user);
+            $this->finalizeAuthentication($user);
             return;
         }
 
-        throw new Application_Exception(
+        throw new Application_Session_Exception(
             'Authentication layer failure',
             'The authentication did not return a user. The authenticator should handle displaying a message to the user if the authentication fails.',
             self::ERROR_AUTH_DID_NOT_RETURN_USER
         );
     }
+
+    /**
+     * Handles the logic and callback loops to authenticate the user.
+     *
+     * It also handles registering new users when the user being
+     * authenticated is not in the known users list yet (even if they
+     * are not allowed to log in, this is a later step).
+     */
+    abstract protected function sendAuthenticationCallbacks() : ?Application_Users_User;
+
+    /**
+     * Stores the specified user in the session as the authenticated user
+     * after the authentication process has completed successfully.
+     *
+     * @param Application_Users_User $user
+     */
+    private function finalizeAuthentication(Application_Users_User $user) : void
+    {
+        $userID = $user->getID();
+
+        $this->log(sprintf('User [%s] | Authentication successful.', $userID));
+
+        // Unpack the user, as processes after this may need to access
+        // the instance. The redirect, for example, needs to know whether
+        // the user is a developer.
+        $unpacked = $this->loadUserByID($userID);
+
+        // Store the user ID in the session.
+        $this->setValue(self::KEY_NAME_USER_ID, $userID);
+
+        $this->triggerUserAuthenticated($unpacked);
+
+        $this->log('User [%s] | Redirecting to the initially requested URL.', $userID);
+
+        if(!isCLI()) {
+            Application::redirect($this->unpackTargetURL());
+        }
+    }
+
+    // endregion
 
     /**
      * Retrieves the user ID as stored in the session, if any.
@@ -182,138 +270,69 @@ abstract class Application_Session_Base implements Application_Session
      * @return Application_User
      * @throws Application_Exception
      */
-    private function unpackUser() : Application_User
+    private function loadUserByID(int $userID) : Application_User
     {
-        $userID = $this->getUserID();
-
         $this->log('User [%s] | Unpacking the user and their rights.', $userID);
 
         $user = Application::createUser($userID);
-        $user->setRights($this->unpackRights());
+        $user->setRights($this->unpackRights($user));
 
         return $user;
     }
 
     /**
-     * Stores the specified user in the session as the authenticated user
-     * after the authentication process has completed successfully.
+     * Checks whether a role preset change has been
+     * requested. This can only be done in devel mode,
+     * so only a developer can initially enable this.
      *
-     * @param Application_Users_User $user
+     * It is necessary to switch back to a developer
+     * role to turn the developer mode back off (because
+     * non-developer roles do not have the right to
+     * do so).
      */
-    private function storeUser(Application_Users_User $user) : void
+    private function checkRolePresetChange() : void
     {
-        $userID = $user->getID();
-
-        $this->log(sprintf('User [%s] | Authentication successful.', $userID));
-
-        if(!Application::isSessionSimulated())
-        {
-            $rights = $this->fetchRights($user);
+        if(!isDevelMode()) {
+            return;
         }
-        else
+
+        $presetName = $this->getPresetByRequest();
+
+        if (empty($presetName)) {
+            return;
+        }
+
+        $this->log(sprintf('Authenticate | Selected rights preset [%s].', $presetName));
+
+        $this->setValue(self::KEY_NAME_RIGHTS_PRESET, $presetName);
+
+        UI::getInstance()->addInfoMessage(t('Switched the role preset to %1$s.', sb()->code($presetName)));
+
+        // The request object is not fully configured yet, do this manually
+        $refresh = str_replace('&amp;', '&', Request::getInstance()->setBaseURL(APP_URL)->buildRefreshURL(array(), array(self::REQUEST_PARAM_RIGHTS_PRESET)));
+
+        Application::redirect($refresh);
+    }
+
+    private function unpackRights(Application_User $user) : array
+    {
+        if(Application::isSessionSimulated() || $user->isDeveloperModeEnabled())
         {
             $rights = $this->fetchSimulatedRights();
         }
+        else
+        {
+            $rights = $this->fetchRights($user);
+        }
+
+        $userID = $user->getID();
 
         $this->log(sprintf('User [%s] | Fetched [%s] rights.', $userID, count($rights)));
         $this->logData($rights);
 
-        $this->setValue(self::KEY_NAME_USER_ID, $userID);
         $this->setValue(self::KEY_NAME_USER_RIGHTS, implode(',', $rights));
 
-        $this->log(sprintf('User [%s] | Stored in the session.', $userID));
-
-        // Unpack the user, as processes after this may need to access
-        // the instance. The redirect, for example, needs to know whether
-        // the user is a developer.
-        $unpacked = $this->unpackUser();
-
-        $this->triggerUserAuthenticated($unpacked);
-
-        $this->log('User [%s] | Redirecting to the initially requested URL.', $userID);
-
-        if(!isCLI()) {
-            Application::redirect($this->unpackTargetURL());
-        }
-    }
-
-    private function initRightPresets() : void
-    {
-        $user = Application::createSystemUser();
-
-        if(!$user instanceof Application_User_Extended)
-        {
-            return;
-        }
-
-        $roles = $user->getRightsManager()->getRoles();
-
-        foreach($roles as  $role)
-        {
-            $this->rightPresets[$role->getID()] = $role->getRightIDs();
-        }
-    }
-
-    /**
-     * Initializes the session when in simulation mode. Has no
-     * effect if simulation mode is disabled.
-     *
-     * @param int $userID The currently authenticated user's ID
-     * @throws Application_Exception
-     */
-    private function initSimulatedSession(int $userID) : int
-    {
-        // Ignore this if we are not in simulation mode.
-        if (!Application::isSessionSimulated()) {
-            return $userID;
-        }
-
-        $this->log('Session is in simulated mode.');
-
-        // Store the name of the rights preset we want to use.
-        if (isset($_REQUEST[self::KEY_NAME_RIGHTS_PRESET]) && array_key_exists($_REQUEST[self::KEY_NAME_RIGHTS_PRESET], $this->rightPresets))
-        {
-            $presetName = $_REQUEST[self::KEY_NAME_RIGHTS_PRESET];
-
-            $this->log(sprintf('Selected rights preset [%s].', $presetName));
-
-            $this->setValue(self::KEY_NAME_RIGHTS_PRESET, $presetName);
-        }
-
-        $simulateID = $this->getSimulatedUserID();
-
-        // Switch the simulated user?
-        if($simulateID !== $userID)
-        {
-            $this->log(sprintf('Using the user ID [%s] for the simulated session.', $simulateID));
-
-            $this->storeUser(AppFactory::createUsers()->getByID($simulateID));
-        }
-
-        return $simulateID;
-    }
-
-    private function getSimulatedUserID() : int
-    {
-        $simulateID = (int)$this->getValue(self::KEY_NAME_SIMULATED_ID, $this->defaultSimulatedUser);
-
-        // A user has been selected in the request.
-        if(isset($_REQUEST[self::KEY_NAME_SIMULATED_ID], $this->simulateableUsers[$_REQUEST[self::KEY_NAME_SIMULATED_ID]]))
-        {
-            $simulateID = (int)$_REQUEST[self::KEY_NAME_SIMULATED_ID];
-        }
-
-        return $simulateID;
-    }
-
-    private function unpackRights() : array
-    {
-        if(Application::isSessionSimulated())
-        {
-            return $this->fetchSimulatedRights();
-        }
-
-        return ConvertHelper::explodeTrim(',', $this->getRightsString());
+        return $rights;
     }
 
     /**
@@ -324,7 +343,6 @@ abstract class Application_Session_Base implements Application_Session
      * that URL after authentication.
      *
      * @return string
-     * @see self::storeUser()
      */
     protected function unpackTargetURL() : string
     {
@@ -352,51 +370,39 @@ abstract class Application_Session_Base implements Application_Session
     }
 
     /**
-     * @return array<string,array<int,string>>
-     * @throws Application_Exception
+     * @return Application_User_Rights_Role[]
      */
     public function getRightPresets() : array
     {
-        $this->requireSimulatedSession();
+        return $this->getSystemRightsManager()->getRoles();
+    }
 
-        if (empty($this->rightPresets)) {
-            throw new Application_Exception(
-                'No right presets defined',
-                'You have to define the available right presets in your session class.',
-                self::ERROR_NO_RIGHT_PRESETS_PRESENT
-            );
-        }
-
-        return $this->rightPresets;
+    public function getSystemRightsManager() : Application_User_Rights
+    {
+        return Application::createSystemUser()->getRightsManager();
     }
 
     public function getRightPreset() : string
     {
-        if (!$this->presetExists(self::ADMIN_PRESET_ID))
+        if (!$this->presetExists(DeveloperRole::ROLE_ID))
         {
             throw new Application_Exception(
                 'Admin rights preset not defined',
                 sprintf(
                     'You have to define the [%s] rights preset in the [%s] class for the simulated session mode to work.',
-                    self::ADMIN_PRESET_ID,
+                    DeveloperRole::ROLE_ID,
                     get_class($this)
                 ),
                 self::ERROR_ADMIN_RIGHTS_PRESET_MISSING
             );
         }
 
-        $select = self::ADMIN_PRESET_ID;
+        $select = DeveloperRole::ROLE_ID;
 
         $sessionPreset = $this->getPresetBySession();
         if(!empty($sessionPreset))
         {
             $select = $sessionPreset;
-        }
-
-        $requestPreset = $this->getPresetByRequest();
-        if (!empty($requestPreset))
-        {
-            $select = $requestPreset;
         }
 
         return (string)$this->getValue(self::KEY_NAME_RIGHTS_PRESET, $select);
@@ -416,9 +422,9 @@ abstract class Application_Session_Base implements Application_Session
 
     public function getPresetByRequest() : string
     {
-        if(isset($_REQUEST[self::KEY_NAME_RIGHTS_PRESET]) && $this->presetExists($_REQUEST[self::KEY_NAME_RIGHTS_PRESET]))
+        if(isset($_REQUEST[self::REQUEST_PARAM_RIGHTS_PRESET]) && $this->presetExists($_REQUEST[self::REQUEST_PARAM_RIGHTS_PRESET]))
         {
-            return $_REQUEST[self::KEY_NAME_RIGHTS_PRESET];
+            return $_REQUEST[self::REQUEST_PARAM_RIGHTS_PRESET];
         }
 
         return '';
@@ -426,23 +432,7 @@ abstract class Application_Session_Base implements Application_Session
 
     public function presetExists(string $presetID) : bool
     {
-        $presets = $this->getRightPresets();
-
-        return isset($presets[$presetID]);
-    }
-
-    private function requireSimulatedSession() : void
-    {
-        if (Application::isSessionSimulated())
-        {
-            return;
-        }
-
-        throw new Application_Exception(
-            'Only available during simulated session mode.',
-            '',
-            self::ERROR_ONLY_FOR_SIMULATED_SESSION
-        );
+        return $this->getSystemRightsManager()->roleIDExists($presetID);
     }
 
     /**
@@ -461,19 +451,18 @@ abstract class Application_Session_Base implements Application_Session
      */
     public function fetchSimulatedRights() : array
     {
-        $this->requireSimulatedSession();
+        $roleID = $this->getRightPreset();
+        $manager = $this->getSystemRightsManager();
 
-        $presetID = $this->getRightPreset();
-
-        if(isset($this->rightPresets[$presetID])) {
-            return $this->rightPresets[$presetID];
+        if($manager->roleIDExists($roleID)) {
+            return $manager->getRoleByID($roleID)->getRightIDs();
         }
 
         throw new Application_Session_Exception(
             'Right preset not defined',
             sprintf(
                 'The right preset [%s] does not exist in class [%s].',
-                $presetID,
+                $roleID,
                 get_class($this)
             ),
             self::ERROR_PRESET_NOT_DEFINED
@@ -498,37 +487,18 @@ abstract class Application_Session_Base implements Application_Session
         return $this->user;
     }
 
-    final public function authenticate() : Application_User
+    private function handleUserLoaded() : void
     {
-        if(isset($this->user))
+        $this->checkRolePresetChange();
+        $this->checkLogout();
+    }
+
+    private function checkLogout() : void
+    {
+        if (isset($_REQUEST['logout']) && string2bool($_REQUEST['logout']) === true)
         {
-            if (isset($_REQUEST['logout']) && string2bool($_REQUEST['logout']) === true)
-            {
-                $this->logOut();
-            }
-
-            return $this->user;
+            $this->logOut();
         }
-
-        $this->initRightPresets();
-
-        $userID = $this->initSimulatedSession($this->getUserID());
-
-        // Starts the authentication process. This ends either by
-        // storing the user ID in the session via `storeUser()`,
-        // or via a redirect to an error page.
-        if ($userID === 0)
-        {
-            $this->log('No user ID stored, starting authentication.');
-
-            $this->initAuthentication();
-        }
-
-        $this->log(sprintf('User ID [%s] found, initializing session.', $this->getUserID()));
-
-        $this->user = $this->unpackUser();
-
-        return $this->user;
     }
 
     /**
