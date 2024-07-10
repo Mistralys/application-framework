71b5aa5bc520eb96803ac54b096a6e3107de8311
Mistralys <s.mordziol@gmail.com>
2024-07-10 07:37:35 +0200

    Added validation code support.

--- src/classes/Application/Interfaces/Validatable.php
+++ src/classes/Application/Interfaces/Validatable.php
@@ -1,12 +1,33 @@
 <?php
+/**
+ * @package Application
+ * @subpackage Interfaces
+ */
 
+declare(strict_types=1);
+
+use AppUtils\OperationResult_Collection;
+
+/**
+ * Interface for an element that can be validated.
+ *
+ * NOTE: Only supports a single validation error
+ * message to be generated. As an alternative,
+ * consider using {@see OperationResult_Collection}.
+ *
+ * @package Application
+ * @subpackage Interfaces
+ *
+ * @see Application_Traits_Validatable
+ */
 interface Application_Interfaces_Validatable
 {
     public function isValid() : bool;
     
     public function getValidationMessage() : ?string;
+    public function getValidationCode() : ?int;
     
-    public function setValidationOption($name, $value) : void;
+    public function setValidationOption(string $name, $value) : void;
     
     public function getValidationOption(string $name, $default=null);
     
