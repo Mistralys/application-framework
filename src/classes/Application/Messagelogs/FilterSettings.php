becbccd10b1560082ea3fddc744a7ca2ee10c24c
Mistralys <s.mordziol@gmail.com>
2024-08-02 10:36:08 +0200

    Added the user filter.

--- src/classes/Application/Messagelogs/FilterSettings.php
+++ src/classes/Application/Messagelogs/FilterSettings.php
@@ -1,42 +1,78 @@
 <?php
 
+declare(strict_types=1);
+
+use Application\AppFactory;
+
 /**
  * @property Application_Messagelogs $collection
  * @property Application_Messagelogs_FilterCriteria $filters
  */
 class Application_Messagelogs_FilterSettings extends DBHelper_BaseFilterSettings
 {
+    public const SETTING_CATEGORY = 'category';
+    public const SETTING_TYPE = 'type';
+    public const SETTING_DATE = 'date';
+    public const SETTING_SEARCH = 'search';
+    public const SETTING_USER = 'user';
+
     /**
      * {@inheritDoc}
      * @see Application_FilterSettings::registerSettings()
      */
     protected function registerSettings() : void
     {
-        $this->registerSetting('category', t('Category'), '');
-        $this->registerSetting('type', t('Type'), '');
-        $this->registerSetting('date', t('Date'));
-        $this->registerSetting('search', t('Search'));
+        $this->registerSetting(self::SETTING_CATEGORY, t('Category'), '');
+        $this->registerSetting(self::SETTING_TYPE, t('Type'), '');
+        $this->registerSetting(self::SETTING_USER, t('User'), '');
+        $this->registerSetting(self::SETTING_DATE, t('Date'));
+        $this->registerSetting(self::SETTING_SEARCH, t('Search'));
     }
 
     protected function injectElements(HTML_QuickForm2_Container $container) : void
     {
-        $categories = $this->addSelect('category', $container);
-        $categories->addOption(t('All categories'), '');
-        
-        $list = $this->collection->getAvailableCategories();
-        foreach($list as $category) {
-            $categories->addOption($category, $category);
+        $this->injectCategories();
+        $this->injectTypes();
+        $this->injectUser();
+
+        $this->addElementDateSearch(self::SETTING_DATE, $container);
+    }
+
+    private function injectUser() : void
+    {
+        $collection = AppFactory::createUsers();
+        $ids = $this->collection->getAvailableUserIDs();
+
+        $users = $this->addSelect(self::SETTING_USER);
+
+        $users->addOption(t('Any user'), '');
+
+        foreach($ids as $id) {
+            $user = $collection->getByID($id);
+            $users->addOption($user->getLabel(), $id);
         }
-        
-        $types = $this->addSelect('type', $container);
+    }
+
+    private function injectTypes() : void
+    {
+        $types = $this->addSelect(self::SETTING_TYPE);
         $types->addOption(t('All types'), '');
-        
+
         $list = $this->collection->getAvailableTypes();
         foreach($list as $type) {
             $types->addOption($type, $type);
         }
-        
-        $this->addElementDateSearch('date', $container);
+    }
+
+    private function injectCategories() : void
+    {
+        $categories = $this->addSelect(self::SETTING_CATEGORY);
+        $categories->addOption(t('All categories'), '');
+
+        $list = $this->collection->getAvailableCategories();
+        foreach($list as $category) {
+            $categories->addOption($category, $category);
+        }
     }
     
     /**
@@ -45,12 +81,21 @@ class Application_Messagelogs_FilterSettings extends DBHelper_BaseFilterSettings
      */
     protected function _configureFilters() : void
     {
-        $this->filters->selectDate((string)$this->getSetting('date'));
-        
-        $this->filters->selectCategory((string)$this->getSetting('category'));
-        
-        $this->filters->selectType((string)$this->getSetting('type'));
-        
-        $this->filters->setSearch((string)$this->getSetting('search'));
+        $this->filters->selectDate((string)$this->getSetting(self::SETTING_DATE));
+        $this->filters->selectCategory((string)$this->getSetting(self::SETTING_CATEGORY));
+        $this->filters->selectType((string)$this->getSetting(self::SETTING_TYPE));
+        $this->filters->setSearch((string)$this->getSetting(self::SETTING_SEARCH));
+
+        $this->configureUser();
+    }
+
+    private function configureUser() : void
+    {
+        $userID = (int)$this->getSetting(self::SETTING_USER);
+        $collection = AppFactory::createUsers();
+
+        if($userID > 0 && $collection->idExists($userID)) {
+            $this->filters->selectUser($userID);
+        }
     }
 }
