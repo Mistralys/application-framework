3158e7386353edf59739e4bc05de89fddd0110c9
Mistralys <s.mordziol@gmail.com>
2024-09-25 16:56:06 +0200

    Fixed a hardcoded media collection reference.

--- src/classes/Application/Tags/Taggables/TagCollectionTrait.php
+++ src/classes/Application/Tags/Taggables/TagCollectionTrait.php
@@ -92,7 +92,7 @@ trait TagCollectionTrait
 
     public function createTagTree(?UI $ui=null) : TreeNode
     {
-        $rootTag = AppFactory::createMedia()->requireRootTag();
+        $rootTag = $this->requireRootTag();
 
         $rootNode = new TreeNode($ui, $rootTag->getLabel());
 
