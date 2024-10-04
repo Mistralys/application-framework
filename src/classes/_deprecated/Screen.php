f515ad6c8c66fcceb48d5102c04ba1239d73fcb4
Mistralys <s.mordziol@gmail.com>
2024-10-04 14:05:51 +0200

    Deprecated & namespaced the admin screen interface.

---
+++ src/classes/_deprecated/Screen.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @package Application
+ * @subpackage Admin
+ */
+
+use Application\Interfaces\Admin\AdminScreenInterface;
+
+/**
+ *
+ * @package Application
+ * @subpackage Admin
+ *
+ * @deprecated Use {@see AdminScreenInterface} instead.
+ */
+interface Application_Admin_ScreenInterface extends AdminScreenInterface
+{
+}
