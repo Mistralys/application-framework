97b7b1a3de968e2b02898057666bcd395a1f3bda
Mistralys <s.mordziol@gmail.com>
2024-10-30 15:00:18 +0100

    Added the app sets UI in the test driver.

---
+++ tests/application/assets/classes/TestDriver/Area/Devel/AppSetsScreen/EditAppSetScreen.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TestDriver\Area\Devel\AppSetsScreen;
+
+use Application_Admin_Area_Devel_Appsets_Edit;
+
+class EditAppSetScreen extends Application_Admin_Area_Devel_Appsets_Edit
+{
+
+}
