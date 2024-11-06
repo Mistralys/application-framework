50fb44cc2c712584d532088ee044f8081122bd1f
Mistralys <s.mordziol@gmail.com>
2024-11-06 18:40:22 +0100

    Moved some logic to the collection.

--- tests/AppFrameworkTestClasses/CountriesTestCase.php
+++ tests/AppFrameworkTestClasses/CountriesTestCase.php
@@ -31,6 +31,7 @@ abstract class CountriesTestCase extends ApplicationTestCase
 
         $this->countries = Application_Countries::getInstance();
 
+        $this->countries->resetCollection();
         $this->countries->clearIgnored();
     }
 
@@ -41,18 +42,7 @@ abstract class CountriesTestCase extends ApplicationTestCase
 
     protected function createInvariantCountry() : Application_Countries_Country
     {
-        $this->assertFalse($this->countries->isoExists(Application_Countries_Country::COUNTRY_INDEPENDENT_ISO));
-
-        DBHelper::insertDynamic(
-            Application_Countries::TABLE_NAME,
-            array(
-                Application_Countries_Country::COL_ISO => Application_Countries_Country::COUNTRY_INDEPENDENT_ISO,
-                Application_Countries::PRIMARY_NAME => Application_Countries_Country::COUNTRY_INDEPENDENT_ID,
-                Application_Countries_Country::COL_LABEL => 'Country independent'
-            )
-        );
-
-        $country = $this->countries->getByID(Application_Countries_Country::COUNTRY_INDEPENDENT_ID);
+        $country = $this->countries->createInvariantCountry();
 
         $this->assertTrue($country->isInvariant());
 
