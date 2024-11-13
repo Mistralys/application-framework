2203cd884fb0cc12a1d5d8f95efcea2e985c32a9
Mistralys <s.mordziol@gmail.com>
2024-11-13 14:57:02 +0100

    Writing the app log for JS errors #15

--- src/classes/Application/ErrorLog/Log/Entry/JavaScript.php
+++ src/classes/Application/ErrorLog/Log/Entry/JavaScript.php
@@ -3,11 +3,30 @@
 declare(strict_types=1);
 
 use Application\AppFactory;
-
-require_once 'Application/ErrorLog/Log/Entry.php';
+use AppUtils\ArrayDataCollection;
+use AppUtils\ConvertHelper\JSONConverter;
 
 class Application_ErrorLog_Log_Entry_JavaScript extends Application_ErrorLog_Log_Entry
 {
+    public const KEY_CODE = 'code';
+    public const KEY_REFERER = 'referer';
+    public const KEY_COLUMN = 'column';
+    public const KEY_LINE = 'line';
+    public const KEY_DETAILS = 'details';
+    public const KEY_LOG_LINES = 'log_lines';
+    public const KEY_MESSAGE = 'message';
+    public const KEY_URL = 'url';
+    public const KEY_TYPE = 'type';
+    public const KEY_CALL_TRACE = 'call_trace';
+
+    public const INDEX_CODE = 0;
+    public const INDEX_MESSAGE = 2;
+    public const INDEX_DETAILS = 3;
+    public const INDEX_REFERER = 4;
+    public const INDEX_URL = 5;
+    public const INDEX_LINE = 6;
+    public const INDEX_COLUMN = 7;
+
     public function getTypeLabel() : string
     {
         return t('JavaScript');
@@ -15,32 +34,32 @@ class Application_ErrorLog_Log_Entry_JavaScript extends Application_ErrorLog_Log
     
     public function getCode() : int
     {
-        return (int)$this->getTokenIndex(0);
+        return (int)$this->getTokenIndex(self::INDEX_CODE);
     }
     
     public function getMessage() : string
     {
-        return $this->getTokenIndex(2);
+        return $this->getTokenIndex(self::INDEX_MESSAGE);
     }
     
     public function getDetails() : string
     {
-        return $this->getTokenIndex(3);
+        return $this->getTokenIndex(self::INDEX_DETAILS);
     }
     
     public function getReferer() : string
     {
-        return $this->getTokenIndex(4);
+        return $this->getTokenIndex(self::INDEX_REFERER);
     }
     
     public function getSource() : string
     {
-        return $this->getTokenIndex(5);
+        return $this->getTokenIndex(self::INDEX_URL);
     }
     
     public function getLine() : int
     {
-        $val = $this->getTokenIndex(6);
+        $val = $this->getTokenIndex(self::INDEX_LINE);
         if(!empty($val)) {
             return (int)$val;
         }
@@ -50,14 +69,14 @@ class Application_ErrorLog_Log_Entry_JavaScript extends Application_ErrorLog_Log
     
     public function getColumn() : int
     {
-        $val = $this->getTokenIndex(7);
+        $val = $this->getTokenIndex(self::INDEX_COLUMN);
         if(!empty($val)) {
             return (int)$val;
         }
         
         return 0;
     }
-    
+
     public function addProperties(UI_PropertiesGrid $grid) : void
     {
         $grid->add(t('Line'), $this->getLine());
@@ -65,9 +84,15 @@ class Application_ErrorLog_Log_Entry_JavaScript extends Application_ErrorLog_Log
         $grid->add(t('Details'), $this->getDetails());
         $grid->add(t('Source'), $this->getSource())->setHelpText(t('The URL or file in which the error happened - not always available.'));
     }
-    
-    public static function logError(int $code, string $type, string $message, string $details, string $referer, string $url, int $line, int $column)
+
+    /**
+     * @param array<string,string|array|int|float|bool|null> $rawData
+     * @return void
+     */
+    public static function logError(array $rawData) : void
     {
+        $data = ArrayDataCollection::create($rawData);
+
         // 0 = code
         // 1 = type
         // 2 = message
@@ -76,18 +101,29 @@ class Application_ErrorLog_Log_Entry_JavaScript extends Application_ErrorLog_Log
         // 5 = url
         // 6 = line
         // 7 = column
-        
+
         $tokens = array(
-            $code,
-            $type,
-            $message,
-            $details,
-            $referer,
-            $url,
-            $line,
-            $column
+            $data->getInt(self::KEY_CODE),
+            $data->getString(self::KEY_TYPE),
+            $data->getString(self::KEY_MESSAGE),
+            $data->getString(self::KEY_DETAILS),
+            $data->getString(self::KEY_REFERER),
+            $data->getString(self::KEY_URL),
+            $data->getInt(self::KEY_LINE),
+            $data->getInt(self::KEY_COLUMN)
         );
-        
-        AppFactory::createErrorLog()->logJavascriptError($tokens);
+
+        $errorLog = AppFactory::createErrorLog();
+        $logID = $errorLog->logJavascriptError($tokens);
+
+        $lines = $data->getArray(self::KEY_LOG_LINES);
+        $lines[] = '';
+        $lines[] = 'Stack trace:';
+        $lines[] = $data->getString(self::KEY_CALL_TRACE);
+        $lines[] = '';
+        $lines[] = 'Raw JSON error data:';
+        $lines[] = JSONConverter::var2json($rawData);
+
+        $errorLog->writeLogfile($logID, $lines);
     }
 }
