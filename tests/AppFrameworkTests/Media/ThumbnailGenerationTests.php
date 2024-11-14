10070bdc7f79712e75f66d9296342f9005651507
Mistralys <s.mordziol@gmail.com>
2024-11-14 14:19:24 +0100

    Added tests.

--- tests/AppFrameworkTests/Media/ThumbnailGenerationTests.php
+++ tests/AppFrameworkTests/Media/ThumbnailGenerationTests.php
@@ -19,4 +19,37 @@ final class ThumbnailGenerationTests extends MediaTestCase
         $this->assertFalse($this->createTestGIFImage(true)->supportsThumbnails());
         $this->assertFalse($this->createTestSVGImage()->supportsThumbnails());
     }
+
+    /**
+     * As a thumbnail cannot be generated for an SVG file,
+     * calling the thumbnail method must return the original
+     * file path.
+     */
+    public function test_generateSVGThumbnailReturnsOriginalPath() : void
+    {
+        $svg = $this->createTestSVGImage();
+
+        $this->assertSame($svg->getPath(), $svg->getThumbnailPath(42));
+        $this->assertSame($svg->getPath(), $svg->createThumbnail(42));
+    }
+
+    /**
+     * As a thumbnail must not be generated for an animated GIF
+     * file (it would lose the animation), calling the thumbnail
+     * method must return the original file path.
+     */
+    public function test_generateAnimatedGIFThumbnailReturnsOriginalPath() : void
+    {
+        $svg = $this->createTestGIFImage(true);
+
+        $this->assertSame($svg->getPath(), $svg->getThumbnailPath(42));
+        $this->assertSame($svg->getPath(), $svg->createThumbnail(42));
+    }
+    public function test_generateThumbnailCreatesNewFile() : void
+    {
+        $jpg = $this->createTestJPGImage();
+
+        $this->assertNotSame($jpg->getPath(), $jpg->getThumbnailPath(42));
+        $this->assertNotSame($jpg->getPath(), $jpg->createThumbnail(42));
+    }
 }
