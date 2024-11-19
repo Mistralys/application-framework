6a5e95d4d23e38a38cf97addb3ef36a6e6e3e09d
Mistralys <s.mordziol@gmail.com>
2024-11-19 08:46:11 +0100

    Simplified structure.

--- src/classes/Application/EventHandler/OfflineEvents/OfflineListener.php
+++ src/classes/Application/EventHandler/OfflineEvents/OfflineListener.php
@@ -1,15 +1,22 @@
 <?php
+/**
+ * @package Application
+ * @subpackage Events
+ */
 
 declare(strict_types=1);
 
 use AppUtils\NamedClosure;
 
+/**
+ * Abstract base class for offline event listeners.
+ *
+ * @package Application
+ * @subpackage Events
+ */
 abstract class Application_EventHandler_OfflineEvents_OfflineListener
 {
-    /**
-     * @var NamedClosure|null
-     */
-    private $callable = null;
+    private ?NamedClosure $callable = null;
 
     public function getCallable() : NamedClosure
     {
@@ -21,5 +28,19 @@ abstract class Application_EventHandler_OfflineEvents_OfflineListener
         return $this->callable;
     }
 
-    abstract protected function wakeUp() : NamedClosure;
+    protected function wakeUp(): NamedClosure
+    {
+        $callback = array($this, 'handleEvent');
+
+        return NamedClosure::fromClosure(Closure::fromCallable($callback), $callback);
+    }
+
+    /**
+     * This method is called when the event is triggered.
+     *
+     * @param Application_EventHandler_Event $event This will be the event class specific to the event that triggered this listener.
+     * @param mixed ...$args Any arguments that were added in the original event trigger call.
+     * @return void
+     */
+    abstract protected function handleEvent(Application_EventHandler_Event $event, ...$args): void;
 }
