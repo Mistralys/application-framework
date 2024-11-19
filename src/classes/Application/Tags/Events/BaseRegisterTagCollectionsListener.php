ecc397cb63af0a01e9616b40151ce2be8cb8bebe
Mistralys <s.mordziol@gmail.com>
2024-11-19 08:51:24 +0100

    Added base tag registration offline listener.

---
+++ src/classes/Application/Tags/Events/BaseRegisterTagCollectionsListener.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @package Application
+ * @subpackage Tags
+ */
+
+declare(strict_types=1);
+
+namespace Application\Tags\Events;
+
+use Application\OfflineEvents\RegisterTagCollectionsEvent;
+use Application\Tags\Taggables\TagCollectionInterface;
+use Application_EventHandler_Event;
+use Application_EventHandler_OfflineEvents_OfflineListener;
+use AppUtils\ClassHelper;
+
+/**
+ * Base class for offline listeners that register tag collections.
+ *
+ * @package Application
+ * @subpackage Tags
+ * @see RegisterTagCollectionsEvent
+ */
+abstract class BaseRegisterTagCollectionsListener extends Application_EventHandler_OfflineEvents_OfflineListener
+{
+    protected function handleEvent(Application_EventHandler_Event $event, ...$args): void
+    {
+        $this->handleTagRegistration(
+            ClassHelper::requireObjectInstanceOf(
+                RegisterTagCollectionsEvent::class,
+                $event
+            )
+        );
+    }
+
+    protected function handleTagRegistration(RegisterTagCollectionsEvent $event) : void
+    {
+        foreach ($this->getCollections() as $collection) {
+            $event->registerTagCollection($collection);
+        }
+    }
+
+    /**
+     * @return TagCollectionInterface[]
+     */
+    abstract protected function getCollections() : array;
+}
