772ae39f8e2cc782bc650be913b7fa7887c5d7b2
Mistralys <s.mordziol@gmail.com>
2024-11-19 08:48:26 +0100

    Added some docs.

--- src/classes/Application/OfflineEvents/RegisterCacheLocationsEvent.php
+++ src/classes/Application/OfflineEvents/RegisterCacheLocationsEvent.php
@@ -8,6 +8,7 @@ declare(strict_types=1);
 
 namespace Application\OfflineEvents;
 
+use Application\CacheControl\BaseRegisterCacheLocationsListener;
 use Application\CacheControl\CacheLocationInterface;
 use Application\CacheControl\CacheManager;
 use Application_EventHandler_Event;
@@ -16,8 +17,14 @@ use Application_EventHandler_Event;
  * This offline event is triggered when the {@see CacheManager} is initialized,
  * to discover and register all locations where data is cached.
  *
+ * ## Usage
+ *
+ * 1. Add listeners in the folder {@see self::EVENT_NAME} in the offline event folder.
+ * 2. Extend the base class {@see BaseRegisterCacheLocationsListener}.
+ *
  * @package Application
  * @subpackage CacheControl
+ * @see BaseRegisterCacheLocationsListener
  */
 class RegisterCacheLocationsEvent extends Application_EventHandler_Event
 {
