0f5c69d88261c56c59ba42eb1893dd86f4ba2f0c
Mistralys <s.mordziol@gmail.com>
2024-11-19 14:30:50 +0100

    Added create button.

--- tests/application/assets/classes/TestDriver/Area/RevisionableScreen/RevisionableListScreen.php
+++ tests/application/assets/classes/TestDriver/Area/RevisionableScreen/RevisionableListScreen.php
@@ -8,11 +8,15 @@ use Application\Revisionable\RevisionableInterface;
 use Application_Admin_Area_Mode_RevisionableList;
 use Application_RevisionableCollection;
 use TestDriver\Revisionables\RevisionableCollection;
+use UI;
 
 class RevisionableListScreen extends Application_Admin_Area_Mode_RevisionableList
 {
     public const URL_NAME = 'list';
 
+    /**
+     * @return RevisionableCollection
+     */
     public function getCollection(): Application_RevisionableCollection
     {
         return RevisionableCollection::getInstance();
@@ -49,5 +53,15 @@ class RevisionableListScreen extends Application_Admin_Area_Mode_RevisionableLis
     public function getTitle(): string
     {
         return t('Available revisionables');
+    }
+
+    protected function _handleSidebar(): void
+    {
+        $this->sidebar->addButton('add-new', t('Create revisionable'))
+            ->setIcon(UI::icon()->add())
+            ->setTooltip(t('Opens the screen to create a new revisionable.'))
+            ->link($this->getCollection()->adminURL()->create());
+
+        parent::_handleSidebar();
     }
 }
