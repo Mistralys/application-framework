c3d25ad714370464ff64b1f1133453404f3c6bf8
Mistralys <s.mordziol@gmail.com>
2024-11-19 08:52:01 +0100

    Added the tag collection registry class.

---
+++ src/classes/Application/Tags/TagCollectionRegistry.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * @package Application
+ * @subpackage Tags
+ */
+
+declare(strict_types=1);
+
+namespace Application\Tags;
+
+use Application\AppFactory;
+use Application\OfflineEvents\RegisterTagCollectionsEvent;
+use Application\Tags\Taggables\TagCollectionInterface;
+use Application\Tags\Taggables\Taggable;
+use Application\Tags\Taggables\TaggableInterface;
+use Application\Tags\Taggables\TaggableUniqueID;
+use AppUtils\Collections\BaseStringPrimaryCollection;
+
+/**
+ * Registry for tag collections, mainly used to find taggable records
+ * by their unique ID.
+ *
+ * ## Usage
+ *
+ * Get an instance via {@see AppFactory::createTags()} and then {@see TagCollection::createCollectionRegistry()}.
+ *
+ * @package Application
+ * @subpackage Tags
+ *
+ * @method TagCollectionInterface getByID(string $id)
+ * @method TagCollectionInterface[] getAll()
+ */
+class TagCollectionRegistry extends BaseStringPrimaryCollection
+{
+    private static ?self $instance = null;
+
+    public static function getInstance(): self
+    {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+    public function getDefaultID(): string
+    {
+        return '';
+    }
+
+    /**
+     * @return void
+     */
+    protected function registerItems(): void
+    {
+        // Triggering the event is enough, as the event listeners
+        // will call the registerCollection method.
+        AppFactory::createOfflineEvents()->triggerEvent(RegisterTagCollectionsEvent::EVENT_NAME);
+    }
+
+    /**
+     * @param TagCollectionInterface $collection
+     * @return void
+     * @see RegisterTagCollectionsEvent::registerTagCollection()
+     */
+    public function registerCollection(TagCollectionInterface $collection): void
+    {
+        $this->registerItem($collection);
+    }
+
+    public function uniqueIDExists(string $uniqueID): bool
+    {
+        return TaggableUniqueID::parse($uniqueID)
+            ->taggableExists();
+    }
+
+    /**
+     * @param string $uniqueID
+     * @return TaggableInterface
+     */
+    public function getTaggableByUniqueID(string $uniqueID): TaggableInterface
+    {
+        return TaggableUniqueID::parse($uniqueID)
+            ->requireExists()
+            ->getTaggable();
+    }
+}
+
