8f67d3919eb2db7887ab90a6fcec93cf95feb5cc
Mistralys <s.mordziol@gmail.com>
2024-11-20 12:15:28 +0100

    Renamed a class.

--- tests/AppFrameworkTestClasses/Traits/ConnectorTestTrait.php
+++ tests/AppFrameworkTestClasses/Traits/ConnectorTestTrait.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace AppFrameworkTestClasses\Traits;
 
+use Application\Ajax\AjaxMethodInterface;
 use Application_AjaxMethod;
 use AppUtils\ConvertHelper\JSONConverter;
 use Connectors_Response;
@@ -13,20 +14,20 @@ trait ConnectorTestTrait
     public function assertResponseIsError(Connectors_Response $response): void
     {
         $this->assertTrue($response->isError());
-        $this->assertResponseHasState($response, Application_AjaxMethod::STATE_ERROR);
+        $this->assertResponseHasState($response, AjaxMethodInterface::STATE_ERROR);
     }
 
     public function assertResponseIsSuccess(Connectors_Response $response): void
     {
         $this->assertFalse($response->isError());
-        $this->assertResponseHasState($response, Application_AjaxMethod::STATE_SUCCESS);
+        $this->assertResponseHasState($response, AjaxMethodInterface::STATE_SUCCESS);
     }
 
     public function assertResponseHasState(Connectors_Response $response, string $state): void
     {
         $rawData = JSONConverter::json2array($response->getRawJSON());
 
-        $this->assertArrayHasKey(Application_AjaxMethod::PAYLOAD_STATE, $rawData, print_r($rawData, true));
-        $this->assertSame($state, $rawData[Application_AjaxMethod::PAYLOAD_STATE]);
+        $this->assertArrayHasKey(AjaxMethodInterface::PAYLOAD_STATE, $rawData, print_r($rawData, true));
+        $this->assertSame($state, $rawData[AjaxMethodInterface::PAYLOAD_STATE]);
     }
 }
