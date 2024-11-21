cf7021a3d1b6d569c094fa21112dcd951a101663
Mistralys <s.mordziol@gmail.com>
2024-11-21 12:43:29 +0100

    Update VersionTests.php

--- tests/AppFrameworkTests/Driver/VersionTests.php
+++ tests/AppFrameworkTests/Driver/VersionTests.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace AppFrameworkTests\Driver;
 
 use AppFrameworkTestClasses\ApplicationTestCase;
+use Application;
 use Application\AppFactory;
 use Application\Driver\VersionInfo;
 
@@ -28,6 +29,39 @@ final class VersionTests extends ApplicationTestCase
         $this->assertEquals('1.0.0', $versionInfo->getFullVersion());
 
         $this->assertTrue($versionInfo->fileExists());
+
+        $before = filemtime($versionInfo->getVersionFile()->getPath());
+
+        usleep(500);
+
+        $new = new VersionInfo();
+        $new->getFullVersion();
+
+        $after = filemtime($versionInfo->getVersionFile()->getPath());
+
+        $this->assertEquals($before, $after);
+    }
+
+    /**
+     * Ensure that the version file is only written once per request
+     * if it does not exist.
+     */
+    public function test_onlyWrittenOncePerRequest() : void
+    {
+        $first = AppFactory::createVersionInfo();
+        $first->getFullVersion();
+
+        $before = filemtime($first->getVersionFile()->getPath());
+
+        usleep(500);
+
+        // Use a new instance to trigger the file creation
+        $new = new VersionInfo();
+        $new->getFullVersion();
+
+        $after = filemtime($new->getVersionFile()->getPath());
+
+        $this->assertEquals($before, $after);
     }
 
     protected function setUp(): void
@@ -37,6 +71,6 @@ final class VersionTests extends ApplicationTestCase
         AppFactory::createVersionInfo()->clearVersion();
         AppFactory::createDevChangelog()->clearCurrentVersion();
 
-        $this->assertFileDoesNotExist(APP_ROOT.'/'.VersionInfo::FILE_NAME);
+        $this->assertFileDoesNotExist(Application::getCacheFolder().'/'.VersionInfo::FILE_NAME);
     }
 }
