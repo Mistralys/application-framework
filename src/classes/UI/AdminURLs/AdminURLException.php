966b8ae98f72f38cd7c2281cdca38411c3f6ca3f
Mistralys <s.mordziol@gmail.com>
2024-12-18 10:45:34 +0100

    Create AdminURLException.php

---
+++ src/classes/UI/AdminURLs/AdminURLException.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @package User Interface
+ * @subpackage Admin URLs
+ */
+
+declare(strict_types=1);
+
+namespace UI\AdminURLs;
+
+use Application_Exception;
+
+/**
+ * @package User Interface
+ * @subpackage Admin URLs
+ */
+class AdminURLException extends Application_Exception
+{
+    public const ERROR_INVALID_HOST = 169601;
+}
