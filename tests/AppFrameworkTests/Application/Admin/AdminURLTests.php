da37e0b145a097e78747e4784593d027c345d95c
Mistralys <s.mordziol@gmail.com>
2024-12-18 10:45:45 +0100

    Updated and added tests.

--- tests/AppFrameworkTests/Application/Admin/AdminURLTests.php
+++ tests/AppFrameworkTests/Application/Admin/AdminURLTests.php
@@ -5,13 +5,15 @@ declare(strict_types=1);
 namespace AppFrameworkTests\Application\Admin;
 
 use AppFrameworkTestClasses\ApplicationTestCase;
+use Application\AppFactory;
 use UI\AdminURLs\AdminURL;
+use UI\AdminURLs\AdminURLException;
 
 final class AdminURLTests extends ApplicationTestCase
 {
     public function test_importURLParameters() : void
     {
-        $url = AdminURL::create()->importURL('https://www.example.com?foo=bar&argh=lopos');
+        $url = AdminURL::create()->importURL(APP_URL.'?foo=bar&argh=lopos');
 
         $this->assertStringContainsString('foo=bar', (string)$url);
         $this->assertStringContainsString('argh=lopos', (string)$url);
@@ -24,23 +26,26 @@ final class AdminURLTests extends ApplicationTestCase
     public function test_importEmptyDispatcher() : void
     {
         // Duplicate slashes are ignored when importing the dispatcher
-        $url = 'https://www.example.com/////';
+        $url = APP_URL.'/////';
 
         $this->assertSame('', AdminURL::create()->importURL($url)->getDispatcher());
     }
 
     public function test_importURLDispatcherScript() : void
     {
-        $url = 'https://www.example.com/dispatcher.php?foo=bar&argh=lopos';
+        $url = APP_URL.'/dispatcher.php?foo=bar&argh=lopos';
 
         $this->assertSame('dispatcher.php', AdminURL::create()->importURL($url)->getDispatcher());
     }
 
     public function test_importURLDispatcherPath() : void
     {
-        $url = 'https://www.example.com/dispatcher/path/?foo=bar&argh=lopos';
+        $url = APP_URL.'/dispatcher/path/?foo=bar&argh=lopos';
 
         $this->assertSame('dispatcher/path/', AdminURL::create()->importURL($url)->getDispatcher());
+
+        echo AppFactory::createRequest()->buildURL().PHP_EOL;
+        echo AdminURL::create()->importURL($url);
     }
 
     public function test_removeParam() : void
@@ -50,5 +55,12 @@ final class AdminURLTests extends ApplicationTestCase
             ->getParams();
 
         $this->assertSame(array('argh' => 'lopos'), $params);
+    }
+
+    public function test_otherHostsThrowAnException() : self
+    {
+        $this->expectExceptionCode(AdminURLException::ERROR_INVALID_HOST);
+
+        AdminURL::create()->importURL('https://example.com/');
     }
 }
