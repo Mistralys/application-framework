ede2c0c23eb5bcd150ad386daa738312420201f5
Mistralys <s.mordziol@gmail.com>
2025-02-07 15:34:14 +0100

    Added day navigation.

--- src/classes/Application/TimeTracker/Admin/Screens/ListScreen/BaseDayListScreen.php
+++ src/classes/Application/TimeTracker/Admin/Screens/ListScreen/BaseDayListScreen.php
@@ -4,8 +4,16 @@ declare(strict_types=1);
 
 namespace Application\TimeTracker\Admin\Screens\ListScreen;
 
+use Application\AppFactory;
 use Application\TimeTracker\Admin\TimeListBuilder;
+use Application\TimeTracker\TimeTrackerCollection;
 use Application_Admin_Area_Mode_Submode;
+use AppUtils\ConvertHelper;
+use AppUtils\HTMLTag;
+use AppUtils\Microtime;
+use DateInterval;
+use Throwable;
+use UI;
 use UI\DataGrid\ListBuilder\ListBuilderScreenInterface;
 use UI\DataGrid\ListBuilder\ListBuilderScreenTrait;
 use UI\Interfaces\ListBuilderInterface;
@@ -16,6 +24,12 @@ class BaseDayListScreen extends Application_Admin_Area_Mode_Submode implements L
 
     public const URL_NAME = 'day';
     public const LIST_ID = 'time-entries-day';
+    public const REQUEST_VAR_DATE = 'date';
+
+    private Microtime $date;
+    private TimeTrackerCollection $timeTracker;
+    private Microtime $previousDay;
+    private Microtime $nextDay;
 
     public function getURLName(): string
     {
@@ -39,7 +53,71 @@ class BaseDayListScreen extends Application_Admin_Area_Mode_Submode implements L
 
     public function createListBuilder(): ListBuilderInterface
     {
-        return new TimeListBuilder($this);
+        return (new TimeListBuilder($this))
+            ->enableDayMode($this->date);
+    }
+
+    protected function _handleHelp(): void
+    {
+        $this->renderer
+            ->getSubtitle()
+            ->setText(sb()
+                ->add(ConvertHelper::date2dayName($this->date))
+                ->sf('%02d.', $this->date->getDay())
+                ->add(ConvertHelper::month2string($this->date->getMonth()))
+                ->add($this->date->getYear())
+            )
+            ->addContextElement($this->renderDateNavigation());
+    }
+
+    private function renderDateNavigation() : \UI_Renderable_Interface
+    {
+        $content =
+            $this->ui->createButtonGroup()
+            ->addButton(UI::button(UI::icon()->previous().' '.t('Previous day'))
+                ->link($this->timeTracker->adminURL()->dayList($this->previousDay))
+            )
+            ->addButton(
+                UI::button(t('Next day').' '.UI::icon()->next())
+                    ->link($this->timeTracker->adminURL()->dayList($this->nextDay))
+            )
+            ->addButton(
+                UI::button(t('Today'))
+                    ->link($this->timeTracker->adminURL()->dayList())
+            ).
+        HTMLTag::create('div')
+            ->addClass('input-append')
+            ->style('margin-left', '20px')
+            ->setContent(
+                HTMLTag::create('input')
+                    ->attr('type', 'text')
+                    ->attr('placeholder', 'yyyy-mm-dd')
+                    ->addClass('input-small').
+                UI::button('Go')
+            );
+
+        return sb()->html($content);
+    }
+
+    protected function _handleCustomActions(): void
+    {
+        $this->date = Microtime::createNow();
+
+        if($this->request->hasParam(self::REQUEST_VAR_DATE)) {
+            try {
+                $this->date = Microtime::createFromString((string)$this->request->getParam(self::REQUEST_VAR_DATE));
+            } catch (Throwable $e) {
+
+            }
+        }
+
+        $this->timeTracker = AppFactory::createTimeTracker();
+
+        $this->previousDay = clone $this->date;
+        $this->previousDay->sub(DateInterval::createFromDateString('1 day'));
+
+        $this->nextDay = clone $this->date;
+        $this->nextDay->add(DateInterval::createFromDateString('1 day'));
     }
 
     protected function _handleSidebarTop(): void
