661abc432256af3b9f80440d2f51cb0e98c767e6
Mistralys <s.mordziol@gmail.com>
2025-02-07 15:39:56 +0100

    Added `setFixedDate()`.

--- src/classes/Application/TimeTracker/TimeFilterCriteria.php
+++ src/classes/Application/TimeTracker/TimeFilterCriteria.php
@@ -4,11 +4,27 @@ declare(strict_types=1);
 
 namespace Application\TimeTracker;
 
+use AppUtils\Microtime;
 use DBHelper_BaseFilterCriteria;
 use DBHelper_StatementBuilder_ValuesContainer;
 
 class TimeFilterCriteria extends DBHelper_BaseFilterCriteria
 {
+    private ?Microtime $fixedDate = null;
+
+    public function setFixedDate(Microtime $fixedDate) : self
+    {
+        $this->fixedDate = $fixedDate;
+        return $this;
+    }
+
+    protected function prepareQuery(): void
+    {
+        if(isset($this->fixedDate)) {
+            $this->addWhereColumnEquals(TimeTrackerCollection::COL_DATE, $this->fixedDate->format('Y-m-d'));
+        }
+    }
+
     protected function _registerJoins(): void
     {
     }
