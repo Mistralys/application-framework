86fec384743197c64b1caa5235632dc6f6b9563a
Mistralys <s.mordziol@gmail.com>
2025-03-03 10:17:45 +0100

    Added tests.

--- tests/AppFrameworkTests/AppFactory/AppFactoryTests.php
+++ tests/AppFrameworkTests/AppFactory/AppFactoryTests.php
@@ -5,10 +5,16 @@ declare(strict_types=1);
 namespace AppFrameworkTests\AppFactory;
 
 use AppFrameworkTestClasses\ApplicationTestCase;
+use Application\AppFactory;
 use Application\AppFactory\ClassCacheHandler;
+use AppUtils\FileHelper\FolderInfo;
+use DBHelper_BaseFilterSettings;
+use TestDriver\TestDBRecords\TestDBFilterSettings;
 
 final class AppFactoryTests extends ApplicationTestCase
 {
+    // region: _Tests
+
     public function test_disabledForUnitTests() : void
     {
         self::assertFalse(ClassCacheHandler::isCacheEnabled());
@@ -21,6 +27,43 @@ final class AppFactoryTests extends ApplicationTestCase
         self::assertTrue(ClassCacheHandler::isCacheEnabled());
     }
 
+    public function test_classListFilteredByInstanceOf() : void
+    {
+        $classes = AppFactory::findClassesInFolder(
+            $this->getTestClassesFolder(),
+            true,
+            DBHelper_BaseFilterSettings::class
+        );
+
+        $this->assertCount(1, $classes);
+        $this->assertContains(TestDBFilterSettings::class, $classes);
+    }
+
+    public function test_classListIsTheSameWithAndWithoutCache() : void
+    {
+        $this->assertFalse(ClassCacheHandler::isCacheEnabled());
+
+        $withoutCache = AppFactory::findClassesInFolder($this->getTestClassesFolder());
+
+        ClassCacheHandler::setCacheEnabled(true);
+
+        $withCache = AppFactory::findClassesInFolder($this->getTestClassesFolder());
+
+        sort($withoutCache);
+        sort($withCache);
+
+        $this->assertSame($withoutCache, $withCache);
+    }
+
+    // endregion
+
+    // region: Support methods
+
+    protected function getTestClassesFolder() : FolderInfo
+    {
+        return FolderInfo::factory($this->getTestAppFolder().'/assets/classes/TestDriver/TestDBRecords');
+    }
+
     protected function setUp(): void
     {
         parent::setUp();
@@ -28,4 +71,6 @@ final class AppFactoryTests extends ApplicationTestCase
         ClassCacheHandler::clearClassCache();
         ClassCacheHandler::setCacheEnabled(null);
     }
+
+    // endregion
 }
