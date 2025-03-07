9381860ecf7f000182143a92c2430e41429b057d
Mistralys <s.mordziol@gmail.com>
2025-03-07 09:07:37 +0100

    Added more time entry types.

--- src/classes/Application/TimeTracker/Types/TimeEntryTypes.php
+++ src/classes/Application/TimeTracker/Types/TimeEntryTypes.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Application\TimeTracker\Types;
 
 use AppUtils\Collections\BaseStringPrimaryCollection;
-use Application\TimeTracker\Types\TimeEntryType;
+use AppUtils\Interfaces\StringPrimaryRecordInterface;
 
 /**
  * @method TimeEntryType getByID(string $id)
@@ -17,6 +17,8 @@ class TimeEntryTypes extends BaseStringPrimaryCollection
     public const TYPE_MEETING = 'meeting';
     public const TYPE_DEVELOPMENT = 'development';
     public const TYPE_MANAGEMENT = 'management';
+    public const TYPE_ABSENCE_GENERAL = 'absence';
+    public const TYPE_ABSENCE_HEALTH = 'health';
     public const DEFAULT_TYPE = self::TYPE_MANAGEMENT;
 
     private static ?TimeEntryTypes $instance = null;
@@ -35,10 +37,17 @@ class TimeEntryTypes extends BaseStringPrimaryCollection
         return self::DEFAULT_TYPE;
     }
 
+    protected function sortItems(StringPrimaryRecordInterface $a, StringPrimaryRecordInterface $b): int
+    {
+        return 0; // We want to keep the order as it is
+    }
+
     protected function registerItems(): void
     {
+        $this->registerItem(new TimeEntryType(self::TYPE_DEVELOPMENT, t('Development')));
         $this->registerItem(new TimeEntryType(self::TYPE_MEETING, t('Meeting')));
         $this->registerItem(new TimeEntryType(self::TYPE_MANAGEMENT, t('Management')));
-        $this->registerItem(new TimeEntryType(self::TYPE_DEVELOPMENT, t('Development')));
+        $this->registerItem(new TimeEntryType(self::TYPE_ABSENCE_GENERAL, (string)sb()->t('Absence:')->t('General')));
+        $this->registerItem(new TimeEntryType(self::TYPE_ABSENCE_HEALTH, (string)sb()->t('Absence:')->t('Health-related')));
     }
 }
