6169398334f3a12a8adb332afdd3a37106deec7d
Mistralys <s.mordziol@gmail.com>
2025-05-09 08:41:07 +0200

    Added a constant.

--- src/classes/UI/Themes/Exception.php
+++ src/classes/UI/Themes/Exception.php
@@ -6,4 +6,5 @@ class UI_Themes_Exception extends Application_Exception
 {
     public const ERROR_THEME_PATH_CONTAINS_NAVIGATION = 177001;
     public const ERROR_INVALID_THEME_PATH = 177002;
+    public const ERROR_UNRECOGNIZED_THEME_PATH = 177003;
 }
