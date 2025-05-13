7e75c25153c7d3a5a94301761d54f2f418d37378
Mistralys <s.mordziol@gmail.com>
2025-05-13 08:54:12 +0200

    Added tests.

--- tests/AppFrameworkTests/Forms/ClientFormTests.php
+++ tests/AppFrameworkTests/Forms/ClientFormTests.php
@@ -6,7 +6,9 @@ namespace AppFrameworkTests\Forms;
 
 use AppFrameworkTestClasses\ApplicationTestCase;
 use AppFrameworkTestClasses\Stubs\ClientFormStub;
+use Application_Formable;
 use UI\Page\Section\GroupControls;
+use UI_Page_Section;
 
 final class ClientFormTests extends ApplicationTestCase
 {
@@ -25,5 +27,21 @@ final class ClientFormTests extends ApplicationTestCase
             GroupControls::CONTROLS_PREFIX,
             $form->render(),
         );
+    }
+
+    public function test_buttonsAreFunctional() : void
+    {
+        $form = new ClientFormStub();
+
+        $this->assertEquals($form->getUI()->getInstanceKey(), $form->getFormInstance()->getUI()->getInstanceKey());
+        $this->assertStringContainsString(Application_Formable::CLIENT_FORM_UI_PREFIX, $form->getUI()->getInstanceKey());
+
+        $html = $form->render();
+
+        //echo $this->saveTestFile($html, 'html')->summarize();
+
+        $this->assertStringNotContainsString(GroupControls::CONTROLS_PREFIX, $html);
+        $this->assertStringContainsString(UI_Page_Section::STYLESHEET_FILE, $html);
+        $this->assertStringContainsString('UI.RegisterSection', $html);
     }
 }
