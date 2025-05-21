297310632a09565a066d1c983447a85e45ab0a4c
Mistralys <s.mordziol@gmail.com>
2025-05-21 16:09:11 +0200

    Documented callable prototype.

--- src/classes/Application/Formable/RecordSettings/Setting.php
+++ src/classes/Application/Formable/RecordSettings/Setting.php
@@ -276,7 +276,18 @@ class Application_Formable_RecordSettings_Setting implements RuntimePropertizabl
    /**
     * Sets a custom callback to use to inject the element.
     *
-    * @param callable(Application_Formable_RecordSettings_Setting) : HTML_QuickForm2_Node $callback
+    * Callable prototype:
+    *
+    * ```php
+    * function(
+    *     Application_Formable_RecordSettings_Setting $setting,
+    *     mixed $arg1,
+    *     mixed $arg2,
+    *     ...
+    * ) : HTML_QuickForm2_Node
+    * ```
+    *
+    * @param callable $callback
     * @param array<int,mixed> $arguments An array of arguments to pass on to the callback. The first parameter is always the setting instance.
     * @return $this
     */
