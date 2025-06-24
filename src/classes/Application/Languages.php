eabe7adf69cae17bf09f56cf000ae8ba0c4258ef
Mistralys <s.mordziol@gmail.com>
2025-06-24 13:17:14 +0200

    Removed unused methods.

--- src/classes/Application/Languages.php
+++ src/classes/Application/Languages.php
@@ -10,6 +10,8 @@ namespace Application;
 
 use Application\Languages\Language;
 use Application\Languages\LanguageException;
+use AppLocalize\Localization;
+use AppLocalize\Localization\Countries\CountryCollection;
 use AppUtils\Collections\BaseStringPrimaryCollection;
 
 /**
@@ -28,16 +30,6 @@ use AppUtils\Collections\BaseStringPrimaryCollection;
  */
 class Languages extends BaseStringPrimaryCollection
 {
-    public const ERROR_UNKNOWN_LANGUAGE_LABEL = 37802;
-
-    public const LANG_DE = 'de';
-    public const LANG_EN = 'en';
-    public const LANG_FR = 'fr';
-    public const LANG_IT = 'it';
-    public const LANG_ES = 'es';
-    public const LANG_PL = 'pl';
-    public const LANG_RO = 'ro';
-
     private static ?Languages $instance = null;
 
     public static function getInstance() : Languages
@@ -65,7 +57,7 @@ class Languages extends BaseStringPrimaryCollection
 
     public function getDefaultID(): string
     {
-        return self::LANG_EN;
+        return $this->getAutoDefault();
     }
 
     public function getByISO(string $iso) : Language
@@ -85,56 +77,8 @@ class Languages extends BaseStringPrimaryCollection
 
             if(!isset($languages[$iso])) {
                 $languages[$iso] = true;
-                $this->registerItem(new Language($iso));
+                $this->registerItem(new Language($iso, CountryCollection::getInstance()->getByISO($country->getISO())->getMainLocale()));
             }
         }
-    }
-
-    /**
-     * @var array<string,string>|NULL
-     */
-    protected static ?array $languageLabels = null;
-
-    /**
-     * @return array<string,string>
-     */
-    protected static function getLanguageLabels() : array
-    {
-        if(isset(self::$languageLabels)) {
-            return self::$languageLabels;
-        }
-
-        self::$languageLabels = array(
-            self::LANG_DE => t('German'),
-            self::LANG_EN => t('English'),
-            self::LANG_FR => t('French'),
-            self::LANG_IT => t('Italian'),
-            self::LANG_ES => t('Spanish'),
-            self::LANG_PL => t('Polish'),
-            self::LANG_RO => t('Romanian')
-        );
-
-        return self::$languageLabels;
-    }
-
-    /**
-     * @param string $iso
-     * @return string
-     * @throws LanguageException
-     */
-    public static function getLabelByISO(string $iso) : string
-    {
-        $iso = strtolower($iso);
-        $labels = self::getLanguageLabels();
-
-        if(isset($labels[$iso])) {
-            return $labels[$iso];
-        }
-
-        throw new LanguageException(
-            sprintf('Unknown language label for language [%s]', $iso),
-            '',
-            self::ERROR_UNKNOWN_LANGUAGE_LABEL
-        );
     }
 }
