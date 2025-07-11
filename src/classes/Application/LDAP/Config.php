fec68eac8e466c9ed2116847caeaa76427a148fb
Mistralys <s.mordziol@gmail.com>
2025-07-11 10:50:00 +0200

    Modernized; Added config options.

--- src/classes/Application/LDAP/Config.php
+++ src/classes/Application/LDAP/Config.php
@@ -1,41 +1,39 @@
 <?php
+/**
+ * @package LDAP
+ * @subpackage Configuration
+ */
 
 declare(strict_types=1);
 
+/**
+ * Configuration utility for LDAP connections. Used to set
+ * connection parameters such as the host, port, and credentials.
+ *
+ * @package LDAP
+ * @subpackage Configuration
+ */
 class Application_LDAP_Config
 {
-    /**
-     * @var string
-     */
-    private $host;
+    public const DEFAULT_PORT = 389;
+    public const DEFAULT_PROTOCOL_VERSION = 3;
 
-    /**
-     * @var int
-     */
-    private $port;
+    private string $host;
+    private ?int $port;
+    private string $password;
+    private string $dn;
+    private string $username;
+    private string $memberSuffix = '';
+    private bool $debug = false;
+    private int $protocolVersion = self::DEFAULT_PROTOCOL_VERSION;
+    private bool $ssl = true;
 
-    /**
-     * @var string
-     */
-    private $password;
-
-    /**
-     * @var string
-     */
-    private $dn;
-
-    /**
-     * @var string
-     */
-    private $username;
-
-    /**
-     * @var string
-     */
-    private $memberSuffix = '';
-
-    public function __construct(string $host, int $port, string $dn, string $username, string $password)
+    public function __construct(string $host, ?int $port, string $dn, string $username, string $password)
     {
+        if($port === 0) {
+            $port = null;
+        }
+
         $this->host = $host;
         $this->port = $port;
         $this->dn = $dn;
@@ -43,20 +41,78 @@ class Application_LDAP_Config
         $this->password = $password;
     }
 
+    public function getProtocolVersion(): int
+    {
+        return $this->protocolVersion;
+    }
+
+    public function setProtocolVersion(int $version): self
+    {
+        $this->protocolVersion = $version;
+        return $this;
+    }
+
+    public function setDebug(bool $debug) : self
+    {
+        $this->debug = $debug;
+        return $this;
+    }
+
+    public function isDebug(): bool
+    {
+        return $this->debug;
+    }
+
     /**
+     * Returns the URI of the LDAP server in the format
+     * `ldap://host:port`.
+     *
      * @return string
      */
+    public function getURI() : string
+    {
+        $scheme = 'ldap';
+        if($this->ssl) {
+            $scheme = 'ldaps';
+        }
+
+        return sprintf(
+            '%s://%s:%d',
+            $scheme,
+            $this->getHost(),
+            $this->getPort()
+        );
+    }
+
+    /**
+     * Whether to use SSL for the connection.
+     * @param bool $enabled
+     * @return $this
+     */
+    public function setSSLEnabled(bool $enabled) : self
+    {
+        $this->ssl = $enabled;
+        return $this;
+    }
+
+    /**
+     * Returns whether SSL is enabled for the connection.
+     *
+     * @return bool
+     */
+    public function isSSLEnabled(): bool
+    {
+        return $this->ssl;
+    }
+
     public function getHost(): string
     {
         return $this->host;
     }
 
-    /**
-     * @return int
-     */
     public function getPort(): int
     {
-        return $this->port;
+        return $this->port ?? self::DEFAULT_PORT;
     }
 
     /**
