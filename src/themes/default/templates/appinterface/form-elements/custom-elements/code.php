b0098ab71b0713365ba8965fcc9e32dead1103b4
Mistralys <s.mordziol@gmail.com>
2025-07-18 19:19:30 +0200

    Using a constant.

--- src/themes/default/templates/appinterface/form-elements/custom-elements/code.php
+++ src/themes/default/templates/appinterface/form-elements/custom-elements/code.php
@@ -4,12 +4,10 @@ declare(strict_types=1);
 
 $form = UI::getInstance()->createForm('example-elements');
 
-$custom = $form->getCustomElements();
-
-foreach($custom as $def)
+foreach($form->getCustomElements() as $def)
 {
     $el = $form->getForm()->addElement($def['alias'], $def['alias']);
-    $el->setRuntimeProperty('demo', true);
+    $el->setRuntimeProperty(UI_Form::PROPERTY_DEMO_MODE, true);
     $el->setLabel($def['name']);
     $el->setComment('Type: '.$def['alias']);
 }
