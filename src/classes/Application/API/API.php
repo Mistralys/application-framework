bebbfa8830f36d5c4411a1c1c27631c1e214e3ad
Mistralys <s.mordziol@gmail.com>
2025-09-18 09:47:43 +0200

    Overhauled the API manager class.

---
+++ src/classes/Application/API/API.php
@@ -0,0 +1,132 @@
+<?php
+
+declare(strict_types=1);
+
+use Application\API\APIException;
+use Application\API\APIMethodInterface;
+use Application\API\BaseMethods\BaseAPIMethod;
+use Application\API\Collection\APIMethodCollection;
+use Application\API\Collection\APIMethodIndex;
+use AppUtils\ClassHelper;
+use AppUtils\ClassHelper\BaseClassHelperException;
+
+class Application_API
+{
+    public const int ERROR_METHOD_NOT_FOUND = 112547001;
+    public const int ERROR_NO_METHOD_SPECIFIED = 112547002;
+    public const int ERROR_INVALID_METHOD_CLASS = 112547003;
+
+    protected Application_Driver $driver;
+    protected Application_Request $request;
+    protected static ?Application_API $instance = null;
+
+    /**
+     * @var string[]
+     */
+    protected array $repositories;
+    private APIMethodCollection $collection;
+
+    protected function __construct()
+    {
+        $this->driver = Application_Driver::getInstance();
+        $this->request = $this->driver->getRequest();
+        $this->collection = new APIMethodCollection($this);
+    }
+
+    /**
+     * Returns the global instance of the API manager,
+     * creating the instance as needed.
+     *
+     * @return Application_API
+     */
+    public static function getInstance() : Application_API
+    {
+        if (!isset(self::$instance)) {
+            self::$instance = new Application_API();
+        }
+
+        return self::$instance;
+    }
+
+    public function getMethodCollection() : APIMethodCollection
+    {
+        return $this->collection;
+    }
+
+    private ?APIMethodIndex $methodIndex = null;
+
+    public function getMethodIndex() : APIMethodIndex
+    {
+        if(!isset($this->methodIndex)) {
+            $this->methodIndex = new APIMethodIndex($this);
+        }
+
+        return $this->methodIndex;
+    }
+
+    public function process(?string $methodName=null) : void
+    {
+        if($methodName === null) {
+            $methodName = $this->request->registerParam('method')->setAlnum()->getString();
+        }
+
+        if (empty($methodName)) {
+            throw new APIException(
+                'No method specified',
+                'The method request parameter was empty.',
+                self::ERROR_NO_METHOD_SPECIFIED
+            );
+        }
+
+        $index = $this->getMethodIndex();
+
+        if(!$index->methodExists($methodName)) {
+            throw new APIException(
+                'Method not found',
+                sprintf(
+                    'The specified method [%s] could not be found in the method index. '.PHP_EOL.
+                    'These are the known API methods: '.PHP_EOL.
+                    '- %s',
+                    $methodName,
+                    implode(PHP_EOL.'- ', $index->getMethodNames())
+                ),
+                self::ERROR_METHOD_NOT_FOUND
+            );
+        }
+
+        $this->loadMethod($index->getMethodClass($methodName))->process();
+    }
+
+    /**
+     *
+     * @param class-string<APIMethodInterface> $class
+     * @return BaseAPIMethod
+     * @throws APIException
+     */
+    public function loadMethod(string $class) : BaseAPIMethod
+    {
+        try
+        {
+            return ClassHelper::requireObjectInstanceOf(
+                BaseAPIMethod::class,
+                new $class($this)
+            );
+        }
+        catch (BaseClassHelperException $e)
+        {
+            throw new APIException(
+                'Invalid API method class',
+                sprintf(
+                    'The method has to extend the [%s] interface. '.PHP_EOL.
+                    'The class [%s] does not. '.PHP_EOL.
+                    'One possible reason is that the API method index is outdated (for example after refactorings). '.PHP_EOL.
+                    'Try clearing the method index to verify (via the Composer or UI cache control).',
+                    APIMethodInterface::class,
+                    $class
+                ),
+                self::ERROR_INVALID_METHOD_CLASS,
+                $e
+            );
+        }
+    }
+}
