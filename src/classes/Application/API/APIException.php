92e5f0271ad4cfc874fdcf6e4d04e9058bd5345d
Mistralys <s.mordziol@gmail.com>
2025-09-18 09:46:53 +0200

    Moved constants.

--- src/classes/Application/API/APIException.php
+++ src/classes/Application/API/APIException.php
@@ -8,5 +8,9 @@ use Application_Exception;
 
 class APIException extends Application_Exception
 {
-
+    public const int ERROR_INVALID_OUTPUT_FORMAT = 59213003;
+    public const int ERROR_NO_OUTPUT_METHODS = 59213001;
+    public const int ERROR_INVALID_INPUT_FORMAT = 59213002;
+    public const int ERROR_NO_INPUT_METHODS = 59213004;
+    public const int ERROR_METHOD_NOT_IN_INDEX = 59213005;
 }
