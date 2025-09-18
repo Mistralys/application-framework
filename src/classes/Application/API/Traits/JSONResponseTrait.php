c1a1acae5c3d4d9355c22e505c9b9e929ed4c956
Mistralys <s.mordziol@gmail.com>
2025-09-18 09:42:31 +0200

    Added API request and response type traits.

---
+++ src/classes/Application/API/Traits/JSONResponseTrait.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * @package API
+ * @subpackage Traits
+ */
+
+declare(strict_types=1);
+
+namespace Application\API\Traits;
+
+use Application\API\ErrorResponse;
+use AppUtils\ArrayDataCollection;
+use AppUtils\ConvertHelper\JSONConverter;
+
+/**
+ * @package API
+ * @subpackage Traits
+ * @see JSONResponseInterface
+ */
+trait JSONResponseTrait
+{
+    public function getResponseMime() : string
+    {
+        return 'application/json';
+    }
+
+    protected function _sendSuccessResponse(ArrayDataCollection $data) : void
+    {
+        $this->sendJSONResponse(
+            JSONResponseInterface::RESPONSE_STATE_SUCCESS,
+            array(
+                JSONResponseInterface::RESPONSE_KEY_DATA => $data->getData()
+            )
+        );
+    }
+
+    protected function _sendErrorResponse(ErrorResponse $response) : void
+    {
+        $this->sendJSONResponse(
+            JSONResponseInterface::RESPONSE_STATE_ERROR,
+            array(
+                JSONResponseInterface::RESPONSE_KEY_CODE => $response->getErrorCode(),
+                JSONResponseInterface::RESPONSE_KEY_MESSAGE => $response->getErrorMessage(),
+                JSONResponseInterface::RESPONSE_KEY_DATA => $response->getErrorData()
+            )
+        );
+    }
+
+    private function sendJSONResponse(string $state, array $data) : void
+    {
+        $data[JSONResponseInterface::RESPONSE_KEY_API] = $this->getAPIData();
+        $data[JSONResponseInterface::RESPONSE_KEY_STATE] = $state;
+
+        ksort($data);
+
+        header('Content-Type: application/json');
+        echo JSONConverter::var2json($data);
+    }
+}
