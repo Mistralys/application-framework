4c479c413eca253a004f669e3a601561e05c6327
Mistralys <s.mordziol@gmail.com>
2025-09-18 09:46:46 +0200

    Added the API method interface.

---
+++ src/classes/Application/API/APIMethodInterface.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * @package API
+ * @subpackage Core
+ */
+
+declare(strict_types=1);
+
+namespace Application\API;
+
+use Application_CORS;
+use AppUtils\ArrayDataCollection;
+use AppUtils\Interfaces\StringPrimaryRecordInterface;
+
+/**
+ * Interface for all API methods.
+ *
+ * @package API
+ * @subpackage Core
+ */
+interface APIMethodInterface extends StringPrimaryRecordInterface
+{
+    public const string PARAM_API_VERSION = 'apiVersion';
+
+    public const int ERROR_REQUEST_DATA_EXCEPTION = 183001;
+    public const int ERROR_RESPONSE_DATA_EXCEPTION = 183002;
+
+    public const string RESPONSE_KEY_ERROR_REQUEST_DATA = 'RequestData';
+
+    public const string API_INFO_KEY_REQUEST_TIME = 'requestTime';
+
+    public function getAPIData() : array;
+
+    public function getMethodName() : string;
+
+    public function getDescription() : string;
+
+    public function getRequestMime() : string;
+    public function getResponseMime() : string;
+
+    /**
+     * Retrieves an indexed array containing available API
+     * version numbers that can be specified to work with.
+     *
+     * @return string[]
+     */
+    public function getVersions() : array;
+
+    /**
+     * Retrieves the current version of the API endpoint.
+     *
+     * @return string
+     */
+    public function getCurrentVersion() : string;
+
+    /**
+     * Gets the version that the method is currently
+     * working with. If a valid version has been specified
+     * in the request, that version is returned. Otherwise,
+     * the current version is returned.
+     *
+     * @return string
+     */
+    public function getActiveVersion() : string;
+
+    /**
+     * Adds a domain name to the list of allowed cross-origin
+     * request sources. Adding one of these enables CORS for
+     * this API endpoint.
+     *
+     * > Note: use the wildcard <code>*</code> as domain to enable
+     * > all cross-origin sources.
+     *
+     * @param string $domain
+     * @return $this
+     */
+    public function allowCORSDomain(string $domain) : self;
+
+    public function getCORS() : Application_CORS;
+
+    /**
+     * Processes the API request and sends the response.
+     * @return never
+     */
+    public function process(): never;
+
+    /**
+     * Processes the method as usual, but instead of sending
+     * the response to the client, it returns the response data
+     * as an ArrayDataCollection object.
+     *
+     * > This is mostly used for unit testing API methods, but
+     * > can potentially allow re-using API methods outside
+     * > the API endpoint context.
+     *
+     * @return ArrayDataCollection
+     * @throws APIException
+     */
+    public function processReturn(): ArrayDataCollection;
+}
