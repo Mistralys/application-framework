ec99a9e327faeff5f09993446b26f09cc18009a0
Mistralys <s.mordziol@gmail.com>
2025-09-18 09:48:33 +0200

    Added the API method index cache location class.

---
+++ src/classes/Application/API/Collection/APICacheLocation.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * @package API
+ * @subpackage Method Collection
+ */
+
+declare(strict_types=1);
+
+namespace Application\AppFactory;
+
+use Application\API\Collection\APIMethodIndex;
+use Application\CacheControl\BaseCacheLocation;
+use AppUtils\FileHelper\JSONFile;
+
+/**
+ * Cache location description class for the API method index,
+ * so it can be handled via the cache control system.
+ *
+ * @package API
+ * @subpackage Method Collection
+ */
+class APICacheLocation extends BaseCacheLocation
+{
+    public const string CACHE_ID = 'APIMethodIndex';
+    private JSONFile $file;
+
+    public function __construct(APIMethodIndex $index)
+    {
+        $this->file = $index->getDataFile();
+    }
+
+    public function getID(): string
+    {
+        return self::CACHE_ID;
+    }
+
+    public function getByteSize(): int
+    {
+        return $this->file->getSize();
+    }
+
+    public function getLabel(): string
+    {
+        return t('API Method Index');
+    }
+
+    public function clear(): void
+    {
+        $this->file->delete();
+    }
+}
