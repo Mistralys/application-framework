e84440a3ee9f49d99cc1188f458ee7a53e80e527
Mistralys <s.mordziol@gmail.com>
2025-09-23 15:40:42 +0200

    Added an error code.

--- src/classes/Application/API/Parameters/APIParameterException.php
+++ src/classes/Application/API/Parameters/APIParameterException.php
@@ -11,4 +11,5 @@ class APIParameterException extends APIException
     public const int ERROR_PARAM_ALREADY_REGISTERED = 183101;
     public const int ERROR_RESERVED_PARAM_NAME = 183102;
     public const int ERROR_INVALID_PARAM_CONFIG = 183103;
+    public const int ERROR_INVALID_DEFAULT_VALUE = 183104;
 }
