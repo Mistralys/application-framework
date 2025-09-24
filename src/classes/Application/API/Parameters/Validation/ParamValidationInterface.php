f1c8c14c4c2aa48efab02ea3ad74f8c98917c584
Mistralys <s.mordziol@gmail.com>
2025-09-24 16:34:47 +0200

    Added a validation constant.

--- src/classes/Application/API/Parameters/Validation/ParamValidationInterface.php
+++ src/classes/Application/API/Parameters/Validation/ParamValidationInterface.php
@@ -12,6 +12,7 @@ interface ParamValidationInterface
     public const int VALIDATION_INVALID_VALUE_TYPE = 183502;
     public const int VALIDATION_INVALID_JSON_DATA = 183503;
     public const int VALIDATION_INVALID_FORMAT_BY_REGEX = 183504;
+    public const int VALIDATION_WARNING_FLOAT_TO_INT = 183505;
 
     public function validate(int|float|bool|string|array $value, OperationResult $result) : void;
 }
