2e6ca32c72b85ed007115aa06e61b29d3ecea6a6
Mistralys <s.mordziol@gmail.com>
2025-09-26 14:28:03 +0200

    Added a reserved param interface.

---
+++ src/classes/Application/API/Parameters/ReservedParamInterface.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @package API
+ * @subpackage Parameters
+ */
+
+declare(strict_types=1);
+
+namespace Application\API\Parameters;
+
+/**
+ * Interface for reserved API parameters.
+ *
+ * @package API
+ * @subpackage Parameters
+ */
+interface ReservedParamInterface extends APIParameterInterface
+{
+
+}
