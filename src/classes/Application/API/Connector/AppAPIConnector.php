de4ce03888bbe572930cd3a4f73e373882efe479
Mistralys <s.mordziol@gmail.com>
2025-10-03 15:12:29 +0200

    Added the specialized App API connector.

---
+++ src/classes/Application/API/Connector/AppAPIConnector.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\API\Connector;
+
+use Application\Bootstrap\Screen\APIBootstrap;
+use AppUtils\ArrayDataCollection;
+use AppUtils\ClassHelper;
+use Connectors;
+use Connectors_Connector;
+
+class AppAPIConnector extends Connectors_Connector
+{
+    private string $appURL;
+
+    public function __construct(string $appURL)
+    {
+        $this->appURL = $appURL;
+        parent::__construct();
+    }
+
+    public static function create(string $appURL) : self
+    {
+        return ClassHelper::requireObjectInstanceOf(
+            self::class,
+            Connectors::createConnector(self::class, $appURL)
+        );
+    }
+
+    protected function checkRequirements(): void
+    {
+    }
+
+    public function getURL(): string
+    {
+        return $this->appURL.'/'.APIBootstrap::DISPATCHER;
+    }
+
+    public function fetchMethodData(string $methodName, array|ArrayDataCollection $params=array()) : ArrayDataCollection
+    {
+        return $this->createAPIMethod()
+            ->fetchJSON($methodName, ArrayDataCollection::create($params));
+    }
+
+    private ?AppAPIMethod $apiMethod = null;
+
+    private function createAPIMethod() : AppAPIMethod
+    {
+        if(!isset($this->apiMethod)) {
+            $this->apiMethod = ClassHelper::requireObjectInstanceOf(
+                AppAPIMethod::class,
+                $this->createMethod(AppAPIMethod::class)
+            );
+        }
+
+        return $this->apiMethod;
+    }
+}
