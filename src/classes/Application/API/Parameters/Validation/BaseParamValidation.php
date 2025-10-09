a78388d08a9dcfe71fb3be6c2984c33ea0a9ed45
Mistralys <s.mordziol@gmail.com>
2025-10-09 14:52:15 +0200

    Removed duplicate of interface method.

--- src/classes/Application/API/Parameters/Validation/BaseParamValidation.php
+++ src/classes/Application/API/Parameters/Validation/BaseParamValidation.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 
 namespace Application\API\Parameters\Validation;
 
-use AppUtils\OperationResult;
-
 abstract class BaseParamValidation implements ParamValidationInterface
 {
-    abstract public function validate(int|float|bool|string|array|null $value, OperationResult $result) : void;
 }
