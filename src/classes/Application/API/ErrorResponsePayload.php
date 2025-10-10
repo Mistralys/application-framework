d07690353ad93ec0dc22cff7c299d8fb52c5ee1a
Mistralys <s.mordziol@gmail.com>
2025-10-10 08:12:09 +0200

    Implementing the response interface.

--- src/classes/Application/API/ErrorResponsePayload.php
+++ src/classes/Application/API/ErrorResponsePayload.php
@@ -4,21 +4,31 @@ declare(strict_types=1);
 
 namespace Application\API;
 
+use Application\API\Response\ResponseInterface;
 use AppUtils\ArrayDataCollection;
 
-class ErrorResponsePayload extends ArrayDataCollection
+class ErrorResponsePayload extends ArrayDataCollection implements ResponseInterface
 {
     public const string KEY_ERROR_CODE = 'errorCode';
     public const string KEY_ERROR_MESSAGE = 'errorMessage';
     public const string KEY_ERROR_DATA = 'errorData';
 
+    private APIMethodInterface $method;
+
     public function __construct(ErrorResponse $response)
     {
+        $this->method = $response->getMethod();
+
         parent::__construct(array(
             self::KEY_ERROR_CODE => $response->getErrorCode(),
             self::KEY_ERROR_MESSAGE => $response->getErrorMessage(),
             self::KEY_ERROR_DATA => $response->getErrorData(),
         ));
+    }
+
+    public function getMethod() : APIMethodInterface
+    {
+        return $this->method;
     }
 
     public function getErrorCode() : int
--- src/classes/Application/API/ResponsePayload.php
+++ src/classes/Application/API/ResponsePayload.php
@@ -4,9 +4,22 @@ declare(strict_types=1);
 
 namespace Application\API;
 
+use Application\API\Response\ResponseInterface;
 use AppUtils\ArrayDataCollection;
 
-class ResponsePayload extends ArrayDataCollection
+class ResponsePayload extends ArrayDataCollection implements ResponseInterface
 {
+    private APIMethodInterface $method;
 
+    public function __construct(APIMethodInterface $method, array $data = array())
+    {
+        $this->method = $method;
+
+        parent::__construct($data);
+    }
+
+    public function getMethod() : APIMethodInterface
+    {
+        return $this->method;
+    }
 }
