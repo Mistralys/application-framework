c6573ef0bd4b9ca39c01233685cab31a7c0ae69c
Mistralys <s.mordziol@gmail.com>
2025-10-10 08:24:24 +0200

    Added a locale API test.

---
+++ tests/AppFrameworkTests/Locales/AppLocaleAPITests.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AppFrameworkTests\Locales;
+
+use Application\API\APIManager;
+use Application\API\APIMethodInterface;
+use Application\Locales\API\AppLocaleAPIInterface;
+use AppLocalize\Localization;
+use AppLocalize\Localization\Locale\de_DE;
+use Mistralys\AppFrameworkTests\TestClasses\APITestCase;
+use TestDriver\API\TestAppLocale\AppLocaleResponse;
+use TestDriver\API\TestAppLocaleMethod;
+
+final class AppLocaleAPITests extends APITestCase
+{
+    public function test_changeLocale() : void
+    {
+        $this->assertSame(Localization::BUILTIN_LOCALE_NAME, Localization::getAppLocaleName(), 'Prerequisite check is that the locale is the default.');
+
+        $_REQUEST[APIMethodInterface::REQUEST_PARAM_METHOD] = TestAppLocaleMethod::METHOD_NAME;
+        $_REQUEST[AppLocaleAPIInterface::PARAM_LOCALE] = de_DE::LOCALE_NAME;
+
+        $method = new TestAppLocaleMethod(APIManager::getInstance());
+
+        $response = $this->assertSuccessfulResponse($method);
+
+        $this->assertSame(de_DE::LOCALE_NAME, Localization::getAppLocaleName(), 'The locale was not changed.');
+
+        $this->assertInstanceOf(AppLocaleResponse::class, $response);
+        $text = $response->getText();
+        $this->assertSame(TestAppLocaleMethod::TEXT_DE, $text, 'The response text is not as expected.');
+    }
+}
