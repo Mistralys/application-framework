b521e59a0966a89ce1da74417cc372c96d76e083
Mistralys <s.mordziol@gmail.com>
2025-10-14 13:26:12 +0200

    Countries API - Added ID/ISO param set.

---
+++ src/classes/Application/Countries/API/ParamSets/BaseAppCountryParamSet.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @package Countries
+ * @subpackage API
+ */
+
+declare(strict_types=1);
+
+namespace Application\Countries\API\ParamSets;
+
+use Application\API\Parameters\Rules\BaseCustomParamSet;
+use Application\Countries\API\AppCountryAPIInterface;
+
+/**
+ * Abstract base class for parameter sets that are used
+ * to resolve a specific country. Implements the interface
+ * {@see AppCountryParamSetInterface}.
+ *
+ * @package Countries
+ * @subpackage API
+ */
+abstract class BaseAppCountryParamSet extends BaseCustomParamSet implements AppCountryParamSetInterface
+{
+    public function __construct(AppCountryAPIInterface $method)
+    {
+        parent::__construct($method);
+    }
+}
---
+++ src/classes/Application/Countries/API/ParamSets/CountryISOSet.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\Countries\API\ParamSets;
+
+use Application\Countries\API\AppCountryISOParam;
+use Application_Countries_Country;
+
+class CountryISOSet extends BaseAppCountryParamSet
+{
+    public const string SET_NAME = 'CountryISO';
+
+    private AppCountryISOParam $param;
+
+    public function getCountry(): ?Application_Countries_Country
+    {
+        return $this->param->getCountry();
+    }
+
+    protected function initParams(): void
+    {
+        $this->param = new AppCountryISOParam();
+        $this->registerParam($this->param);
+    }
+
+    protected function _getID(): string
+    {
+        return self::SET_NAME;
+    }
+}
