7777089cfb5cfb8c818161f5c8b51382cef3b9a5
Mistralys <s.mordziol@gmail.com>
2025-10-16 09:29:09 +0200

    Added API utility class `KeyDescription`.

---
+++ src/classes/Application/API/Utilities/KeyDescription.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * @package API
+ * @subpackage Utilities
+ */
+
+declare(strict_types=1);
+
+namespace Application\API\Utilities;
+
+use Application\API\APIManager;
+use Application\MarkdownRenderer;
+
+/**
+ * Utility class used to describe an API response key with its path and a description.
+ *
+ * @package API
+ * @subpackage Utilities
+ */
+class KeyDescription implements KeyPathInterface
+{
+    public KeyPath $path;
+    public string $description;
+
+    /**
+     * @param KeyPath $path
+     * @param string $description Markdown-enabled description of the key.
+     * @param mixed ...$args Optional arguments to be used with sprintf to format the description.
+     */
+    public function __construct(KeyPath $path, string $description, ...$args)
+    {
+        $this->path = $path;
+        $this->description = sprintf($description, ...$args);
+    }
+
+    /**
+     * @param string|KeyPath $path
+     * @param string $description Markdown-enabled description of the key.
+     * @param mixed ...$args Optional arguments to be used with sprintf to format the description.
+     * @return static
+     */
+    public static function create(string|KeyPath $path, string $description, ...$args): self
+    {
+        return new self(KeyPath::create($path), $description, ...$args);
+    }
+
+    public function getPath(): string
+    {
+        return $this->path->getPath();
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function renderDescription() : string
+    {
+        return MarkdownRenderer::create()->renderInline(APIManager::getInstance()->markdownifyMethodNames($this->getDescription()));
+    }
+
+    public function __toString() : string
+    {
+        return $this->getPath().': '.$this->description;
+    }
+}
