38672ea6b484f310332bfa6db6419031d9713176
Mistralys <s.mordziol@gmail.com>
2025-10-16 09:30:26 +0200

    Added `assertMethodCallIsSuccessful()`.

--- tests/AppFrameworkTestClasses/API/APIMethodTestTrait.php
+++ tests/AppFrameworkTestClasses/API/APIMethodTestTrait.php
@@ -8,11 +8,15 @@ declare(strict_types=1);
 
 namespace AppFrameworkTestClasses\API;
 
+use Application\API\APIManager;
 use Application\API\APIMethodInterface;
 use Application\API\ErrorResponsePayload;
 use Application\API\Parameters\Validation\ParamValidationInterface;
 use Application\API\ResponsePayload;
+use AppUtils\ArrayDataCollection;
+use AppUtils\ClassHelper;
 use AppUtils\OperationResult;
+use Maileditor\API\GetBusinessAreasAPI;
 
 /**
  * @package Test Classes
@@ -70,5 +74,33 @@ trait APIMethodTestTrait
         $this->assertSame($code, $response->getErrorCode(), 'The response code is not as expected.');
 
         return $response;
+    }
+
+    /**
+     * @param class-string<APIMethodInterface> $methodClass
+     * @param array<string,string|int|bool|float|array|NULL>|ArrayDataCollection|NULL $requestParams Any request parameters that the method may require
+     * @return void
+     */
+    public function assertMethodCallIsSuccessful(string $methodClass, array|ArrayDataCollection|NULL $requestParams=null) : void
+    {
+        if($requestParams === null) {
+            $requestParams = array();
+        }
+
+        if($requestParams instanceof ArrayDataCollection) {
+            $requestParams = $requestParams->getData();
+        }
+
+        $method = new $methodClass(APIManager::getInstance());
+
+        $this->assertInstanceOf(APIMethodInterface::class, $method, 'Must be an API method class name');
+
+        $_REQUEST[APIMethodInterface::REQUEST_PARAM_METHOD] = $method->getMethodName();
+
+        foreach($requestParams as $key => $value) {
+            $_REQUEST[$key] = $value;
+        }
+
+        $this->assertSuccessfulResponse(new GetBusinessAreasAPI(APIManager::getInstance())->processReturn());
     }
 }
