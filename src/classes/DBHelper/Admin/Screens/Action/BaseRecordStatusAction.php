baba4e9bbec393a3e67269f10929e62476d139ca
Mistralys <s.mordziol@gmail.com>
2025-10-20 16:25:44 +0200

    Removed unused import.

--- src/classes/DBHelper/Admin/Screens/Action/BaseRecordStatusAction.php
+++ src/classes/DBHelper/Admin/Screens/Action/BaseRecordStatusAction.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace DBHelper\Admin\Screens\Action;
 
 use Application\Traits\AllowableMigrationTrait;
-use Application_Admin_Area_Mode_Submode_Action_CollectionRecord;
 use DBHelper\Admin\Traits\RecordStatusScreenInterface;
 use DBHelper\Admin\Traits\RecordStatusScreenTrait;
 
