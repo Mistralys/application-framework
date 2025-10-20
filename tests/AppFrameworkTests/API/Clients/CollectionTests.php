0a51e8759c8cb4adb55a9374a25e8a95609162c0
Mistralys <s.mordziol@gmail.com>
2025-10-20 13:28:33 +0200

    Added tests.

---
+++ tests/AppFrameworkTests/API/Clients/CollectionTests.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AppFrameworkTests\API\Clients;
+
+use AppFrameworkTestClasses\ApplicationTestCase;
+use Application\API\Clients\APIClientsCollection;
+use Application\AppFactory;
+
+final class CollectionTests extends ApplicationTestCase
+{
+    // region: _Tests
+
+    public function test_createNewClient(): void
+    {
+        $client = AppFactory::createAPIClients()->createNewClient(
+            'Test Client',
+            'TESTCLIENT01'
+        );
+
+        $this->assertSame('Test Client', $client->getLabel());
+        $this->assertSame('TESTCLIENT01', $client->getForeignID());
+        $this->assertEmpty($client->getComments());
+        $this->assertNotNull($client->getRecordMicrotimeKey(APIClientsCollection::COL_DATE_CREATED));
+        $this->assertTrue($client->isActive());
+        $this->assertTrue($client->getCreatedByID() > 0);
+    }
+
+    // endregion
+
+    // region: Support methods
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->startTransaction();
+    }
+
+    // endregion
+}
