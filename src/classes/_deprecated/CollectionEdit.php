f66551aff0150613ccb68985a8f4438a58f53f5e
Mistralys <s.mordziol@gmail.com>
2025-10-20 13:13:46 +0200

    Deprecated the submode collection edit screen.

---
+++ src/classes/_deprecated/CollectionEdit.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+use DBHelper\Admin\Screens\BaseSubmodeRecordSettingsScreen;
+
+/**
+ * @deprecated Use {@see BaseSubmodeRecordSettingsScreen} instead.
+ */
+abstract class Application_Admin_Area_Mode_Submode_CollectionEdit extends BaseSubmodeRecordSettingsScreen{}
