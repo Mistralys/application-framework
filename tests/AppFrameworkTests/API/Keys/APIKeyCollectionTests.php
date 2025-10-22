681daab505d722ad8deab2a4faeb1b3132007885
Mistralys <s.mordziol@gmail.com>
2025-10-22 14:54:23 +0200

    Added API key tests.

---
+++ tests/AppFrameworkTests/API/Keys/APIKeyCollectionTests.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AppFrameworkTests\API\Keys;
+
+use Application\AppFactory;
+use Mistralys\AppFrameworkTests\TestClasses\APIClientTestCase;
+
+final class APIKeyCollectionTests extends APIClientTestCase
+{
+    // region: _Tests
+
+    public function test_createNewKey(): void
+    {
+        $client = AppFactory::createAPIClients()->createNewClient(
+            'Test Client',
+            'TESTCLIENT01'
+        );
+
+        $key = $client->createNewAPIKey('Test key');
+
+        $this->assertSame('Test key', $key->getLabel());
+    }
+
+    public function test_setLabel() : void
+    {
+        $key = $this->createTestAPIKey();
+        $keyID = $key->getID();
+
+        $this->assertSame(
+            'Updated Label',
+            $key
+                ->setLabel('Updated Label')
+                ->saveChained()
+                ->getCollection()
+                ->resetCollection() // Force DB reload
+                ->getByID($keyID)
+                ->getLabel()
+        );
+    }
+
+    public function test_grantAllMethods() : void
+    {
+        $key = $this->createTestAPIKey();
+        $methods = $key->getMethods();
+
+        $this->assertFalse($key->areAllMethodsGranted());
+        $this->assertFalse($methods->areAllGranted());
+        $this->assertEmpty($methods->getMethodNames());
+
+        $key->setGrantAll(true);
+
+        $this->assertTrue($key->areAllMethodsGranted());
+        $this->assertTrue($methods->areAllGranted());
+        $this->assertNotEmpty($methods->getMethodNames());
+    }
+
+    // endregion
+}
