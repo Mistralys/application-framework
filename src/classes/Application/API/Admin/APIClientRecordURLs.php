cdc4005861ebb0e579b39b3d28d053389f1639f5
Mistralys <s.mordziol@gmail.com>
2025-10-22 14:49:03 +0200

    Added `apiKeys()`.

--- src/classes/Application/API/Admin/APIClientRecordURLs.php
+++ src/classes/Application/API/Admin/APIClientRecordURLs.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Application\API\Admin;
 
+use Application\API\Admin\Screens\APIKeys\BaseAPIKeysSubmode;
 use Application\API\Admin\Screens\BaseAPIClientsArea;
 use Application\API\Admin\Screens\BaseAPIClientSettingsScreen;
 use Application\API\Admin\Screens\BaseAPIClientStatusScreen;
@@ -43,5 +44,12 @@ class APIClientRecordURLs
         return $this
             ->base()
             ->submode(BaseAPIClientSettingsScreen::URL_NAME);
+    }
+
+    public function apiKeys() : AdminURLInterface
+    {
+        return $this
+            ->base()
+            ->submode(BaseAPIKeysSubmode::URL_NAME);
     }
 }
