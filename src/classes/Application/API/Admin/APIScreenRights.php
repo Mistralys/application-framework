c8e5e0605c0c44ff781eaec038a1652223e51349
Mistralys <s.mordziol@gmail.com>
2025-10-22 14:49:21 +0200

    Added screen rights.

--- src/classes/Application/API/Admin/APIScreenRights.php
+++ src/classes/Application/API/Admin/APIScreenRights.php
@@ -16,4 +16,6 @@ class APIScreenRights
     public const string SCREEN_CLIENTS_VIEW_STATUS = APIRightsInterface::RIGHT_VIEW_API_CLIENTS;
     public const string SCREEN_CLIENTS_SETTINGS = APIRightsInterface::RIGHT_VIEW_API_CLIENTS;
     public const string SCREEN_CLIENTS_SETTINGS_EDIT = APIRightsInterface::RIGHT_EDIT_API_CLIENTS;
+    public const string SCREEN_API_KEYS = APIRightsInterface::RIGHT_VIEW_API_CLIENTS;
+    public const string SCREEN_API_KEYS_CREATE = APIRightsInterface::RIGHT_CREATE_API_CLIENTS;
 }
