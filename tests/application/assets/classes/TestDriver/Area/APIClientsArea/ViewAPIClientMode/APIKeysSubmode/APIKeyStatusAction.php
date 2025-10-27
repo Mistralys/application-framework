17e898e8492ee3033cba29b0d886f57465e8fa95
Mistralys <s.mordziol@gmail.com>
2025-10-27 14:00:53 +0100

    Added test app API key status screen.

---
+++ tests/application/assets/classes/TestDriver/Area/APIClientsArea/ViewAPIClientMode/APIKeysSubmode/APIKeyStatusAction.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TestDriver\Area\APIClientsArea\ViewAPIClientMode\APIKeysSubmode;
+
+use Application\API\Admin\Screens\APIKeys\BaseAPIKeyStatusAction;
+
+class APIKeyStatusAction extends BaseAPIKeyStatusAction
+{
+
+}
