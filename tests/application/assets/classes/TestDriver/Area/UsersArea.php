34cdeedbff44b3b42b77de8cf781090608f64f5b
Mistralys <s.mordziol@gmail.com>
2025-10-29 15:57:44 +0100

    Added test app user management screens.

---
+++ tests/application/assets/classes/TestDriver/Area/UsersArea.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TestDriver\Area;
+
+use Application\Users\Admin\Screens\BaseUsersArea;
+
+class UsersArea extends BaseUsersArea
+{
+
+}
---
+++ tests/application/assets/classes/TestDriver/Area/UsersArea/UserListMode.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TestDriver\Area\UsersArea;
+
+use Application\Users\Admin\Screens\Mode\BaseUserListMode;
+
+class UserListMode extends BaseUserListMode
+{
+
+}
