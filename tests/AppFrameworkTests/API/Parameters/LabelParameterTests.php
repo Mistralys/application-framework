e7309a28def257e8300065f467bb2bfa001d8de0
Mistralys <s.mordziol@gmail.com>
2025-10-31 13:08:56 +0100

    Fixed a test.

--- tests/AppFrameworkTests/API/Parameters/LabelParameterTests.php
+++ tests/AppFrameworkTests/API/Parameters/LabelParameterTests.php
@@ -24,7 +24,7 @@ final class LabelParameterTests extends APITestCase
     {
         $this->assertParamInvalidWithValue(
             new LabelParameter('foo', 'Param Label'),
-            ''
+            '>Invalid Label<',
         );
     }
 }
