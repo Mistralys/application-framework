dfaaff9f8b872c95218dad38bf4d666ddfd26967
Mistralys <s.mordziol@gmail.com>
2025-10-31 12:46:18 +0100

    Added methods for the new param types.

--- src/classes/Application/API/Parameters/ParamTypeSelector.php
+++ src/classes/Application/API/Parameters/ParamTypeSelector.php
@@ -4,6 +4,14 @@ declare(strict_types=1);
 
 namespace Application\API\Parameters;
 
+use Application\API\Parameters\CommonTypes\AliasParameter;
+use Application\API\Parameters\CommonTypes\AlphabeticalParameter;
+use Application\API\Parameters\CommonTypes\AlphanumericParameter;
+use Application\API\Parameters\CommonTypes\DateParameter;
+use Application\API\Parameters\CommonTypes\EmailParameter;
+use Application\API\Parameters\CommonTypes\LabelParameter;
+use Application\API\Parameters\CommonTypes\MD5Parameter;
+use Application\API\Parameters\CommonTypes\NameOrTitleParameter;
 use Application\API\Parameters\Type\BooleanParameter;
 use Application\API\Parameters\Type\IDListParameter;
 use Application\API\Parameters\Type\IntegerParameter;
@@ -73,5 +81,78 @@ class ParamTypeSelector
 
         return $param;
 
+    }
+
+    public function alias(bool $allowCapitalLetters) : AliasParameter
+    {
+        $param = new AliasParameter($allowCapitalLetters, $this->name, $this->label);
+
+        $this->manager->registerParam($param);
+
+        return $param;
+    }
+
+    public function alphabetical() : AlphabeticalParameter
+    {
+        $param = new AlphabeticalParameter($this->name, $this->label);
+
+        $this->manager->registerParam($param);
+
+        return $param;
+
+    }
+
+    public function alphanumeric() : AlphanumericParameter
+    {
+        $param = new AlphanumericParameter($this->name, $this->label);
+
+        $this->manager->registerParam($param);
+
+        return $param;
+    }
+
+    public function date() : DateParameter
+    {
+        $param = new DateParameter($this->name, $this->label);
+
+        $this->manager->registerParam($param);
+
+        return $param;
+    }
+
+    public function email() : EmailParameter
+    {
+        $param = new EmailParameter($this->name, $this->label);
+
+        $this->manager->registerParam($param);
+
+        return $param;
+    }
+
+    public function label() : LabelParameter
+    {
+        $param = new LabelParameter($this->name, $this->label);
+
+        $this->manager->registerParam($param);
+
+        return $param;
+    }
+
+    public function md5() : MD5Parameter
+    {
+        $param = new MD5Parameter($this->name, $this->label);
+
+        $this->manager->registerParam($param);
+
+        return $param;
+    }
+
+    public function nameOrTitle() : NameOrTitleParameter
+    {
+        $param = new NameOrTitleParameter($this->name, $this->label);
+
+        $this->manager->registerParam($param);
+
+        return $param;
     }
 }
