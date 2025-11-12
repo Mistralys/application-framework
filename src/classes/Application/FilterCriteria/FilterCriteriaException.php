f26541ae63f89424e91899559811fc9f55826db4
Mistralys <s.mordziol@gmail.com>
2025-11-12 09:47:24 +0100

    Added exception codes.

--- src/classes/Application/FilterCriteria/FilterCriteriaException.php
+++ src/classes/Application/FilterCriteria/FilterCriteriaException.php
@@ -9,4 +9,15 @@ use Application_Exception;
 class FilterCriteriaException extends Application_Exception
 {
 
+    public const int ERROR_MISSING_SELECT_KEYWORD = 710004;
+    public const int ERROR_JOIN_ALREADY_REGISTERED = 710007;
+    public const int ERROR_CANNOT_USE_WILDCARD_AND_DISTINCT = 710009;
+    public const int ERROR_JOIN_ALREADY_ADDED = 710008;
+    public const int ERROR_INVALID_WHERE_STATEMENT = 710001;
+    public const int ERROR_CUSTOM_COLUMN_NOT_REGISTERED = 710005;
+    public const int ERROR_JOIN_ID_NOT_FOUND = 710006;
+    public const int ERROR_EMPTY_SELECT_FIELDS_LIST = 710002;
+    public const int ERROR_MAX_BUILD_ITERATIONS_REACHED = 90502;
+    public const int ERROR_MAX_REPLACE_ITERATIONS_REACHED = 90503;
+    public const int ERROR_CANNOT_REGISTER_COLUMN_AGAIN = 90501;
 }
