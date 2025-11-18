0a8a1c50018fa2f2250642e41ed2875ceca7c2ef
Mistralys <s.mordziol@gmail.com>
2025-11-18 14:18:54 +0100

    Added a trait analysis stub class.

---
+++ tests/AppFrameworkTestClasses/Stubs/Admin/ScreenRightsContainerStub.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AppFrameworkTestClasses\Stubs\Admin;
+
+use Application\Admin\ScreenRightsContainerInterface;
+use Application\Admin\ScreenRightsContainerTrait;
+use Application\Admin\ScreenRightsInterface;
+use Application\NewsCentral\NewsScreens;
+
+class ScreenRightsContainerStub implements ScreenRightsContainerInterface
+{
+    use ScreenRightsContainerTrait;
+
+    protected function createAdminScreens(): ScreenRightsInterface
+    {
+        return new NewsScreens();
+    }
+}
