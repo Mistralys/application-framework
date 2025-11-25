4f9050f82686c2de16f29e86dd7eacb0561700b4
Mistralys <s.mordziol@gmail.com>
2025-11-25 17:01:22 +0100

    Added missing test interface method.

--- tests/AppFrameworkTestClasses/API/APIClientTestInterface.php
+++ tests/AppFrameworkTestClasses/API/APIClientTestInterface.php
@@ -5,11 +5,12 @@ declare(strict_types=1);
 namespace AppFrameworkTestClasses\API;
 
 use AppFrameworkTestClasses\ApplicationTestCaseInterface;
+use Application\API\Clients\Keys\APIKeyRecord;
 
 /**
  * @see APIClientTestTrait
  */
 interface APIClientTestInterface extends ApplicationTestCaseInterface
 {
-
+    public function createTestAPIKey() : APIKeyRecord;
 }
