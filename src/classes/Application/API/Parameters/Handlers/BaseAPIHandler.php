1e527540f9e3875a45047b22ec1b3e3748332788
Mistralys <s.mordziol@gmail.com>
2025-11-25 15:54:16 +0100

    Added `requireValue()`.

--- src/classes/Application/API/Parameters/Handlers/BaseAPIHandler.php
+++ src/classes/Application/API/Parameters/Handlers/BaseAPIHandler.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Application\API\Parameters\Handlers;
 
 use Application\API\APIMethodInterface;
+use Application\API\Parameters\APIParameterException;
 use Application\API\Parameters\APIParamManager;
 
 abstract class BaseAPIHandler implements APIHandlerInterface
@@ -30,6 +31,19 @@ abstract class BaseAPIHandler implements APIHandlerInterface
     public function resolveValue() : mixed
     {
         return $this->selectedValue ?? $this->resolveValueFromSubject();
+    }
+
+    public function requireValue(): string|int|float|bool|array|object
+    {
+        $value = $this->resolveValue();
+
+        if($value === null) {
+            throw new APIParameterException(
+                'Required parameter value is missing.'
+            );
+        }
+
+        return $value;
     }
 
     /**
