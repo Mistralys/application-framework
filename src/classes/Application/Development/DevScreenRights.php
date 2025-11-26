82fcc4b40c0c927fa0f70bb7b688ec4e79727438
Mistralys <s.mordziol@gmail.com>
2025-11-26 22:21:29 +0100

    Added some dev screen right constants.

--- src/classes/Application/Development/DevScreenRights.php
+++ src/classes/Application/Development/DevScreenRights.php
@@ -8,5 +8,11 @@ use Application_User;
 
 class DevScreenRights
 {
-    public const SCREEN_RIGHTS_OVERVIEW = Application_User::RIGHT_DEVELOPER;
+    public const string SCREEN_RIGHTS_OVERVIEW = Application_User::RIGHT_DEVELOPER;
+    public const string SCREEN_RENAMER = Application_User::RIGHT_DEVELOPER;
+    public const string SCREEN_RENAMER_SEARCH = Application_User::RIGHT_DEVELOPER;
+    public const string SCREEN_RENAMER_RESULTS = Application_User::RIGHT_DEVELOPER;
+    public const string SCREEN_RENAMER_REPLACE = Application_User::RIGHT_DEVELOPER;
+    public const string SCREEN_RENAMER_EXPORT = Application_User::RIGHT_DEVELOPER;
+    public const string SCREEN_RENAMER_CONFIGURATION = Application_User::RIGHT_DEVELOPER;
 }
