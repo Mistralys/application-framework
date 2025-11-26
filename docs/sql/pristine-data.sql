60bec709e27b8d0a5af2b62fdc82b3645b06261b
Mistralys <s.mordziol@gmail.com>
2025-11-26 22:35:12 +0100

    Updated the pristine SQL structure.

---
+++ docs/sql/pristine-data.sql
@@ -0,0 +1,14 @@
+
+INSERT INTO `countries` (`country_id`, `iso`, `label`) VALUES
+    (1, 'de', 'Germany'),
+    (2, 'fr', 'France'),
+    (3, 'es', 'Spain'),
+    (4, 'uk', 'United Kingdom'),
+    (5, 'pl', 'Poland'),
+    (6, 'it', 'Italy'),
+    (7, 'us', 'United States'),
+    (8, 'ro', 'Romania'),
+    (9, 'ca', 'Canada'),
+    (10, 'at', 'Austria'),
+    (11, 'mx', 'Mexico'),
+    (9999, 'zz', 'Country-independent');
