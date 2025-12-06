1797d81fd9cf7a8762b947c4c719d96b8a328646
Mistralys <s.mordziol@gmail.com>
2025-12-06 11:42:45 +0100

    Fixed missing time tracker SQL.

--- docs/sql/2025-06-19-time-tracker.sql
+++ docs/sql/2025-06-19-time-tracker.sql
@@ -5,7 +5,41 @@
 
 START TRANSACTION;
 
-ALTER TABLE `time_tracker_entries` ADD `ticket_url` TEXT NOT NULL DEFAULT '' AFTER `ticket`;
+--
+-- Table structure for table `time_tracker_entries`
+--
+
+CREATE TABLE `time_tracker_entries` (
+    `time_entry_id` int(11) UNSIGNED NOT NULL,
+    `user_id` int(11) UNSIGNED NOT NULL,
+    `date` date NOT NULL,
+    `time_start` time DEFAULT NULL,
+    `time_end` time DEFAULT NULL,
+    `duration` int(11) UNSIGNED NOT NULL,
+    `type` varchar(40) NOT NULL,
+    `ticket` varchar(160) NOT NULL,
+    `ticket_url` text NOT NULL DEFAULT '',
+    `processed` enum('yes','no') NOT NULL DEFAULT 'no',
+    `comments` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `time_tracker_entry_data`
+--
+
+CREATE TABLE `time_tracker_entry_data` (
+    `time_entry_id` int(11) UNSIGNED NOT NULL,
+    `name` varchar(180) NOT NULL,
+    `value` mediumtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `time_tracker_time_spans`
+--
 
 CREATE TABLE `time_tracker_time_spans` (
     `time_span_id` int(11) UNSIGNED NOT NULL,
@@ -16,8 +50,42 @@ CREATE TABLE `time_tracker_time_spans` (
     `days` int(11) UNSIGNED NOT NULL,
     `processed` enum('yes','no') NOT NULL DEFAULT 'no',
     `comments` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `time_tracker_types`
+--
+
+CREATE TABLE `time_tracker_types` (
+    `time_type_id` int(11) UNSIGNED NOT NULL,
+    `label` varchar(160) NOT NULL,
+    `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for table `time_tracker_entries`
+--
+ALTER TABLE `time_tracker_entries`
+    ADD PRIMARY KEY (`time_entry_id`),
+    ADD KEY `user_id` (`user_id`),
+    ADD KEY `date` (`date`),
+    ADD KEY `type` (`type`),
+    ADD KEY `ticket` (`ticket`),
+    ADD KEY `duration` (`duration`),
+    ADD KEY `processed` (`processed`);
+
+--
+-- Indexes for table `time_tracker_entry_data`
+--
+ALTER TABLE `time_tracker_entry_data`
+    ADD KEY `time_entry_id` (`time_entry_id`),
+    ADD KEY `name` (`name`);
+
+--
+-- Indexes for table `time_tracker_time_spans`
+--
 ALTER TABLE `time_tracker_time_spans`
     ADD PRIMARY KEY (`time_span_id`),
     ADD KEY `user_id` (`user_id`),
@@ -27,7 +95,17 @@ ALTER TABLE `time_tracker_time_spans`
     ADD KEY `type` (`type`),
     ADD KEY `days` (`days`);
 
-ALTER TABLE `time_tracker_time_spans`
-    MODIFY `time_span_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- Indexes for table `time_tracker_types`
+--
+ALTER TABLE `time_tracker_types`
+    ADD PRIMARY KEY (`time_type_id`),
+    ADD KEY `label` (`label`);
+
+--
+-- Constraints for table `time_tracker_entry_data`
+--
+ALTER TABLE `time_tracker_entry_data`
+    ADD CONSTRAINT `time_tracker_entry_data_ibfk_1` FOREIGN KEY (`time_entry_id`) REFERENCES `time_tracker_entries` (`time_entry_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 COMMIT;
