41b7a8fdeeed2ec288e21cd1abd0e7f900e42de1
Mistralys <s.mordziol@gmail.com>
2025-12-10 17:06:24 +0100

    Added some error constants.

--- src/classes/Application/Admin/Exception.php
+++ src/classes/Application/Admin/Exception.php
@@ -1,6 +1,12 @@
 <?php
 
-class Application_Admin_Exception extends Application_Exception
-{
+declare(strict_types=1);
 
+use Application\ApplicationException;
+
+class Application_Admin_Exception extends ApplicationException
+{
+    public const int ERROR_SCREEN_SOURCE_NOT_FOUND = 188401;
+    public const int ERROR_SCREEN_INDEX_NOT_FOUND = 188403;
+    public const int ERROR_SCREEN_INDEX_INVALID = 188404;
 }
