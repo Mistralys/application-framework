0ee78acc053dc3b62076d17ffe7d018f6beb26ed
Mistralys <s.mordziol@gmail.com>
2025-12-12 10:04:27 +0100

    Added `adminURL()`.

--- src/classes/Application/Development/DevManager.php
+++ src/classes/Application/Development/DevManager.php
@@ -4,10 +4,27 @@ declare(strict_types=1);
 
 namespace Application\Development;
 
+use Application\Development\Admin\AppDevAdminURLs;
 use AppUtils\FileHelper\FolderInfo;
 
 class DevManager
 {
+    private static ?DevManager $instance = null;
+
+    public static function getInstance() : static
+    {
+        if(self::$instance === null) {
+            self::$instance = new static();
+        }
+
+        return self::$instance;
+    }
+
+    public function adminURL() : AppDevAdminURLs
+    {
+        return AppDevAdminURLs::getInstance();
+    }
+
     public static function getAdminScreensFolder() : FolderInfo
     {
         return FolderInfo::factory(__DIR__.'/Admin/Screens')->requireExists();
