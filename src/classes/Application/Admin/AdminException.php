8fde281cb66b4df92768c053b11cddb0e8bf60c6
Mistralys <s.mordziol@gmail.com>
2025-12-19 16:29:13 +0100

    Added a constant.

--- src/classes/Application/Admin/AdminException.php
+++ src/classes/Application/Admin/AdminException.php
@@ -7,8 +7,8 @@ use Application\ApplicationException;
 class AdminException extends ApplicationException
 {
     public const int ERROR_SCREEN_SOURCE_NOT_FOUND = 188401;
-    public const int ERROR_SCREEN_INDEX_NOT_FOUND = 188403;
     public const int ERROR_SCREEN_INDEX_INVALID = 188404;
     public const int ERROR_SCREEN_SUBSCREEN_NOT_FOUND = 188405;
     public const int ERROR_INVALID_APP_SOURCE_FOLDER = 188406;
+    public const int ERROR_ADMIN_AREA_NOT_FOUND = 188407;
 }
