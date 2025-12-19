2a9eda89f27d4707b236adceb1528ae9c3c1be68
Mistralys <s.mordziol@gmail.com>
2025-12-19 17:01:53 +0100

    Removed obsolete tests, fixed others.

--- tests/AppFrameworkTests/AppSets/AppSetTest.php
+++ tests/AppFrameworkTests/AppSets/AppSetTest.php
@@ -5,32 +5,68 @@ declare(strict_types=1);
 namespace AppFrameworkTests\AppSets;
 
 use AppFrameworkTestClasses\ApplicationTestCase;
+use Application\Admin\Index\AdminScreenIndex;
+use Application\Admin\Welcome\Screens\WelcomeArea;
+use Application\AppFactory;
+use Application\AppSets\AppSetsCollection;
 use Application\NewsCentral\Admin\Screens\ManageNewsArea;
-use Application_Sets_Set;
+use Application\AppSets\AppSet;
+use Application\Sets\AppSetsException;
 
 final class AppSetTest extends ApplicationTestCase
 {
-    public function test_recognizeByURLName() : void
+    public function test_createNew() : void
     {
-        $set = Application_Sets_Set::fromArray(array(
-            Application_Sets_Set::KEY_ID => 'test',
-            Application_Sets_Set::KEY_DEFAULT_AREA => ManageNewsArea::URL_NAME,
-            Application_Sets_Set::KEY_ENABLED => array()
-        ));
+        $set = AppSetsCollection::getInstance()->createNew(
+            'test',
+            'Label',
+            AppFactory::createDriver()->createArea(ManageNewsArea::URL_NAME)
+        );
 
-        $this->assertSame(ManageNewsArea::URL_NAME, $set->getDefaultArea()->getURLName());
+        $this->assertSame('test', $set->getAlias());
+        $this->assertSame('Label', $set->getLabel());
+        $this->assertInstanceOf(ManageNewsArea::class, $set->getDefaultArea());
     }
 
-    public function test_recognizeByClassName() : void
+    public function test_createNewAliasExists() : void
     {
-        $name = getClassTypeName(ManageNewsArea::class);
+        AppSetsCollection::getInstance()->createNew(
+            'test',
+            'Label',
+            AppFactory::createDriver()->createArea(WelcomeArea::URL_NAME)
+        );
 
-        $set = Application_Sets_Set::fromArray(array(
-            Application_Sets_Set::KEY_ID => 'test',
-            Application_Sets_Set::KEY_DEFAULT_AREA => $name,
-            Application_Sets_Set::KEY_ENABLED => array()
-        ));
+        $this->expectException(AppSetsException::class);
+        $this->expectExceptionCode(AppSetsException::ERROR_ALIAS_ALREADY_EXISTS);
 
-        $this->assertSame(ManageNewsArea::class, get_class($set->getDefaultArea()));
+        AppSetsCollection::getInstance()->createNew(
+            'test',
+            'Label',
+            AppFactory::createDriver()->createArea(WelcomeArea::URL_NAME)
+        );
+    }
+
+    public function test_defaultAppSetHasAllAreasEnabled() :void
+    {
+        $collection = AppFactory::createAppSets();
+        $defaultSet = $collection->getDefaultSet();
+
+        $driver = AppFactory::createDriver();
+        $areas = $driver->getAdminAreaObjects();
+
+        foreach($areas as $area) {
+            $this->assertTrue($defaultSet->isAreaEnabled($area));
+        }
+
+        $this->assertSame(AdminScreenIndex::getInstance()->getAdminAreaURLNames(), $defaultSet->getEnabledURLNames());
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->startTransaction();
+
+        AppSetsCollection::getInstance()->resetCollection();
     }
 }
