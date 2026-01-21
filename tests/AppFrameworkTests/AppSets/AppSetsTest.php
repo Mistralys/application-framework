c6206535d63c7519f140492c985dbe0e48a8bb44
Mistralys <s.mordziol@gmail.com>
2026-01-21 13:55:04 +0100

    Added some tests.

--- tests/AppFrameworkTests/AppSets/AppSetsTest.php
+++ tests/AppFrameworkTests/AppSets/AppSetsTest.php
@@ -55,10 +55,74 @@ final class AppSetsTest extends ApplicationTestCase
         $areas = $driver->getAdminAreaObjects();
 
         foreach($areas as $area) {
-            $this->assertTrue($defaultSet->isAreaEnabled($area));
+            $this->assertTrue(
+                $defaultSet->isAreaEnabled($area),
+                sprintf('Area "%s" should be enabled but is not', $area->getURLName())
+            );
         }
 
         $this->assertSame(AdminScreenIndex::getInstance()->getAdminAreaURLNames(), $defaultSet->getEnabledURLNames());
+    }
+
+    public function test_defaultAppSetURLNamesFormat() : void
+    {
+        $collection = AppFactory::createAppSets();
+        $defaultSet = $collection->getDefaultSet();
+
+        // Check what getEnabledAreaURLNames returns
+        $urlNames = $defaultSet->getEnabledAreaURLNames();
+        $this->assertIsArray($urlNames, 'getEnabledAreaURLNames should return an array');
+
+        // Check the raw data from loadData
+        $reflection = new \ReflectionClass($defaultSet);
+        $method = $reflection->getMethod('loadData');
+        $method->setAccessible(true);
+        $data = $method->invoke($defaultSet);
+
+        $this->assertIsString(
+            $data[AppSetsCollection::COL_URL_NAMES],
+            'COL_URL_NAMES in loadData should be a string (comma-separated)'
+        );
+    }
+
+    public function test_defaultAppSetGetEnabledAreas() : void
+    {
+        $collection = AppFactory::createAppSets();
+        $defaultSet = $collection->getDefaultSet();
+
+        $enabledAreas = $defaultSet->getEnabledAreas();
+        $allAreas = AppFactory::createDriver()->getAdminAreaObjects();
+
+        $this->assertCount(
+            count($allAreas),
+            $enabledAreas,
+            'Default set should have all areas enabled'
+        );
+    }
+
+    public function test_defaultAppSetInitializesAreasFromURLNames() : void
+    {
+        $collection = AppFactory::createAppSets();
+        $defaultSet = $collection->getDefaultSet();
+
+        // Get the URL names that should be enabled
+        $expectedURLNames = $defaultSet->getEnabledAreaURLNames();
+        $this->assertNotEmpty($expectedURLNames, 'Default set should have enabled URL names');
+
+        // Verify each URL name corresponds to an enabled area
+        $enabledAreas = $defaultSet->getEnabledAreas(false); // Exclude core areas
+        $enabledURLNames = array_map(function($area) {
+            return $area->getURLName();
+        }, $enabledAreas);
+
+        sort($expectedURLNames);
+        sort($enabledURLNames);
+
+        $this->assertSame(
+            $expectedURLNames,
+            $enabledURLNames,
+            'Enabled areas should match the URL names from getEnabledAreaURLNames()'
+        );
     }
 
     protected function setUp(): void
