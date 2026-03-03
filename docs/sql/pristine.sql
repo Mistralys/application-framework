c65cc30f09612f8046b1371591b08be0d1d8e5ff
Mistralys <s.mordziol@gmail.com>
2026-03-03 19:48:50 +0100

    SQL: Added missing table.

--- docs/sql/pristine.sql
+++ docs/sql/pristine.sql
@@ -314,6 +314,22 @@ CREATE TABLE `app_ratings_screens` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `app_sets`
+--
+
+CREATE TABLE `app_sets` (
+    `app_set_id` int(11) UNSIGNED NOT NULL,
+    `alias` varchar(160) CHARACTER SET ascii COLLATE ascii_general_ci NOT NULL,
+    `is_active` enum('yes','no') NOT NULL DEFAULT 'no',
+    `label` varchar(180) NOT NULL,
+    `description` text NOT NULL DEFAULT '',
+    `default_url_name` varchar(80) CHARACTER SET ascii COLLATE ascii_general_ci NOT NULL,
+    `enabled_url_names` text NOT NULL DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `app_settings`
 --
 
@@ -928,6 +944,22 @@ ALTER TABLE `app_ratings_screens`
     ADD UNIQUE KEY `hash` (`hash`),
     ADD KEY `dispatcher` (`dispatcher`),
     ADD KEY `path` (`path`);
+
+--
+-- Indexes for table `app_sets`
+--
+ALTER TABLE `app_sets`
+    ADD PRIMARY KEY (`app_set_id`),
+    ADD KEY `alias` (`alias`),
+    ADD KEY `label` (`label`),
+    ADD KEY `is_active` (`is_active`),
+    ADD KEY `default_url_name` (`default_url_name`);
+
+--
+-- Indexes for table `app_sets`
+--
+ALTER TABLE `app_sets`
+    MODIFY `app_set_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- Indexes for table `app_settings`
